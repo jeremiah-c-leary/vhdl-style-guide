301c27327e88d51427b630a898bda468f02e37d4
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2017-07-31 22:12:54 -0500

    Updates for whitespace rule 002:
      1)  Added whitespace rule 002

--- modules/rule_whitespace.py
+++ modules/rule_whitespace.py
@@ -1,13 +1,18 @@
 
 import rule
 
-
-class rule_001(rule.rule):
-    '''Whitespace rule 001 checks spaces at the end of lines.'''
-
+class whitespace_rule(rule.rule):
+    
     def __init__(self):
         rule.rule.__init__(self)
         self.name = 'whitespace'
+
+
+class rule_001(whitespace_rule):
+    '''Whitespace rule 001 checks spaces at the end of lines.'''
+
+    def __init__(self):
+        whitespace_rule.__init__(self)
         self.identifier = '001'
 
     def analyze(self, lines):
@@ -16,3 +21,19 @@ class rule_001(rule.rule):
             if sLine.endswith(' '):
                 lFailureLines.append(iLineNumber)
         return {self.name: {self.identifier: lFailureLines}}
+
+
+class rule_002(whitespace_rule):
+    '''Whitespace rule 002 checks for tabs in lines'''
+
+    def __init__(self):
+        whitespace_rule.__init__(self)
+        self.identifier = '002'
+
+    def analyze(self, lines):
+        lFailureLines = []
+        for iLineNumber, sLine in enumerate(lines):
+            if '\t' in sLine:
+                lFailureLines.append(iLineNumber)
+        return {self.name: {self.identifier: lFailureLines}}
+
