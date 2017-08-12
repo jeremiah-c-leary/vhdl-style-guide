b6acb48ba59c092336b44c5f2906e4d8bd97ac9f
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2017-08-12 16:38:11 -0500

    Updating tests to reflect change in rule class for storing rule violations.

--- modules/tests/rule_whitespace/rule_whitespace_001.py
+++ modules/tests/rule_whitespace/rule_whitespace_001.py
@@ -17,19 +17,26 @@ class testRuleWhitespaceMethods(unittest.TestCase):
     def test_rule_001(self):
         oRule = rule_whitespace.rule_001()
 
-        dExpected = {'whitespace' : {'001' :[1,3]}}
+        dExpected = [2,4]
         lLines = []
         lLines.append('  This is a test of ending whitespace')
         lLines.append('  This is a test of ending whitespace ')
         lLines.append('  This is a test of ending whitespace')
         lLines.append('  This is a test of ending whitespace  ')
         lLines.append('  This is a test of ending whitespace')
-        self.assertEqual(oRule.analyze(lLines), dExpected)
+        oRule.analyze(lLines)
+        self.assertEqual(oRule.violations, dExpected)
+
+    def test_rule_002_exists(self):
+        oRule = rule_whitespace.rule_002()
+        self.assertTrue(oRule)
+        self.assertEqual(oRule.name, 'whitespace')
+        self.assertEqual(oRule.identifier, '002')
 
     def test_rule_002(self):
         oRule = rule_whitespace.rule_002()
 
-        dExpected = {'whitespace' : {'002' :[0,1,4]}}
+        dExpected = [1,2,5]
         lLines = []
         lLines.append('  This is a test of tabs\t')
         lLines.append('\tThis is a test of tabs')
@@ -37,7 +44,8 @@ class testRuleWhitespaceMethods(unittest.TestCase):
         lLines.append('  This is a test of tabs')
         lLines.append('  This is a \t test of tabs')
         lLines.append('  This is a test of tabs')
-        self.assertEqual(oRule.analyze(lLines), dExpected)
+        oRule.analyze(lLines)
+        self.assertEqual(oRule.violations, dExpected)
 
 
 if __name__ == '__main__':
