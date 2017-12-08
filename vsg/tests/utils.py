04db8c65ee8d0462b99fa6759f5d465d8a1bae0e
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2017-12-07 19:26:00 -0600

    Improving code coverage by excluding code in the test utils package. This is test code and does not need to be covered.

--- vsg/tests/utils.py
+++ vsg/tests/utils.py
@@ -1,6 +1,6 @@
 
 
-def debug_lines(oFile, iLineNumber, iNumberOfLines):
+def debug_lines(oFile, iLineNumber, iNumberOfLines):  # pragma: no cover
 
     for iIndex in range (0, iNumberOfLines):
         print '{0:5d} | {1:s}'.format(iLineNumber + iIndex, oFile.lines[iLineNumber + iIndex].line)
