bca183b159dd8af7554a2d8d6e2da1d50ce1ddd4
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2017-12-15 11:47:18 -0600

    Fixing build error in concurrent_006.

--- vsg/rules/concurrent/rule_006.py
+++ vsg/rules/concurrent/rule_006.py
@@ -1,16 +1,18 @@
 
-from vsg.rules import keyword_alignment_rule
+from vsg import rule
+from vsg import check
+from vsg import fix
 
 
-class rule_006(keyword_alignment_rule):
+class rule_006(rule.rule):
     '''
     Concurrent rule 006 ensures the alignment of the "<=" keyword over multiple lines.
     '''
 
     def __init__(self):
-        keyword_alignment_rule.__init__(self, 'concurrent', '006')
+        rule.rule.__init__(self, 'concurrent', '006')
         self.solution = 'Inconsistent alignment of "<=" in group of lines.'
-        self.
+        self.phase = 5
 
     def analyze(self, oFile):
         lGroup = []
