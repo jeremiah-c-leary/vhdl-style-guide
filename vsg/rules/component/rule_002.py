32997c99c3c268bdbf9ae7615da9e6b7270beef2
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2017-12-28 11:16:37 -0600

    Refactored component_002 and constant_006 rules.

--- vsg/rules/component/rule_002.py
+++ vsg/rules/component/rule_002.py
@@ -1,28 +1,12 @@
 
-from vsg import rule
-from vsg import fix
-
-import re
+from vsg.rules import single_space_after_rule
 
 
-class rule_002(rule.rule):
+class rule_002(single_space_after_rule):
     '''
     Component rule 002 checks for a single space after the "component" keyword.
     '''
 
     def __init__(self):
-        rule.rule.__init__(self)
-        self.name = 'component'
-        self.identifier = '002'
+        single_space_after_rule.__init__(self, 'component', '002', 'isComponentDeclaration', 'component')
         self.solution = 'Remove extra spaces after "component" keyword.'
-        self.phase = 2
-
-    def analyze(self, oFile):
-        for iLineNumber, oLine in enumerate(oFile.lines):
-            if oLine.isComponentDeclaration:
-                if re.match('^\s*component\s\s+\w', oLine.lineLower):
-                    self.add_violation(iLineNumber)
-
-    def _fix_violations(self, oFile):
-        for iLineNumber in self.violations:
-            fix.enforce_one_space_after_word(self, oFile.lines[iLineNumber], 'component')
--- vsg/rules/constant/rule_006.py
+++ vsg/rules/constant/rule_006.py
@@ -1,28 +1,12 @@
 
-from vsg import rule
-from vsg import fix
-
-import re
+from vsg.rules import single_space_before_character_rule
 
 
-class rule_006(rule.rule):
+class rule_006(single_space_before_character_rule):
     '''
     Constant rule 006 checks there is at least a single space before the colon.
     '''
 
     def __init__(self):
-        rule.rule.__init__(self)
-        self.name = 'constant'
-        self.identifier = '006'
+        single_space_before_character_rule.__init__(self, 'constant', '006', 'isConstant', ':')
         self.solution = 'Add a single space before the colon.'
-        self.phase = 2
-
-    def analyze(self, oFile):
-        for iLineNumber, oLine in enumerate(oFile.lines):
-            if oLine.isConstant:
-                if re.match('^\s*constant\s+\S+:', oLine.lineLower):
-                    self.add_violation(iLineNumber)
-
-    def _fix_violations(self, oFile):
-        for iLineNumber in self.violations:
-            fix.enforce_one_space_before_word(self, oFile.lines[iLineNumber], ':')
