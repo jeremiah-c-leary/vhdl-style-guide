7a77f509d9f21e9cf6fa2902049b1bf4a8e38d45
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2017-12-29 08:08:23 -0600

    Refactoring concurrent_002.

--- vsg/rules/concurrent/rule_002.py
+++ vsg/rules/concurrent/rule_002.py
@@ -1,26 +1,12 @@
 
-from vsg import rule
-from vsg import check
-from vsg import fix
+from vsg.rules import single_space_after_character_rule
 
 
-class rule_002(rule.rule):
+class rule_002(single_space_after_character_rule):
     '''
     Concurrent rule 002 checks there is a single space after the assignment.
     '''
 
     def __init__(self):
-        rule.rule.__init__(self)
-        self.name = 'concurrent'
-        self.identifier = '002'
+        single_space_after_character_rule.__init__(self, 'concurrent', '002', 'isConcurrentBegin', '<=')
         self.solution = 'Remove all but one space after the <=.'
-        self.phase = 2
-
-    def analyze(self, oFile):
-        for iLineNumber, oLine in enumerate(oFile.lines):
-            if oLine.isConcurrentBegin:
-                check.is_single_space_after_character(self, '<=', oLine, iLineNumber)
-
-    def _fix_violations(self, oFile):
-        for iLineNumber in self.violations:
-            fix.enforce_one_space_after_word(self, oFile.lines[iLineNumber], '<=')
