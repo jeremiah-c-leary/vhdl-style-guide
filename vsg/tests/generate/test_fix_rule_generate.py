82fe3c0a1fad44495f95b38dc9232c519ed46bd7
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2019-02-05 17:06:40 -0600

    Updates for issue #199
    Added fix test for generate_008

--- vsg/tests/generate/test_fix_rule_generate.py
+++ vsg/tests/generate/test_fix_rule_generate.py
@@ -4,37 +4,46 @@ import unittest
 from vsg.rules import generate
 from vsg import vhdlFile
 
-# Read in test file used for all tests
-oFile = vhdlFile.vhdlFile(os.path.join(os.path.dirname(__file__),'..','generate','generate_test_input.vhd'))
-
 class testFixRuleGenerateMethods(unittest.TestCase):
+
+    def setUp(self):
+        # Read in test file used for all tests
+        self.oFile = vhdlFile.vhdlFile(os.path.join(os.path.dirname(__file__),'..','generate','generate_test_input.vhd'))
 
     def test_fix_rule_001(self):
         oRule = generate.rule_001()
-        oRule.fix(oFile)
-        oRule.analyze(oFile)
+        oRule.fix(self.oFile)
+        oRule.analyze(self.oFile)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_002(self):
         oRule = generate.rule_002()
-        oRule.fix(oFile)
-        oRule.analyze(oFile)
+        oRule.fix(self.oFile)
+        oRule.analyze(self.oFile)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_003(self):
         oRule = generate.rule_003()
-        oRule.fix(oFile)
-        oRule.analyze(oFile)
+        oRule.fix(self.oFile)
+        oRule.analyze(self.oFile)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_004(self):
         oRule = generate.rule_004()
-        oRule.fix(oFile)
-        oRule.analyze(oFile)
+        oRule.fix(self.oFile)
+        oRule.analyze(self.oFile)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_005(self):
         oRule = generate.rule_005()
-        oRule.fix(oFile)
-        oRule.analyze(oFile)
+        oRule.fix(self.oFile)
+        oRule.analyze(self.oFile)
         self.assertEqual(oRule.violations, [])
+
+    def test_fix_rule_008(self):
+        oRule = generate.rule_008()
+        oRule.fix(self.oFile)
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
+        self.assertEqual(self.oFile.lines[29].line, '  end generate GENERATE_1;')
+
