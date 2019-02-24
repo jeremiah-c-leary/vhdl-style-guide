25cd3157d02f5c551fd43eb58f2f42b02700e11a
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2019-02-24 10:07:05 -0600

    Updates for issue #212
      1)  Refactored tests to use setUp procedure for generic tests

--- vsg/tests/instantiation/test_fix_generic_rule_instantiation.py
+++ vsg/tests/instantiation/test_fix_generic_rule_instantiation.py
@@ -5,70 +5,72 @@ import unittest
 from vsg.rules import instantiation
 from vsg import vhdlFile
 
-oFileGeneric = vhdlFile.vhdlFile(os.path.join(os.path.dirname(__file__),'instantiation_generic_test_input.vhd'))
-
 class testFixRuleInstantiationMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.oFileGeneric = vhdlFile.vhdlFile(os.path.join(os.path.dirname(__file__),'instantiation_generic_test_input.vhd'))
+        
 
     def test_fix_rule_012(self):
         oRule = instantiation.rule_012()
-        oRule.fix(oFileGeneric)
-        oRule.analyze(oFileGeneric)
+        oRule.fix(self.oFileGeneric)
+        oRule.analyze(self.oFileGeneric)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_013(self):
         oRule = instantiation.rule_013()
-        oRule.fix(oFileGeneric)
-        oRule.analyze(oFileGeneric)
+        oRule.fix(self.oFileGeneric)
+        oRule.analyze(self.oFileGeneric)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_014(self):
         oRule = instantiation.rule_014()
-        oRule.fix(oFileGeneric)
-        oRule.analyze(oFileGeneric)
+        oRule.fix(self.oFileGeneric)
+        oRule.analyze(self.oFileGeneric)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_015(self):
         oRule = instantiation.rule_015()
-        oRule.fix(oFileGeneric)
-        oRule.analyze(oFileGeneric)
+        oRule.fix(self.oFileGeneric)
+        oRule.analyze(self.oFileGeneric)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_016(self):
         oRule = instantiation.rule_016()
-        oRule.fix(oFileGeneric)
-        oRule.analyze(oFileGeneric)
+        oRule.fix(self.oFileGeneric)
+        oRule.analyze(self.oFileGeneric)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_017(self):
         oRule = instantiation.rule_017()
-        oRule.fix(oFileGeneric)
-        oRule.analyze(oFileGeneric)
+        oRule.fix(self.oFileGeneric)
+        oRule.analyze(self.oFileGeneric)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_001_generics(self):
         oRule = instantiation.rule_001()
-        oRule.fix(oFileGeneric)
-        oRule.analyze(oFileGeneric)
+        oRule.fix(self.oFileGeneric)
+        oRule.analyze(self.oFileGeneric)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_rule_018(self):
         oRule = instantiation.rule_018()
-        oRule.fix(oFileGeneric)
-        oRule.analyze(oFileGeneric)
+        oRule.fix(self.oFileGeneric)
+        oRule.analyze(self.oFileGeneric)
         self.assertEqual(oRule.violations, [])
 
     def test_fix_fule_025(self):
         oRule = instantiation.rule_025()
-        oRule.fix(oFileGeneric)
-        oRule.analyze(oFileGeneric)
+        oRule.fix(self.oFileGeneric)
+        oRule.analyze(self.oFileGeneric)
         self.assertEqual(oRule.violations, [])
-        self.assertEqual(oFileGeneric.lines[81].line, '    generic map (')
-        self.assertEqual(oFileGeneric.lines[82].line, '      GENERIC_1 => generic_1,')
+        self.assertEqual(self.oFileGeneric.lines[81].line, '    generic map (')
+        self.assertEqual(self.oFileGeneric.lines[82].line, '      GENERIC_1 => generic_1,')
 
     def test_fix_fule_026(self):
         oRule = instantiation.rule_026()
-        oRule.fix(oFileGeneric)
-        oRule.analyze(oFileGeneric)
+        oRule.fix(self.oFileGeneric)
+        oRule.analyze(self.oFileGeneric)
         self.assertEqual(oRule.violations, [])
-        self.assertEqual(oFileGeneric.lines[85].line, '    port map (')
-        self.assertEqual(oFileGeneric.lines[86].line, '      PORT_1 => w_port_1,')
+        self.assertEqual(self.oFileGeneric.lines[86].line, '    port map (')
+        self.assertEqual(self.oFileGeneric.lines[87].line, '      PORT_1 => w_port_1,')
