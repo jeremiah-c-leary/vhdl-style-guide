e3b548ae5f5d1ebbf7c70759c8140b1f8219bdf6
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2019-03-09 11:12:44 -0600

    Updates for issue #213
      1)  Additional tests added to mirror testing with JSON configurations.

--- vsg/tests/vsg/test_vsg.py
+++ vsg/tests/vsg/test_vsg.py
@@ -110,3 +110,42 @@ class testVsg(unittest.TestCase):
         lActual = subprocess.check_output(['bin/vsg','--configuration','vsg/tests/vsg/config_2.json','--output_format','syntastic']).split('\n')
         self.assertEqual(lActual, lExpected)
 
+    def test_multiple_yaml_configuration_w_multiple_filelists(self):
+        lExpected = []
+        lExpected.append('ERROR: vsg/tests/vsg/entity1.vhd(7)port_007 -- Change the number of spaces after the "in" keyword to four spaces.')
+        lExpected.append('ERROR: vsg/tests/vsg/entity2.vhd(8)port_008 -- Change the number of spaces after the "out" keyword to three spaces.')
+        lExpected.append('')
+
+        lActual = subprocess.check_output(['bin/vsg','--configuration','vsg/tests/vsg/config_1.yaml','vsg/tests/vsg/config_2.yaml','--output_format','syntastic']).split('\n')
+        self.assertEqual(lActual, lExpected)
+
+    def test_single_yaml_configuration_w_rule_disable(self):
+        lExpected = []
+        lExpected.append('')
+
+        lActual = subprocess.check_output(['bin/vsg','--configuration','vsg/tests/vsg/config_3.yaml','--output_format','syntastic','-f','vsg/tests/vsg/entity1.vhd']).split('\n')
+        self.assertEqual(lActual, lExpected)
+
+    def test_multiple_yaml_configuration_w_rule_disable(self):
+        lExpected = []
+        lExpected.append('ERROR: vsg/tests/vsg/entity1.vhd(7)port_007 -- Change the number of spaces after the "in" keyword to four spaces.')
+        lExpected.append('')
+
+        lActual = subprocess.check_output(['bin/vsg','--configuration','vsg/tests/vsg/config_3.yaml','vsg/tests/vsg/config_4.yaml','--output_format','syntastic','-f','vsg/tests/vsg/entity1.vhd']).split('\n')
+        self.assertEqual(lActual, lExpected)
+
+    def test_reverse_yaml_multiple_configuration_w_rule_disable(self):
+        lExpected = []
+        lExpected.append('')
+
+        lActual = subprocess.check_output(['bin/vsg','--configuration','vsg/tests/vsg/config_4.yaml','vsg/tests/vsg/config_3.yaml','--output_format','syntastic','-f','vsg/tests/vsg/entity1.vhd']).split('\n')
+        self.assertEqual(lActual, lExpected)
+
+    def test_globbing_filenames_in_yaml_configuration(self):
+        lExpected = []
+        lExpected.append('ERROR: vsg/tests/vsg/entity2.vhd(8)port_008 -- Change the number of spaces after the "out" keyword to three spaces.')
+        lExpected.append('ERROR: vsg/tests/vsg/entity1.vhd(7)port_007 -- Change the number of spaces after the "in" keyword to four spaces.')
+        lExpected.append('')
+
+        lActual = subprocess.check_output(['bin/vsg','--configuration','vsg/tests/vsg/config_glob.yaml','--output_format','syntastic']).split('\n')
+        self.assertEqual(lActual, lExpected)
