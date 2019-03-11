522ba311a3b5c57d39fb2e85e25d07336742d9ca
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2019-03-10 22:10:33 -0500

    Refactoring rules for codeclimate.

--- vsg/rules/entity/rule_018.py
+++ vsg/rules/entity/rule_018.py
@@ -1,51 +1,17 @@
 
-from vsg import rule
-from vsg import check
-from vsg import fix
-from vsg import line
+from vsg.rules import keyword_alignment_rule
 
 
-class rule_018(rule.rule):
+class rule_018(keyword_alignment_rule):
     '''
     Entity rule 018 ensures the alignment of comments in the entity.
     '''
 
     def __init__(self):
-        rule.rule.__init__(self)
-        self.name = 'entity'
-        self.identifier = '018'
-        self.solution = 'Inconsistent alignment of comments in entity.'
+        keyword_alignment_rule.__init__(self, 'entity', '018')
+        self.solution = 'Inconsistent alignment of comments.'
+        self.sKeyword = '--'
+        self.sStartGroupTrigger = 'insideEntity'
+        self.sEndGroupTrigger = 'isEndEntityDeclaration'
+        self.sLineTrigger = 'hasInlineComment'
         self.phase = 6
-
-    def analyze(self, oFile):
-        lGroup = []
-        fGroupFound = False
-        iStartGroupIndex = None
-        for iLineNumber, oLine in enumerate(oFile.lines):
-            if self._is_vsg_off(oLine):
-                continue
-            fGroupFound, iStartGroupIndex = search_for_group(fGroupFound, oLine, iStartGroupIndex, iLineNumber)
-            if oLine.isEndEntityDeclaration:
-                lGroup.append(oLine)
-                fGroupFound = False
-                check.keyword_alignment(self, iStartGroupIndex, '--', lGroup)
-                lGroup = []
-                iStartGroupIndex = None
-            store_lines_for_group(fGroupFound, oLine, lGroup)
-
-    def _fix_violations(self, oFile):
-        fix.keyword_alignment(self, oFile)
-
-
-def search_for_group(fGroupFound, oLine, iStartGroupIndex, iLineNumber):
-    if not fGroupFound and oLine.insideEntity:
-        return True, iLineNumber
-    return fGroupFound, iStartGroupIndex
-
-
-def store_lines_for_group(fGroupFound, oLine, lGroup):
-    if fGroupFound:
-        if oLine.hasComment and not oLine.isComment:
-            lGroup.append(oLine)
-        else:
-            lGroup.append(line.line('Removed line'))
