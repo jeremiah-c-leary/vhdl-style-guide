7b315dfe2c1016451456dee2b0d7ab924bb190a8
Michał Kruszewski <mkru@protonmail.com>
2019-10-07 15:16:37 +0200

    Make type rule 002 use case_rule as a base.

--- vsg/rules/type_definition/rule_002.py
+++ vsg/rules/type_definition/rule_002.py
@@ -1,12 +1,12 @@
 
-from vsg.rules import lower_case_rule
+from vsg.rules import case_rule
+from vsg import utils
 
-
-class rule_002(lower_case_rule):
+class rule_002(case_rule):
     '''
-    Type rule 002 checks the "type" keyword is lowercase.
+    Type rule 002 checks the "type" keyword has proper case.
     '''
 
     def __init__(self):
-        lower_case_rule.__init__(self, 'type', '002', 'isTypeKeyword', 'type')
-        self.solution = 'Lowercase "type" keyword.'
+        case_rule.__init__(self, 'type', '002', 'isTypeKeyword', utils.extract_first_word)
+        self.solution = 'Change type keyword to ' + self.case + 'case'
