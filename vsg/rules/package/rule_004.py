5f09995b29527e6bd7114a2abc1d5edbeeb27081
Michał Kruszewski <mkru@protonmail.com>
2019-10-07 15:07:23 +0200

    Make package rule 004 use case_rule as a base.

--- vsg/rules/package/rule_004.py
+++ vsg/rules/package/rule_004.py
@@ -1,11 +1,13 @@
 
-from vsg.rules import lower_case_rule
+from vsg.rules import case_rule
+from vsg import utils
 
 
-class rule_004(lower_case_rule):
+class rule_004(case_rule):
     '''
-    Package rule 004 checks the "package" keyword is lower case.
+    Package rule 004 checks the "package" keyword has proper case.
     '''
 
     def __init__(self):
-        lower_case_rule.__init__(self, 'package', '004', 'isPackageKeyword', 'package')
+        case_rule.__init__(self, 'package', '004', 'isPackageKeyword', utils.extract_first_word)
+        self.solution = 'Change package keyword to ' + self.case + 'case'
