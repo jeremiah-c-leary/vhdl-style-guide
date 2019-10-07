5057e084ee0038bf545c9707645063b7a4baad2e
Michał Kruszewski <mkru@protonmail.com>
2019-10-07 15:36:52 +0200

    Make generic rule 009 use case_rule as a base.

--- vsg/rules/generic/rule_009.py
+++ vsg/rules/generic/rule_009.py
@@ -1,12 +1,13 @@
 
-from vsg.rules import lower_case_rule
+from vsg.rules import case_rule
+from vsg import utils
 
 
-class rule_009(lower_case_rule):
+class rule_009(case_rule):
     '''
-    Generic rule 009 checks the "generic" keyword is lowercase.
+    Generic rule 009 checks the "generic" keyword has proper case.
     '''
 
     def __init__(self):
-        lower_case_rule.__init__(self, 'generic', '009', 'isGenericKeyword', 'generic')
-        self.solution = 'Lowercase "generic" keyword.'
+        case_rule.__init__(self, 'generic', '009', 'isGenericKeyword', utils.extract_first_word)
+        self.solution = 'Change generic keyword to ' + self.case + 'case'
