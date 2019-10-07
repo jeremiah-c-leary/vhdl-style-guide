b449cd7f47369cdb1363cb8f52fd4955b8985c52
Michał Kruszewski <mkru@protonmail.com>
2019-10-07 15:19:08 +0200

    Make library rule 005 use case_rule as a base.

--- vsg/rules/library/rule_005.py
+++ vsg/rules/library/rule_005.py
@@ -1,12 +1,13 @@
 
-from vsg.rules import lower_case_rule
+from vsg.rules import case_rule
+from vsg import utils
 
 
-class rule_005(lower_case_rule):
+class rule_005(case_rule):
     '''
-    Library rule 005 checks the use keyword is lower case.
+    Library rule 005 checks the use keyword has proper case.
     '''
 
     def __init__(self):
-        lower_case_rule.__init__(self, 'library', '005', 'isLibraryUse', 'use')
-        self.solution = 'Change "use" keyword to lowercase.'
+        case_rule.__init__(self, 'library', '005', 'isLibraryUse', utils.extract_first_word)
+        self.solution = 'Change use keyword to ' + self.case + 'case'
