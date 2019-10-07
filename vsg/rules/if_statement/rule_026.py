3ab00dddd6a7326d5210745b6de5b8552100771b
Michał Kruszewski <mkru@protonmail.com>
2019-10-07 15:32:55 +0200

    Make if rule 026 use case_rule as a base.

--- vsg/rules/if_statement/rule_026.py
+++ vsg/rules/if_statement/rule_026.py
@@ -1,11 +1,13 @@
 
-from vsg.rules import lower_case_rule
+from vsg.rules import case_rule
+from vsg import utils
 
 
-class rule_026(lower_case_rule):
+class rule_026(case_rule):
     '''
-    If rule 001 checks the **elsif** keyword is lowercase.
+    If rule 026 checks the **elsif** keyword has proper case.
     '''
 
     def __init__(self):
-        lower_case_rule.__init__(self, 'if', '026', 'isElseIfKeyword', 'elsif')
+        case_rule.__init__(self, 'if', '026', 'isElseIfKeyword', utils.extract_first_word)
+        self.solution = 'Change elsif keyword to ' + self.case + 'case'
