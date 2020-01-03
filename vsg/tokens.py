d4ca0159eada5cc2504a364c2924e5b1ce3f5410
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2020-01-02 18:43:38 -0600

    Updates for issue #312:
    Added all operators and assignment symbols.

--- vsg/tokens.py
+++ vsg/tokens.py
@@ -1,5 +1,6 @@
 
-lSingleCharacterSymbols = [',', ':', '(', ')', '\'', '"', '+']
+lSingleCharacterSymbols = [',', ':', '(', ')', '\'', '"', '+', '&', '-', '*', '/', '<', '>']
+lMultipleCharacterSymbols = [':=', '\=', '<=', '=>', '>=', '**']
 
 def create(sString):
     '''
@@ -8,24 +9,49 @@ def create(sString):
     lReturn = []
     sToken = ''
     fCommentFound = False
+    fMultipleCharacterSymbolFound = False
+    fLastChar = False
     for iIndex, sChar in enumerate(sString):
+        if iIndex + 1 == len(sString):
+            fLastChar = True
+            sNextChar = ''
+        else:
+            sNextChar = sString[iIndex + 1]
+
         if len(sToken) == 0:
             sToken = sChar
-            if sChar == '-' and sString[iIndex + 1] == '-':
+            if sChar == '-' and sNextChar == '-':
                 fCommentFound = True
+                continue
+            if sChar + sNextChar in lMultipleCharacterSymbols:
+                fMultipleCharacterSymbolFound = True
+                sToken = sChar
                 continue
             if sChar in lSingleCharacterSymbols:
                 lReturn.append(sToken)
                 sToken = ''
         else:
             # Handle comments
-            if sChar == '-' and sString[iIndex + 1] == '-':
+            if sChar == '-' and sNextChar == '-':
                 fCommentFound = True
                 lReturn.append(sToken)
                 sToken = ''
             if fCommentFound:
                 sToken += sChar
                 continue
+            # Handle multiple character symbols
+            if fMultipleCharacterSymbolFound:
+                fMultipleCharacterSymbolFound = False
+                sToken += sChar
+                lReturn.append(sToken)
+                sToken = ''
+                continue
+            if not fLastChar:
+                if sChar + sNextChar in lMultipleCharacterSymbols:
+                    fMultipleCharacterSymbolFound = True
+                    lReturn.append(sToken)
+                    sToken = sChar
+                    continue
             if sChar in lSingleCharacterSymbols:
                 lReturn.append(sToken)
                 lReturn.append(sChar)
