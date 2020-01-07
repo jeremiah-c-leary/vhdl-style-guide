747536883733a000c16623160f2075449ca93534
Will Kamp <willkamp@gmail.com>
2020-01-08 09:41:29 +1300

    Fix rule comment_004 to use the comment location in oLine, such that it picks up the fix for comment detection.

--- vsg/rules/comment/rule_004.py
+++ vsg/rules/comment/rule_004.py
@@ -1,12 +1,26 @@
 
-from vsg.rules import single_space_before_character_rule
+from vsg import rule
 
 
-class rule_004(single_space_before_character_rule):
+class rule_004(rule.rule):
     '''
     Comment rule 004 ensures there is at least one space before comments on a line with code.
     '''
 
     def __init__(self):
-        single_space_before_character_rule.__init__(self, 'comment', '004', 'hasInlineComment', '--')
+        rule.rule.__init__(self, name='comment', identifier='004')
+        self.phase = 2
         self.solution = 'Add a space before the comment --'
+
+    def _analyze(self, oFile, oLine, iLineNumber):
+        if oLine.__dict__['hasInlineComment']:
+            if oLine.line[oLine.commentColumn - 1] != ' ':
+                self.add_violation(iLineNumber)
+
+    def _fix_violations(self, oFile):
+        for iLineNumber in self.violations:
+            oLine = oFile.lines[iLineNumber]
+            idx = oLine.commentColumn
+            oLine.update_line(" ".join((oLine.line[:idx],oLine.line[idx:])))
+
+
