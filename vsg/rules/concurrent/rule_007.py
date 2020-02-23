ad1e1f0da43f4e46840c23e8e7fd491941edfbf6
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2020-02-23 17:00:00 -0600

    Updates for issue #332:
    Refactored rule concurrent_007 to improve readability.

--- vsg/rules/concurrent/rule_007.py
+++ vsg/rules/concurrent/rule_007.py
@@ -22,47 +22,51 @@ class rule_007(rule.rule):
 
     def _analyze(self, oFile, oLine, iLineNumber):
         if not self.allow_single_line:
-            if oLine.insideConcurrent and re.match('^.*\selse\s+[\w|\']', oLine.lineNoComment.lower()):
-                sLine = oLine.lineNoComment.lower()
-                dViolation = utils.create_violation_dict(iLineNumber)
-                dViolation['slice_index'] = []
-                iStartIndex = 0
-                while sLine.find(' else', iStartIndex) > 0:
-                    iIndex = sLine.find(' else', iStartIndex) + len(' else')
-                    dViolation['slice_index'].append(iIndex)
-                    iStartIndex = iIndex
-                self.add_violation(dViolation)
+            sLine = oLine.lineNoComment.lower()
+            if oLine.insideConcurrent and re.match('^.*\selse\s+[\w|\']', sLine):
+                _update_violation(self, iLineNumber, sLine)
         if self.allow_single_line and oLine.isConcurrentBegin:
             sLine = oLine.lineNoComment.lower()
             if re.match('^.*\selse\s+.*\selse', sLine):
-                dViolation = utils.create_violation_dict(iLineNumber)
-                dViolation['slice_index'] = []
-                iStartIndex = 0
-                while sLine.find(' else', iStartIndex) > 0:
-                    iIndex = sLine.find(' else', iStartIndex) + len(' else')
-                    dViolation['slice_index'].append(iIndex)
-                    iStartIndex = iIndex
-                self.add_violation(dViolation)
+                _update_violation(self, iLineNumber, sLine)
 
     def _fix_violations(self, oFile):
         for dViolation in self.violations[::-1]:
             iLineNumber = dViolation['lineNumber']
             for iSliceIndex in dViolation['slice_index'][::-1]:
                 oLine = oFile.lines[iLineNumber]
-                sLine = oLine.line
-                oFile.lines.insert(iLineNumber + 1, copy.deepcopy(oLine))
-                utils.remove_comment_attributes_from_line(oLine)
-                oLine.isEndConcurrent = False
-                oLine.update_line(oLine.line[:iSliceIndex])
-                oLine = oFile.lines[iLineNumber + 1]
-                oLine.isConcurrentBegin = False
-                if iSliceIndex == dViolation['slice_index'][-1]:
-                    oLine.isEndConcurrent = True
-                else:
-                    oLine.isEndConcurrent = False
-                sLine = oLine.line[iSliceIndex:]
-                try:
-                    oLine.commentColumn = sLine.index('--')
-                except ValueError:
-                    oLine.commentColumn = None
-                oLine.update_line(sLine)
+                utils.copy_line(oFile, iLineNumber)
+                _modify_existing_line(oLine, iSliceIndex)
+                _modify_copied_line(oFile, iLineNumber, iSliceIndex, dViolation)
+
+
+def _update_violation(self, iLineNumber, sLine):
+    dViolation = utils.create_violation_dict(iLineNumber)
+    dViolation['slice_index'] = []
+    iStartIndex = 0
+    while sLine.find(' else', iStartIndex) > 0:
+        iIndex = sLine.find(' else', iStartIndex) + len(' else')
+        dViolation['slice_index'].append(iIndex)
+        iStartIndex = iIndex
+    self.add_violation(dViolation)
+
+
+def _modify_existing_line(oLine, iSliceIndex):
+    utils.remove_comment_attributes_from_line(oLine)
+    oLine.isEndConcurrent = False
+    oLine.update_line(oLine.line[:iSliceIndex])
+
+
+def _modify_copied_line(oFile, iLineNumber, iSliceIndex, dViolation):
+    oLine = oFile.lines[iLineNumber + 1]
+    oLine.isConcurrentBegin = False
+    if iSliceIndex == dViolation['slice_index'][-1]:
+        oLine.isEndConcurrent = True
+    else:
+        oLine.isEndConcurrent = False
+    sLine = oLine.line[iSliceIndex:]
+    try:
+        oLine.commentColumn = sLine.index('--')
+    except ValueError:
+        oLine.commentColumn = None
+    oLine.update_line(sLine)
