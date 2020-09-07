f257d5aa5df34660b3682df80d5aac8412ec483e
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-09-07 09:13:42 -0500

    Improving tests for unbounded_array_definition.

--- vsg/tests/unbounded_array_definition/classification_test_input.vhd
+++ vsg/tests/unbounded_array_definition/classification_test_input.vhd
@@ -3,6 +3,8 @@ architecture RTL of FIFO is
 
   type memory is array (natural range<>) of std_logic_vector(3 downto 0);
 
+  type memory is array (natural range<>, natural range <>) of std_logic_vector(3 downto 0);
+
 begin
 
 end architecture RTL;
