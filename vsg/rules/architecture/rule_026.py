b00d0a1bcc17135d447d26ac58efed660dfb7d4a
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-09-26 08:29:29 -0500

    Refactored rule architecture_026 to use a different base rule.

--- vsg/rules/architecture/rule_026.py
+++ vsg/rules/architecture/rule_026.py
@@ -1,14 +1,23 @@
 
-from vsg.rules import align_tokens_in_region_between_tokens
+from vsg.rules import align_tokens_in_region_between_tokens_unless_between_tokens
 
 from vsg import token
 
+lAlign = []
+lAlign.append(token.file_declaration.colon)
+lAlign.append(token.signal_declaration.colon)
+lAlign.append(token.constant_declaration.colon)
+lAlign.append(token.variable_declaration.colon)
 
-class rule_026(align_tokens_in_region_between_tokens):
+lUnless = []
+lUnless.append([token.subprogram_body.is_keyword,token.subprogram_body.begin_keyword])
+
+
+class rule_026(align_tokens_in_region_between_tokens_unless_between_tokens):
     '''
-    Architecture rule 025 checks for valid architecture names.
+    Architecture rule 026 aligns colons for file, signal, constant and variable declarations in the architecture_body.
     '''
 
     def __init__(self):
-        align_tokens_in_region_between_tokens.__init__(self, 'architecture', '026', [token.file_declaration.colon, token.signal_declaration.colon, token.constant_declaration.colon, token.variable_declaration.colon], token.architecture_body.is_keyword, token.architecture_body.begin_keyword)
+        align_tokens_in_region_between_tokens_unless_between_tokens.__init__(self, 'architecture', '026', lAlign, token.architecture_body.is_keyword, token.architecture_body.begin_keyword, lUnless)
         self.solution = 'Align identifer.'
