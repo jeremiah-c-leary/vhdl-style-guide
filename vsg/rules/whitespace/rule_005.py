8a6fe75b5f9ef159ba8928da3e0890eb2492379d
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-11-03 20:46:28 -0600

    Refactoring rule whitespace_005 for new parser.

--- vsg/rules/whitespace/rule_005.py
+++ vsg/rules/whitespace/rule_005.py
@@ -1,32 +1,58 @@
 
-from vsg import rule
-from vsg import utils
+from vsg import parser
+from vsg import rule_item
+from vsg import violation
 
-import re
+from vsg.vhdlFile import utils
+
+lTokens = []
+lTokens.append(parser.open_parenthesis)
 
 
-class rule_005(rule.rule):
-    '''Whitespace rule 005 checks for spaces after an open parenthesis.'''
+class rule_005(rule_item.Rule):
+    '''
+    Checks for spaces after an open parenthesis.
+    '''
 
     def __init__(self):
-        rule.rule.__init__(self, 'whitespace', '005')
-        self.phase = 2
+        rule_item.Rule.__init__(self, 'whitespace', '005')
         self.solution = 'Remove spaces after open (.'
+        self.phase = 2
+        self.iSpaces = 0
+        self.lTokens = [parser.open_parenthesis]
 
-    def _analyze(self, oFile, oLine, iLineNumber):
-        sLine = oLine.lineNoComment
-        if re.match('^.*\(\s+', sLine) and not re.match('^.*\(\s+[0-9]', sLine):
-            dViolation = utils.create_violation_dict(iLineNumber)
-            self.add_violation(dViolation)
+    def analyze(self, oFile):
+        lToi = []
 
-    def _fix_violations(self, oFile):
-        for dViolation in self.violations:
-            oLine = utils.get_violating_line(oFile, dViolation)
-            iCommentIndex = oLine.line.find('--')
-            if iCommentIndex == -1:
-                oLine.update_line(re.sub(r'\((\s+)([A-Za-z_\(])', r'(\2', oLine.line))
-            else:
-                sLine = oLine.line[:iCommentIndex]
-                sLine = re.sub(r'\((\s+)([A-Za-z_\(])', r'(\2', sLine)
-                sLine = sLine + oLine.line[iCommentIndex:]
-                oLine.update_line(sLine)
+        lToi = oFile.get_n_tokens_before_and_after_tokens(2, self.lTokens)
+
+        for oToi in lToi:
+            iLine, lTokens = utils.get_toi_parameters(oToi)
+            dAction = {}
+
+            oRight = lTokens[-2]
+            if isinstance(oRight, parser.whitespace):
+                if not utils.token_is_whitespace_or_comment(lTokens[-1]):
+                    if not lTokens[-1].get_value().isnumeric():
+                        oViolation = violation.New(iLine, oToi, self.solution)
+                        self.add_violation(oViolation)
+
+
+    def fix(self, oFile):
+        '''
+        Applies fixes for any rule violations.
+        '''
+        if self.fixable:
+            self.analyze(oFile)
+            self._print_debug_message('Fixing rule: ' + self.name + '_' + self.identifier)
+            self._fix_violation(oFile)
+            self.violations = []
+
+    def _fix_violation(self, oFile):
+        for oViolation in self.violations:
+            lTokens = oViolation.get_tokens()
+            oToken = lTokens.pop()
+            lTokens.pop()
+            lTokens.append(oToken)
+            oViolation.set_tokens(lTokens)
+        oFile.update(self.violations)
--- vsg/tests/whitespace/test_rule_whitespace.py
+++ vsg/tests/whitespace/test_rule_whitespace.py
@@ -15,34 +15,6 @@ class testRuleWhitespaceMethods(unittest.TestCase):
         self.lFile = utils.read_vhdlfile(sFileName)
         self.oFile = vhdlFile.vhdlFile(self.lFile)
 
-    def test_005(self):
-        oRule = whitespace.rule_005()
-        self.assertTrue(oRule)
-        self.assertEqual(oRule.name, 'whitespace')
-        self.assertEqual(oRule.identifier, '005')
-        self.assertEqual(oRule.phase, 2)
-
-        dExpected = utils.add_violation_list([1,3,5])
-        self.oFile.lines.append(line.line('  This is a test of parenthesis ( failure'))
-        self.oFile.lines.append(line.line('  This is a test of parenthesis (pass'))
-        self.oFile.lines.append(line.line('  This is a test of parentehsis (  failure'))
-        self.oFile.lines.append(line.line('  This is a test of parentehsis (  7 pass'))
-        self.oFile.lines.append(line.line('  This is a test of parenthesis ( pass) --  ( pass'))
-        self.oFile.lines.append(line.line(' --  This is a test of parenthesis ( pass)'))
-        self.oFile.lines[5].hasComment = True
-        self.oFile.lines[6].isComment = True
-        oRule.analyze(self.oFile)
-        self.assertEqual(oRule.violations, dExpected)
-        oRule.fix(self.oFile)
-        oRule.analyze(self.oFile)
-        self.assertEqual(oRule.violations, [])
-        self.assertEqual(self.oFile.lines[1].line, '  This is a test of parenthesis (failure')
-        self.assertEqual(self.oFile.lines[2].line, '  This is a test of parenthesis (pass')
-        self.assertEqual(self.oFile.lines[3].line, '  This is a test of parentehsis (failure')
-        self.assertEqual(self.oFile.lines[4].line, '  This is a test of parentehsis (  7 pass')
-        self.assertEqual(self.oFile.lines[5].line, '  This is a test of parenthesis (pass) --  ( pass')
-        self.assertEqual(self.oFile.lines[6].line, ' --  This is a test of parenthesis ( pass)')
-
 
 
     def test_011(self):
