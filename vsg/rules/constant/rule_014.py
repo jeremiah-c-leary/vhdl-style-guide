619a0a8b3498248392f36669549af8170e836cb5
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-11-08 13:19:54 -0600

    Refactoring rule constant_014 for new parser.

--- vsg/rules/constant/rule_014.py
+++ vsg/rules/constant/rule_014.py
@@ -1,43 +1,145 @@
 
-import re
+import copy
 
-from vsg import rule
-from vsg import utils
+from vsg import rule_item
+from vsg import parser
+from vsg import token
+from vsg import violation
+
+from vsg.vhdlFile import utils
 
 
-class rule_014(rule.rule):
+lTokenPairs = []
+lTokenPairs.append([token.constant_declaration.assignment_operator, token.constant_declaration.semicolon])
+
+
+class rule_014(rule_item.Rule):
     '''
-    Constant rule 014 checks the indent of multiline constants that are not arrays.
+    Checks the indent of multiline constants that are not arrays.
+
+    Parameters
+    ----------
+
+    name : string
+       The group the rule belongs to.
+
+    identifier : string
+       unique identifier.  Usually in the form of 00N.
+
+    trigger : parser object type
+       object type to apply the case check against
     '''
 
     def __init__(self):
-        rule.rule.__init__(self)
-        self.name = 'constant'
-        self.identifier = '014'
-        self.solution = 'Align with := keyword on constant declaration line.'
+        rule_item.Rule.__init__(self, 'constant', '014')
+        self.solution = 'Align one space after assignment operator'
         self.phase = 5
+        self.lTokenPairs = lTokenPairs
+        self.bExcludeLastToken = False
 
-    def _pre_analyze(self):
-        self.alignmentColumn = 0
-        self.fKeywordFound = False
+    def analyze(self, oFile):
+        lToi = []
+        for lTokenPair in self.lTokenPairs:
+            aToi = oFile.get_tokens_bounded_by(lTokenPair[0], lTokenPair[1], bExcludeLastToken=self.bExcludeLastToken)
+            lToi = utils.combine_two_token_class_lists(lToi, aToi)
 
-    def _analyze(self, oFile, oLine, iLineNumber):
-        if not oLine.isConstantArray and oLine.insideConstant:
-            if oLine.isConstant and ':=' in oLine.line:
-                self.alignmentColumn = oLine.line.index(':=') + len(':= ')
-                self.fKeywordFound = True
-            elif not oLine.isConstant and self.fKeywordFound:
-                sMatch = ' ' * self.alignmentColumn
-                if not re.match('^' + sMatch + '\w', oLine.line):
-                    dViolation = utils.create_violation_dict(iLineNumber)
-                    dViolation['column'] = self.alignmentColumn
-                    self.add_violation(dViolation)
-            if oLine.isConstantEnd:
-                self.fKeywordFound = False
+        for oToi in lToi:
 
-    def _fix_violations(self, oFile):
-        for dViolation in self.violations:
-            iLineNumber = utils.get_violation_line_number(dViolation)
-            sLine = oFile.lines[iLineNumber].line
-            sNewLine = ' ' * dViolation['column'] + sLine.strip()
-            oFile.lines[iLineNumber].update_line(sNewLine)
+            iLine, lTokens = utils.get_toi_parameters(oToi)
+
+            if utils.are_next_consecutive_token_types_ignoring_whitespace([parser.open_parenthesis], 1, lTokens):
+                continue
+
+            iStartColumn = calculate_start_column(oFile, oToi)
+            lColumn = []
+            lColumn.append(iStartColumn)
+            bCheckAlignment = False
+            iFirstColumn = oFile.get_column_of_token_index(oToi.get_start_index())
+            iColumn = iFirstColumn
+            iPreviousColumn = 0
+            iIndent = 0
+#            print('-'*80)
+            for iToken, oToken in enumerate(lTokens):
+
+                iLine = utils.increment_line_number(iLine, oToken)
+
+                if isinstance(oToken, parser.carriage_return):
+                    bCheckAlignment = True
+                    iPreviousColumn = lColumn[-1]
+                    iColumn = 0
+                    if isinstance(lTokens[iToken + 1], parser.whitespace):
+                        iIndent = len(lTokens[iToken + 1].get_value())
+                    else:
+                        iIndent = 0
+                    continue
+
+                if isinstance(oToken, parser.blank_line):
+                    bCheckAlignment = False
+                    continue
+
+                iColumn += len(oToken.get_value())
+
+                if isinstance(oToken, parser.open_parenthesis):
+                    lTemp = lColumn.copy()
+                    lColumn.append(iColumn + iPreviousColumn - iIndent)
+#                    print(f'{iColumn} | {iPreviousColumn} | {iIndent} | {lTemp} | {lColumn}')
+
+                if isinstance(oToken, parser.close_parenthesis):
+                    lColumn.pop()
+
+                if bCheckAlignment:
+                    if isinstance(oToken, parser.whitespace):
+                        if len(oToken.get_value()) != lColumn[-1]:
+                            dAction = {}
+                            dAction['line'] = iLine
+                            dAction['column'] = lColumn[-1]
+                            dAction['action'] = 'adjust'
+                            dAction['indent'] = iIndent
+                            dAction['previous'] = iPreviousColumn
+                            oViolation = violation.New(iLine, oToi.extract_tokens(iToken, iToken), self.solution)
+                            oViolation.set_action(dAction)
+                            self.add_violation(oViolation)
+#                            print(dAction)
+                    else:
+                        if lColumn != 0:
+                            dAction = {}
+                            dAction['column'] = lColumn[-1]
+                            dAction['action'] = 'insert'
+                            oViolation = violation.New(iLine, oToi.extract_tokens(iToken, iToken), self.solution)
+                            oViolation.set_action(dAction)
+                            self.add_violation(oViolation)
+                    bCheckAlignment = False
+
+
+    def fix(self, oFile):
+        '''
+        Applies fixes for any rule violations.
+        '''
+        if self.fixable:
+            self.analyze(oFile)
+            self._print_debug_message('Fixing rule: ' + self.name + '_' + self.identifier)
+            self._fix_violation(oFile)
+            self.violations = []
+
+    def _fix_violation(self, oFile):
+        for oViolation in self.violations:
+            lTokens = oViolation.get_tokens()
+            dAction = oViolation.get_action()
+
+            if dAction['action'] == 'adjust':
+                lTokens[0].set_value(' '*dAction['column'])
+            else:
+                lTokens.insert(0, parser.whitespace(' '*dAction['column']))
+
+            oViolation.set_tokens(lTokens)
+               
+        oFile.update(self.violations)
+
+
+def calculate_start_column(oFile, oToi):
+    iReturn = oFile.get_column_of_token_index(oToi.get_start_index())
+    iReturn += len(oToi.get_tokens()[0].get_value())
+    iReturn += 1
+    return iReturn
+
+
