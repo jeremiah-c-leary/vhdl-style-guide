51614d367a362409f143649be6e776c82b24e92e
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-11-10 18:12:48 -0600

    Removing old code from previous parser.

--- vsg/rules/variable_assignment/rule_005.py
+++ vsg/rules/variable_assignment/rule_005.py
@@ -1,27 +1,4 @@
 
-from vsg.rules import keyword_alignment_rule
-
-class rule_005(keyword_alignment_rule):
-    '''
-    Variable assignment rule 005 ensures the alignment of the ":=" keyword over multiple lines.
-    '''
-
-    def __init__(self):
-        keyword_alignment_rule.__init__(self, 'variable_assignment', '005')
-        self.solution = 'Inconsistent alignment of ":=" in group of lines.'
-        self.sKeyword = ':='
-        self.sStartGroupTrigger = 'isProcessBegin'
-        self.sEndGroupTrigger = 'isEndProcess'
-        self.lLineTriggers = ['isVariableAssignment']
-
-        self.if_control_statements_end_group = True
-        self.configuration.append('if_control_statements_end_group')
-
-        self.configuration_triggers += [{'name': 'if_control_statements_end_group', 'triggers': ['isIfKeyword',
-                                                                                                 'isElseIfKeyword',
-                                                                                                 'isElseKeyword',
-                                                                                                 'isEndIfKeyword']}]
-
 from vsg.rules import align_tokens_in_region_between_tokens_unless_between_tokens
 
 from vsg import token
