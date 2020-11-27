d41c520e5b8d78a8755258c01f7c9b81ae5413f5
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-11-27 10:13:00 -0600

    Refactoring rule sequential_002 to use single_space_after base rule.

--- vsg/rules/sequential/rule_002.py
+++ vsg/rules/sequential/rule_002.py
@@ -2,21 +2,22 @@
 from vsg import parser
 from vsg import token
 
-from vsg.rules import single_space_between_token_pairs
+from vsg.rules import single_space_after_token
 
 lTokens = []
-lTokens.append([token.simple_force_assignment.assignment, token.simple_force_assignment.force_keyword])
-lTokens.append([token.simple_release_assignment.assignment, token.simple_release_assignment.release_keyword])
-lTokens.append([token.simple_waveform_assignment.assignment, token.delay_mechanism.transport_keyword])
-lTokens.append([token.simple_waveform_assignment.assignment, token.delay_mechanism.reject_keyword])
-lTokens.append([token.simple_waveform_assignment.assignment, token.delay_mechanism.inertial_keyword])
-lTokens.append([token.simple_waveform_assignment.assignment, parser.todo])
+lTokens.append(token.simple_waveform_assignment.assignment)
+lTokens.append(token.simple_force_assignment.assignment)
+lTokens.append(token.simple_release_assignment.assignment)
+lTokens.append(token.selected_waveform_assignment.assignment)
+lTokens.append(token.selected_force_assignment.assignment)
+lTokens.append(token.conditional_waveform_assignment.assignment)
+lTokens.append(token.conditional_force_assignment.assignment)
 
 
-class rule_002(single_space_between_token_pairs):
+class rule_002(single_space_after_token):
     '''
-    Checks for a single space after the <=.
+    Checks for at least a single space after the <=.
     '''
     def __init__(self):
-        single_space_between_token_pairs.__init__(self, 'sequential', '002', lTokens)
-        self.solution = 'Ensure a single space after the <=.'
+        single_space_after_token.__init__(self, 'sequential', '002', lTokens)
+        self.solution = 'Ensure at least a single space after the <=.'
