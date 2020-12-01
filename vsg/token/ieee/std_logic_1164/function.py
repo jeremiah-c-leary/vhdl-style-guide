3095211e093ec14a97e85fe73c6b6198e69546b3
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-12-01 08:59:23 -0600

    Adding unique_id docstring to ieee tokens.

--- vsg/token/ieee/std_logic_1164/function.py
+++ vsg/token/ieee/std_logic_1164/function.py
@@ -3,12 +3,18 @@ from vsg import parser
 
 
 class rising_edge(parser.function):
+    '''
+    unique_id = ieee_std_logic_1164_function : rising_edge
+    '''
 
     def __init__(self, sString):
         parser.function.__init__(self, sString)
 
 
 class falling_edge(parser.function):
+    '''
+    unique_id = ieee_std_logic_1164_function : falling_edge
+    '''
 
     def __init__(self, sString):
         parser.function.__init__(self, sString)
--- vsg/token/ieee/std_logic_1164/types.py
+++ vsg/token/ieee/std_logic_1164/types.py
@@ -3,18 +3,27 @@ from vsg import parser
 
 
 class std_logic_vector(parser.type):
+    '''
+    unique_id = ieee_std_logic_1164_types : std_logic_vector
+    '''
 
     def __init__(self, sString):
         parser.type.__init__(self, sString)
 
 
 class std_ulogic_vector(parser.type):
+    '''
+    unique_id = ieee_std_logic_1164_types : std_ulogic_vector
+    '''
 
     def __init__(self, sString):
         parser.type.__init__(self, sString)
 
 
 class std_ulogic(parser.type):
+    '''
+    unique_id = ieee_std_logic_1164_types : std_ulogic
+    '''
 
     def __init__(self, sString):
         parser.type.__init__(self, sString)
