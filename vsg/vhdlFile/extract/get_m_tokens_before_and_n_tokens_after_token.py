267e62ea5bbbc31b119f6c82b90b88a41f3c89d8
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-12-01 09:03:32 -0600

    Refactoring extract function to improve readability.

--- vsg/vhdlFile/extract/get_m_tokens_before_and_n_tokens_after_token.py
+++ vsg/vhdlFile/extract/get_m_tokens_before_and_n_tokens_after_token.py
@@ -1,29 +1,18 @@
 
-from vsg import parser
-
-from vsg.vhdlFile import vhdlFile as utils
+from vsg.vhdlFile.extract import tokens
+from vsg.vhdlFile.extract import utils
 
 
 def get_m_tokens_before_and_n_tokens_after_token(iM, iN, lTokens, lAllTokens, oTokenMap):
     lReturn = []
 
-    lIndexes = get_indexes_of_token_list(lTokens, oTokenMap)
+    lIndexes = utils.get_indexes_of_token_list(lTokens, oTokenMap)
 
     for iIndex in lIndexes:
         iLine = oTokenMap.get_line_number_of_index(iIndex)
         iStart = iIndex - iM
         iEnd = iIndex + iN
         if iStart >= 0:
-            lReturn.append(utils.Tokens(iStart, iLine, lAllTokens[iStart:iEnd + 1]))
-
-    return lReturn
-
-
-def get_indexes_of_token_list(lTokens, oTokenMap):
-    lReturn = []
-    for oToken in lTokens:
-        lReturn.extend(oTokenMap.get_token_indexes(oToken))
-
-    lReturn.sort()
+            lReturn.append(tokens.New(iStart, iLine, lAllTokens[iStart:iEnd + 1]))
 
     return lReturn
--- vsg/vhdlFile/extract/utils.py
+++ vsg/vhdlFile/extract/utils.py
@@ -8,3 +8,13 @@ def is_token_at_start_of_line(iIndex, oTokenMap):
     if oTokenMap.is_token_at_index(parser.carriage_return, iIndex - 2) and oTokenMap.is_token_at_index(parser.whitespace, iIndex - 1):
         return True
     return False
+
+
+def get_indexes_of_token_list(lTokens, oTokenMap):
+    lReturn = []
+    for oToken in lTokens:
+        lReturn.extend(oTokenMap.get_token_indexes(oToken))
+
+    lReturn.sort()
+
+    return lReturn
