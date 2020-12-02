89b336fbcf9749708cadaedc0cd805123a95a354
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-12-01 19:05:47 -0600

    Switching to local token class.

--- vsg/vhdlFile/extract/get_all_tokens.py
+++ vsg/vhdlFile/extract/get_all_tokens.py
@@ -1,7 +1,7 @@
 
-from vsg.vhdlFile import vhdlFile as utils
+from vsg.vhdlFile.extract import tokens
 
 
 def get_all_tokens(lAllTokens):
 
-    return utils.Tokens(0, 1, lAllTokens)
+    return tokens.New(0, 1, lAllTokens)
