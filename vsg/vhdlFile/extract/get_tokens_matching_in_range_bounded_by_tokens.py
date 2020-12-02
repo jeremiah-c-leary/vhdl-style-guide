1627ca74557358532d76bd8e1adb8a290b130689
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-12-01 19:25:00 -0600

    Refactored extract function to use token map.

--- vsg/vhdlFile/extract/get_tokens_matching_in_range_bounded_by_tokens.py
+++ vsg/vhdlFile/extract/get_tokens_matching_in_range_bounded_by_tokens.py
@@ -5,20 +5,19 @@ from vsg.vhdlFile.extract import tokens
 
 
 def get_tokens_matching_in_range_bounded_by_tokens(lTokens, oStart, oEnd, lAllTokens, oTokenMap):
-        iLine = 1
-        lReturn = []
-        bSearch = False
-        for iIndex in range(0, len(lAllTokens)):
-            if isinstance(lAllTokens[iIndex], oStart):
-                bSearch = True
-            if isinstance(lAllTokens[iIndex], oEnd):
-                bSearch = False
-            if bSearch:
-                for oToken in lTokens:
-                    if isinstance(lAllTokens[iIndex], oToken):
-                        lReturn.append(tokens.New(iIndex, iLine, [lAllTokens[iIndex]]))
 
-            if isinstance(lAllTokens[iIndex], parser.carriage_return):
-                iLine +=1
+        lStart, lEnd = oTokenMap.get_token_pair_indexes(oStart, oEnd)
+
+        lIndexes = []
+        for iStart, iEnd in zip(lStart, lEnd):
+            for oToken in lTokens:
+                lIndexes.extend(oTokenMap.get_token_indexes_between_indexes(oToken, iStart, iEnd))
+
+        lIndexes.sort()
+
+        lReturn = []
+        for iIndex in lIndexes:
+            iLine = oTokenMap.get_line_number_of_index(iIndex)
+            lReturn.append(tokens.New(iIndex, iLine, [lAllTokens[iIndex]]))
 
         return lReturn
