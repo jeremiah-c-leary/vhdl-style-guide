1b1b59c055c0048015b855a0e179eeaffa3f1a8d
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2020-12-15 10:51:30 -0600

    Adding script to output parser results without having to setup a test.

---
+++ vsg/__parser__.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import os
+import glob
+
+from . import vhdlFile
+
+from vsg.tests import utils
+
+
+def parse_command_line_arguments():
+    '''Parses the command line arguments and returns them.'''
+
+    parser = argparse.ArgumentParser(
+      prog='VHDL Style Guide (VSG) Parser',
+      description='''Outputs the results from parsing a VHDL file.''')
+
+    parser.add_argument('-f', '--filename', help='File to print parser output')
+    parser.add_argument('-w', '--whitespace', default=False, action='store_true', help='Include whitespace objects')
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    else:
+        return parser.parse_args()
+
+
+def read_vhdlfile(sFileName):
+    try:
+        lLines = []
+        with open(sFileName) as oFile:
+            for sLine in oFile:
+                lLines.append(sLine)
+        oFile.close()
+        return lLines
+    except UnicodeDecodeError:
+        lLines = []
+        with open(sFileName, encoding="ISO-8859-1") as oFile:
+            for sLine in oFile:
+                lLines.append(sLine)
+        oFile.close()
+        return lLines
+
+    except IOError:
+        return []
+
+
+def main():
+    '''Main routine of parser output'''
+
+    fExitStatus = 0
+
+    commandLineArguments = parse_command_line_arguments()
+
+    sFileName = commandLineArguments.filename
+
+    oVhdlFile = vhdlFile.vhdlFile(read_vhdlfile(sFileName))
+    oVhdlFile.filename = sFileName
+
+    utils.print_objects(oVhdlFile, not commandLineArguments.whitespace)
+
+    sys.exit(fExitStatus)
+
+
+if __name__ == '__parser__':
+    main()
