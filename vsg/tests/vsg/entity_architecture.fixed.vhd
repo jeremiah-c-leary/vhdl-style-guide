56c24073ab3c675def1269d1f4f1f6cfe7589a62
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-01-23 15:44:38 -0600

    Adding missing file.

---
+++ vsg/tests/vsg/entity_architecture.fixed.vhd
@@ -0,0 +1,17 @@
+
+entity entity1 is
+  port (
+    port1 : in    std_logic;
+    port2 : out   std_logic;
+    port3 : inout std_logic;
+    port4 : in    std_logic;
+    port5 : out   std_logic;
+    port6 : inout std_logic
+  );
+end entity entity1;
+
+architecture arch of entity1 is
+
+begin
+
+end architecture arch;
