6b134f56fb956d9f07acd75de4cd23a6443de8be
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-02-08 19:52:52 -0600

    Updates for issue 486:
    Updating version so it does not error out if git does not exist.

--- vsg/version.py
+++ vsg/version.py
@@ -23,24 +23,30 @@ def print_version(oCommandLineArguments, sShaNum=sShaNum):
         exit(0)
 
 def get_git_sha(sVersion, sShaNum):
-    if sShaNum is None and sVersion is None:
+    try:
+        if sShaNum is None and sVersion is None:
+            sReturnPath = os.getcwd()
+            sPath = os.path.dirname(__file__)
+            os.chdir(sPath)
+            lActual = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+            lActual = str(lActual.decode('utf-8')).split('\n')
+            sReturn = lActual[0]
+            os.chdir(sReturnPath)
+        else:
+            return sShaNum
+        return sReturn
+    except subprocess.CalledProcessError:
+        return 'Unknown'
+
+def get_git_branch():
+    try:
         sReturnPath = os.getcwd()
         sPath = os.path.dirname(__file__)
         os.chdir(sPath)
-        lActual = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+        lActual = subprocess.check_output(['git', 'branch', '--show-current'])
         lActual = str(lActual.decode('utf-8')).split('\n')
         sReturn = lActual[0]
         os.chdir(sReturnPath)
-    else:
-        return sShaNum
-    return sReturn
-
-def get_git_branch():
-    sReturnPath = os.getcwd()
-    sPath = os.path.dirname(__file__)
-    os.chdir(sPath)
-    lActual = subprocess.check_output(['git', 'branch', '--show-current'])
-    lActual = str(lActual.decode('utf-8')).split('\n')
-    sReturn = lActual[0]
-    os.chdir(sReturnPath)
-    return sReturn
+        return sReturn
+    except subprocess.CalledProcessError:
+        return 'Unknown'
