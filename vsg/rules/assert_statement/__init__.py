95cb88bd3a766adfffdffae141bf69691b77a1e8
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-03-31 08:02:57 -0500

    Updates for issue #548
    Adding rules to move assertion report and severity keywords to their own line.
    
      1)  Updated documentation
      2)  Added tests
      3)  Added rules

--- vsg/rules/assert_statement/__init__.py
+++ vsg/rules/assert_statement/__init__.py
@@ -1,4 +1,8 @@
 
 from .rule_001 import rule_001
+from .rule_002 import rule_002
+from .rule_003 import rule_003
+from .rule_004 import rule_004
+from .rule_005 import rule_005
 
 from .rule_400 import rule_400
