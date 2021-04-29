29539967c17f4d2a604e6e2312a372b488b0ef6a
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-04-29 10:41:09 -0500

    Updates for issue #565
    Adding loop_statement rule to rule set.

--- vsg/rules/__init__.py
+++ vsg/rules/__init__.py
@@ -100,6 +100,7 @@ from vsg.rules import if_statement
 from vsg.rules import instantiation
 from vsg.rules import length
 from vsg.rules import library
+from vsg.rules import loop_statement
 from vsg.rules import package
 from vsg.rules import package_body
 from vsg.rules import port
