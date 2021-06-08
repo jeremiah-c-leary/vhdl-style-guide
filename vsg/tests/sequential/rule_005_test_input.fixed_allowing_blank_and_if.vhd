37e012f2e035db94d27677deb992a8d538438feb
imd <ian.davis@roke.co.uk>
2021-06-08 22:40:43 +0100

    improved sequential_006 tests

--- vsg/tests/sequential/rule_005_test_input.fixed_allowing_blank_and_if.vhd
+++ vsg/tests/sequential/rule_005_test_input.fixed_allowing_blank_and_if.vhd
@@ -6,18 +6,18 @@ begin
   process
   begin
 
-    sig1 <= sig2;
-    sig2 <= sig3;
-    sig2 <= sig3;
+    sig1          <= sig2;
+    sig2          <= sig3;
+    sig2          <= sig3;
 
     if a = b then
-      sig3 <= sig4;
-      sig4 <= sig5;
-      sig4 <= sig5;
+      sig3        <= sig4;
+      sig4        <= sig5;
+      sig4        <= sig5;
       if c = d then
-        sig6 <= sig7;
-        sig7 <= sig8;
-        sig7 <= sig8;
+        sig6      <= sig7;
+        sig7      <= sig8;
+        sig7      <= sig8;
       end if;
     end if;
 
@@ -45,17 +45,17 @@ begin
   process
   begin
 
-    sig1<= sig2;
-    sig2   <= sig3;
-    sig2    <= sig3;
+    sig1     <= sig2;
+    sig2     <= sig3;
+    sig2     <= sig3;
 
     if a = b then
-      sig3    <= sig4;
-      sig4<= sig5;
-      sig4  <= sig5;
+      sig3   <= sig4;
+      sig4   <= sig5;
+      sig4   <= sig5;
       if c = d then
-        sig6    <= sig7;
-        sig7  <= sig8;
+        sig6 <= sig7;
+        sig7 <= sig8;
         sig7 <= sig8;
       end if;
     end if;
@@ -63,9 +63,9 @@ begin
     case address is
 
       when 0 =>
-        sig5<= sig6;
-        sig6<= sig7;
-        sig6     <= sig7;
+        sig5 <= sig6;
+        sig6 <= sig7;
+        sig6 <= sig7;
 
     end case;
 
