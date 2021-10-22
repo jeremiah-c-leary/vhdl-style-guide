099e637011c7ed61fe49d37a3b6392a1cc8e7076
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-10-22 06:52:52 -0500

    Updates for issue #639
    Addressing issue where function and procedure rules were identical.
    
      1)  Updated documentation
          a)  Added documentation for subprogram_body rules
          b)  Updated deprecated rule documentation
      2)  Updated tests
          a)  Removed deprecated rule tests
          b)  Added subprogram_body rules
          c)  Updated function and procedure rules to show the issue
      3)  Updated rules to ensure function rules operate on functions and procedure rules operate on procedures
          a)  function_013, procedure_008
          b)  function_015, procedure_010
          c)  function_012, procedure_401
          d)  function_502, procedure_502
          e)  function_004, procedure_503
          f)  function_013, procedure_504
          g)  function_506, procedure_506
      4)  Moved function and procedure rules to subprogram_body rules
          a)  function_201, procedure_201 --> subprogram_body_201
          b)  function_202, procedure_202 --> subprogram_body_202
          c)  function_203, procedure_203 --> subprogram_body_203
          d)  function_204, procedure_204 --> subprogram_body_204
          e)  function_007, procedure_205 --> subprogram_body_205
      5)  Deprecated rules
          a)  function_201, procedure_201
          b)  function_202, procedure_202
          c)  function_203, procedure_203
          d)  function_204, procedure_204
          e)  function_007, procedure_205

---
+++ vsg/vhdlFile/extract/get_tokens_bounded_by_unless_between.py
@@ -0,0 +1,80 @@
+
+from vsg import parser
+
+from vsg.vhdlFile.extract import tokens
+
+
+def get_tokens_bounded_by_unless_between(oStart, oEnd, lUnless, lAllObjects, oTokenMap, include_trailing_whitespace=False, bExcludeLastToken=False, bIncludeTillEndOfLine=False, bIncludeTillBeginningOfLine=False):
+
+#    oTokenMap.pretty_print()
+    lReturn = []
+
+    lStart, lEnd = oTokenMap.get_token_pair_indexes(oStart, oEnd)
+
+    lUnlessStart, lUnlessEnd = get_unless_token_indexes(lUnless, oTokenMap)
+
+    lStart, lEnd = filter_token_pairs(lStart, lEnd, lUnlessStart, lUnlessEnd)
+
+    lCarriageReturns = oTokenMap.get_token_indexes(parser.carriage_return)
+
+    lNewStart = []
+    if bIncludeTillBeginningOfLine:
+        for iStart in lStart:
+            lNewStart.append(oTokenMap.get_index_of_carriage_return_before_index(iStart) + 1)
+    else:
+        lNewStart = lStart
+
+    lNewEnd = []
+    if bIncludeTillEndOfLine:
+        for iEnd in lEnd:
+            lNewEnd.append(oTokenMap.get_index_of_carriage_return_after_index(iEnd))
+    else:
+        lNewEnd = lEnd
+
+    if bExcludeLastToken:
+        lNewEnd = [x - 1 for x in lNewEnd]
+
+    lWhiteSpace = oTokenMap.get_token_indexes(parser.whitespace)
+    if include_trailing_whitespace:
+        for iNewEnd, iIndex in enumerate(lNewEnd):
+            if iIndex + 1 in lWhiteSpace:
+                lNewEnd[iNewEnd] +=1
+    elif not bIncludeTillEndOfLine:
+        for iNewEnd, iIndex in enumerate(lNewEnd):
+            if iIndex in lWhiteSpace:
+                lNewEnd[iNewEnd] -= 1
+            if lNewEnd[iNewEnd] in lCarriageReturns:
+                lNewEnd[iNewEnd] -= 1
+
+    for iStart, iEnd, iIndex in zip(lNewStart, lNewEnd, lEnd):
+        lTemp = lAllObjects[iStart: iEnd + 1]
+        iStartLine = oTokenMap.get_line_number_of_index(iStart)
+        oToi = tokens.New(iStart, iStartLine, lTemp)
+        oToi.set_token_value(iIndex - iStart)
+        lReturn.append(oToi)
+
+    return lReturn
+
+
+def get_unless_token_indexes(lUnless, oTokenMap):
+    lReturnStart = []
+    lReturnEnd = []
+    for unless in lUnless:
+        lStart, lEnd = oTokenMap.get_token_pair_indexes(unless[0], unless[1])
+        lReturnStart.extend(lStart)
+        lReturnEnd.extend(lEnd)
+    return lReturnStart, lReturnEnd
+
+
+def filter_token_pairs(lStart, lEnd, lUnlessStart, lUnlessEnd):
+    lReturnStart = []
+    lReturnEnd = []
+    for iStart, iEnd in zip(lStart, lEnd):
+        for iUnlessStart, iUnlessEnd in zip(lUnlessStart, lUnlessEnd):
+            if iUnlessStart < iStart < iUnlessEnd and \
+               iUnlessStart < iEnd < iUnlessEnd:
+                   break;
+        else:
+            lReturnStart.append(iStart)
+            lReturnEnd.append(iEnd)
+    return lReturnStart, lReturnEnd
