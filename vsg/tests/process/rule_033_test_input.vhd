5ed551cf530d0acc11c86d95f53cffa0a055dd80
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-11-08 06:05:16 -0600

    Updates for issue #664 (#666)
    * Updates for issue #664
    
    Colons in subprogram_declarative_parts were being aligned with process_declarative_parts.
    
      1)  Updated tests to expose the issue
      2)  Updated rule process_033 to exclude subprogram_declarative_parts
    
    * Fixing codeclimate issue.

--- vsg/tests/process/rule_033_test_input.vhd
+++ vsg/tests/process/rule_033_test_input.vhd
@@ -24,5 +24,20 @@ begin
   begin
   end process;
 
+  process
+
+    procedure some_procedure (count : integer) is
+
+      variable v_count      : std_logic_vector(SOME_CONSTANT - 1 downto 0);
+
+    begin
+
+    end procedure;
+
+    variable var1  : integer := 0;
+    file     file1 : load_file_file open read_mode is load_file_name;
+    constant con1  : std_logic := '1';
+
+  begin end process;
 
 end architecture RTL;
