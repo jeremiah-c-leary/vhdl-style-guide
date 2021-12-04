e1b19065b56c48ab791082a79bb4edc273de61b1
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-12-04 08:24:23 -0600

    Updates for issue #691 (#692)
    * Updates for issue #691
    
    A conditional variable assignment was being classified as a conditional waveform assignment.
    
      1)  Added to test to expose the issue
      2)  sequential_statement checks for variable assignments before waveform assignments
    
    * Updates for issue #691
    
    Fixing unrelated test that occasionally fails due to order of file analysis.

--- vsg/tests/vsg/test_main.py
+++ vsg/tests/vsg/test_main.py
@@ -408,6 +408,9 @@ class testMain(unittest.TestCase):
         # Read in another alternate expected JSON file for comparison
         lAlternateExpected2 = []
         utils.read_file(os.path.join(os.path.dirname(__file__),'json-expected.alternate2.json'), lAlternateExpected2)
+        # Read in another alternate expected JSON file for comparison
+        lAlternateExpected3 = []
+        utils.read_file(os.path.join(os.path.dirname(__file__),'json-expected.alternate3.json'), lAlternateExpected3)
         # Read in the actual JSON file for comparison
         lActual = []
         utils.read_file(os.path.join('deleteme.json'), lActual)
@@ -420,6 +423,8 @@ class testMain(unittest.TestCase):
             self.assertEqual(lActual, lAlternateExpected)
         elif lActual == lAlternateExpected2:
             self.assertEqual(lActual, lAlternateExpected2)
+        elif lActual == lAlternateExpected3:
+            self.assertEqual(lActual, lAlternateExpected3)
         else:
             self.assertEqual(lActual, lAlternateExpected2)
 
