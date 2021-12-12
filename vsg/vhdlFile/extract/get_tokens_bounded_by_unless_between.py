2eaaf93c45ef16a60ef432b738136f4a12dab463
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2021-12-12 05:54:03 -0600

    Refactoring get_tokens_bounded_by_unless_betweent to improve readability.

--- vsg/vhdlFile/extract/get_tokens_bounded_by_unless_between.py
+++ vsg/vhdlFile/extract/get_tokens_bounded_by_unless_between.py
@@ -14,26 +14,38 @@ def get_tokens_bounded_by_unless_between(oStart, oEnd, lUnless, lAllObjects, oTo
 
     lStart, lEnd = filter_token_pairs(lStart, lEnd, lUnlessStart, lUnlessEnd)
 
-    lCarriageReturns = oTokenMap.get_token_indexes(parser.carriage_return)
+    lNewStart = adjust_start_token(bIncludeTillBeginningOfLine, lStart, oTokenMap)
 
-    lNewStart = []
+    lNewEnd = adjust_end_token(bIncludeTillEndOfLine, bExcludeLastToken, include_trailing_whitespace, lEnd, oTokenMap)
+
+    for iStart, iEnd, iIndex in zip(lNewStart, lNewEnd, lEnd):
+        lTemp = lAllObjects[iStart: iEnd + 1]
+        iStartLine = oTokenMap.get_line_number_of_index(iStart)
+        oToi = tokens.New(iStart, iStartLine, lTemp)
+        oToi.set_token_value(iIndex - iStart)
+        lReturn.append(oToi)
+
+    return lReturn
+
+
+def adjust_start_token(bIncludeTillBeginningOfLine, lStart, oTokenMap):
+    lReturn = []
     if bIncludeTillBeginningOfLine:
         for iStart in lStart:
-            lNewStart.append(oTokenMap.get_index_of_carriage_return_before_index(iStart) + 1)
+            lReturn.append(oTokenMap.get_index_of_carriage_return_before_index(iStart) + 1)
     else:
-        lNewStart = lStart
+        lReturn = lStart
+    return lReturn
 
-    lNewEnd = []
-    if bIncludeTillEndOfLine:
-        for iEnd in lEnd:
-            lNewEnd.append(oTokenMap.get_index_of_carriage_return_after_index(iEnd))
-    else:
-        lNewEnd = lEnd
 
-    if bExcludeLastToken:
-        lNewEnd = [x - 1 for x in lNewEnd]
+def adjust_end_token(bIncludeTillEndOfLine, bExcludeLastToken, include_trailing_whitespace, lEnd, oTokenMap):
+    lNewEnd = include_end_of_line(bIncludeTillEndOfLine, lEnd, oTokenMap)
+    lNewEnd = exclude_last_token(bExcludeLastToken, lNewEnd)
+
+    lCarriageReturns = oTokenMap.get_token_indexes(parser.carriage_return)
 
     lWhiteSpace = oTokenMap.get_token_indexes(parser.whitespace)
+
     if include_trailing_whitespace:
         for iNewEnd, iIndex in enumerate(lNewEnd):
             if iIndex + 1 in lWhiteSpace:
@@ -45,14 +57,25 @@ def get_tokens_bounded_by_unless_between(oStart, oEnd, lUnless, lAllObjects, oTo
             if lNewEnd[iNewEnd] in lCarriageReturns:
                 lNewEnd[iNewEnd] -= 1
 
-    for iStart, iEnd, iIndex in zip(lNewStart, lNewEnd, lEnd):
-        lTemp = lAllObjects[iStart: iEnd + 1]
-        iStartLine = oTokenMap.get_line_number_of_index(iStart)
-        oToi = tokens.New(iStart, iStartLine, lTemp)
-        oToi.set_token_value(iIndex - iStart)
-        lReturn.append(oToi)
 
+    return lNewEnd
+
+
+def include_end_of_line(bIncludeTillEndOfLine, lEnd, oTokenMap):
+    lReturn = []
+    if bIncludeTillEndOfLine:
+        for iEnd in lEnd:
+            lReturn.append(oTokenMap.get_index_of_carriage_return_after_index(iEnd))
+    else:
+        lReturn = lEnd
     return lReturn
+
+
+def exclude_last_token(bExcludeLastToken, lNewEnd):
+    if bExcludeLastToken:
+        return [x - 1 for x in lNewEnd]
+    else:
+        return lNewEnd
 
 
 def get_unless_token_indexes(lUnless, oTokenMap):
