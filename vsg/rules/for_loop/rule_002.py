aa163ba59572f70d54cf4bc89b61fbc42e18e117
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-01-29 07:27:02 -0600

    Fixing solution text.

--- vsg/rules/for_loop/rule_002.py
+++ vsg/rules/for_loop/rule_002.py
@@ -9,4 +9,4 @@ class rule_002(deprecated_rule.Rule):
 
     def __init__(self):
         deprecated_rule.Rule.__init__(self, 'for_loop', '002')
-        self.message.append('Rule ' + self.unique_id + ' move been moved to loop_statement_302.')
+        self.message.append('Rule ' + self.unique_id + ' has been moved to loop_statement_302.')
