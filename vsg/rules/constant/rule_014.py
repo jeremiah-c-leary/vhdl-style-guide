4fd8e2da0948d09b6c270e72be50250a5790ad37
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-01-31 11:08:52 -0600

    Updates for issue #705:
    Changing rule constant_014 to include align_left and align_parent options.

--- vsg/rules/constant/rule_014.py
+++ vsg/rules/constant/rule_014.py
@@ -1,20 +1,19 @@
 
+from vsg.rules import multiline_alignment_between_tokens as Rule
+
 from vsg import parser
 from vsg import token
-from vsg import violation
-
-from vsg.rules import utils as rules_utils
-from vsg.rule_group import alignment
 from vsg.vhdlFile import utils
-
 
 lTokenPairs = []
 lTokenPairs.append([token.constant_declaration.assignment_operator, token.constant_declaration.semicolon])
 
 
-class rule_014(alignment.Rule):
+class rule_014(Rule):
     '''
     This rule checks the indent of multiline constants that do not contain arrays.
+
+    Refer to the section `Configuring Multiline Indent Rules <configuring_multiline_indent_rules.html>`_ for more information.
 
     **Violation**
 
@@ -32,102 +31,29 @@ class rule_014(alignment.Rule):
     '''
 
     def __init__(self):
-        alignment.Rule.__init__(self, 'constant', '014')
-        self.solution = 'Align one space after assignment operator'
-        self.subphase = 4
-        self.lTokenPairs = lTokenPairs
-        self.bExcludeLastToken = False
+        Rule.__init__(self, 'constant', '014', lTokenPairs)
+        self.subphase = 3
+        self.phase = 5
 
-    def analyze(self, oFile):
-        lToi = []
+    def _get_tokens_of_interest(self, oFile):
         for lTokenPair in self.lTokenPairs:
-            aToi = oFile.get_tokens_bounded_by(lTokenPair[0], lTokenPair[1], bExcludeLastToken=self.bExcludeLastToken)
-            lToi = utils.combine_two_token_class_lists(lToi, aToi)
-
-        for oToi in lToi:
-
-            iLine, lTokens = utils.get_toi_parameters(oToi)
-
-            if utils.are_next_consecutive_token_types_ignoring_whitespace([parser.open_parenthesis], 1, lTokens):
-                continue
-
-            iStartColumn = calculate_start_column(oFile, oToi)
-            lColumn = []
-            lColumn.append(iStartColumn)
-            bCheckAlignment = False
-            iFirstColumn = oFile.get_column_of_token_index(oToi.get_start_index())
-            iColumn = iFirstColumn
-            iPreviousColumn = 0
-            iIndent = 0
-#            print('-'*80)
-            for iToken, oToken in enumerate(lTokens):
-
-                iLine = utils.increment_line_number(iLine, oToken)
-
-                if isinstance(oToken, parser.carriage_return):
-                    bCheckAlignment = True
-                    iPreviousColumn = lColumn[-1]
-                    iColumn = 0
-                    if isinstance(lTokens[iToken + 1], parser.whitespace):
-                        iIndent = len(lTokens[iToken + 1].get_value())
-                    else:
-                        iIndent = 0
-                    continue
-
-                if isinstance(oToken, parser.blank_line):
-                    bCheckAlignment = False
-                    continue
-
-                iColumn += len(oToken.get_value())
-
-                if isinstance(oToken, parser.open_parenthesis):
-                    lColumn.append(iColumn + iPreviousColumn - iIndent)
-
-                if isinstance(oToken, parser.close_parenthesis):
-                    lColumn.pop()
-
-                if bCheckAlignment:
-                    if isinstance(oToken, parser.whitespace):
-                        if len(oToken.get_value()) != lColumn[-1]:
-                            dAction = {}
-                            dAction['line'] = iLine
-                            dAction['column'] = lColumn[-1]
-                            dAction['action'] = 'adjust'
-                            dAction['indent'] = iIndent
-                            dAction['previous'] = iPreviousColumn
-                            oViolation = violation.New(iLine, oToi.extract_tokens(iToken, iToken), self.solution)
-                            oViolation.set_action(dAction)
-                            self.add_violation(oViolation)
-#                            print(dAction)
-                    else:
-                        if lColumn != 0:
-                            dAction = {}
-                            if isinstance(oToken, parser.open_parenthesis):
-                                dAction['column'] = lColumn[-2]
-                            else:
-                                dAction['column'] = lColumn[-1]
-                            dAction['action'] = 'insert'
-                            oViolation = violation.New(iLine, oToi.extract_tokens(iToken, iToken), self.solution)
-                            oViolation.set_action(dAction)
-                            self.add_violation(oViolation)
-                    bCheckAlignment = False
-
-    def _fix_violation(self, oViolation):
-        lTokens = oViolation.get_tokens()
-        dAction = oViolation.get_action()
-
-        if dAction['action'] == 'adjust':
-            lTokens[0].set_value(' '*dAction['column'])
-        else:
-            rules_utils.insert_whitespace(lTokens, 0, dAction['column'])
-
-        oViolation.set_tokens(lTokens)
+            lToi = oFile.get_tokens_bounded_by(lTokenPair[0], lTokenPair[1])
+            lToi = remove_arrays(lToi)
+        return lToi
 
 
-def calculate_start_column(oFile, oToi):
-    iReturn = oFile.get_column_of_token_index(oToi.get_start_index())
-    iReturn += len(oToi.get_tokens()[0].get_value())
-    iReturn += 1
-    return iReturn
+def remove_arrays(lToi):
+    lReturn = []
+    for oToi in lToi:
+        lTokens = oToi.get_tokens()
+        if not starts_with_paren(lTokens):
+            lReturn.append(oToi)
+    return lReturn
 
 
+def starts_with_paren(lTokens):
+
+    iToken = utils.find_next_non_whitespace_token(1, lTokens)
+    if isinstance(lTokens[iToken], parser.open_parenthesis):
+        return True
+    return False
--- vsg/tests/constant/test_rule_014.py
+++ vsg/tests/constant/test_rule_014.py
@@ -14,6 +14,10 @@ lExpected = []
 lExpected.append('')
 utils.read_file(os.path.join(sTestDir, 'rule_014_test_input.fixed.vhd'), lExpected)
 
+lExpected_align_left_true_align_paren_false = []
+lExpected_align_left_true_align_paren_false.append('')
+utils.read_file(os.path.join(sTestDir, 'rule_014_test_input.fixed_align_left_true_align_paren_false.vhd'), lExpected_align_left_true_align_paren_false)
+
 
 class test_constant_rule(unittest.TestCase):
 
@@ -41,6 +45,30 @@ class test_constant_rule(unittest.TestCase):
         lActual = self.oFile.get_lines()
 
         self.assertEqual(lExpected, lActual)
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
+
+    def test_rule_014_align_left_true_align_paren_false(self):
+        oRule = constant.rule_014()
+        oRule.align_left = "yes"
+        oRule.align_paren = "no"
+
+        lExpected = [7, 10, 31, 34, 37]
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
+
+    def test_fix_rule_014_align_left_true_align_paren_false(self):
+        oRule = constant.rule_014()
+        oRule.align_left = "yes"
+        oRule.align_paren = "no"
+
+        oRule.fix(self.oFile)
+
+        lActual = self.oFile.get_lines()
+
+        self.assertEqual(lExpected_align_left_true_align_paren_false, lActual)
 
         oRule.analyze(self.oFile)
         self.assertEqual(oRule.violations, [])
