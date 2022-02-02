5de1a483b8518b3b8611b34ef20ce3a87963c572
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-02-02 17:03:56 -0600

    Adding rule alias_declaration_102.

--- vsg/rules/alias_declaration/__init__.py
+++ vsg/rules/alias_declaration/__init__.py
@@ -2,6 +2,7 @@ from .rule_001 import rule_001
 
 from .rule_100 import rule_100
 from .rule_101 import rule_101
+from .rule_102 import rule_102
 
 from .rule_500 import rule_500
 from .rule_501 import rule_501
