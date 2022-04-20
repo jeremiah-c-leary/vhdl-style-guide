b448eb93196facdf8f4fd5427ebd87a06327e2cd
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-04-19 19:22:21 -0500

    Updating rule library_500 to allow case exceptions.

--- vsg/rules/library/rule_500.py
+++ vsg/rules/library/rule_500.py
@@ -33,3 +33,4 @@ class rule_500(token_case):
     def __init__(self):
         token_case.__init__(self, 'library', '500', lTokens)
         self.groups.append('case::name')
+        self.configuration.append('case_exceptions')
