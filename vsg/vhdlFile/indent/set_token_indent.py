815d1a72672b59b5bb07d6c8b5c3ceff0682d3a1
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-04-22 07:43:51 -0500

    Refactoring to remove number of parameters passed to functions.

--- vsg/vhdlFile/indent/set_token_indent.py
+++ vsg/vhdlFile/indent/set_token_indent.py
@@ -7,12 +7,9 @@ from vsg.vhdlFile import utils
 
 def set_token_indent(dIndentMap, lTokens):
     lTokenKeys, dIndents = process_indent_map(dIndentMap)
-
-    iIndent = 0
-    bLibraryFound = False
-    bArchitectureFound = False
-    dVars = {}
-    dVars['insideConcurrentSignalAssignment'] = False
+    cParams = parameters()
+    cParams.lTokenKeys = lTokenKeys
+    cParams.dIndents = dIndents
 
     for iToken, oToken in enumerate(lTokens):
 
@@ -20,7 +17,7 @@ def set_token_indent(dIndentMap, lTokens):
             continue
 
         if isinstance(oToken, parser.blank_line):
-            bLibraryFound = False
+            cParams.bLibraryFound = False
             continue
 
         if isinstance(oToken, parser.carriage_return):
@@ -30,92 +27,92 @@ def set_token_indent(dIndentMap, lTokens):
 
         iTokenIndent = None
 
-        if sUniqueId in lTokenKeys:
-            token_key = dIndents[sUniqueId]['token']
-            after_key = dIndents[sUniqueId]['after']
-            iTokenIndent = update_indent_var(iIndent, token_key)
+        if sUniqueId in cParams.lTokenKeys:
+            token_key = cParams.dIndents[sUniqueId]['token']
+            after_key = cParams.dIndents[sUniqueId]['after']
+            iTokenIndent = update_indent_var(cParams.iIndent, token_key)
             oToken.set_indent(iTokenIndent)
-            iIndent = update_indent_var(iIndent, after_key)
+            cParams.iIndent = update_indent_var(cParams.iIndent, after_key)
 
         if isinstance(oToken, token.context_declaration.end_keyword):
-            bLibraryFound = False
+            cParams.bLibraryFound = False
             continue
 
         if isinstance(oToken, token.library_clause.keyword):
-            bLibraryFound = True
+            cParams.bLibraryFound = True
             continue
 
         if isinstance(oToken, token.use_clause.keyword):
-            if not bArchitectureFound:
-                oToken.set_indent(iIndent + 1)
+            if not cParams.bArchitectureFound:
+                oToken.set_indent(cParams.iIndent + 1)
             else:
-                oToken.set_indent(iIndent)
+                oToken.set_indent(cParams.iIndent)
             continue
 
         if isinstance(oToken, token.context_reference.keyword):
-            if bLibraryFound:
-                oToken.set_indent(iIndent + 1)
+            if cParams.bLibraryFound:
+                oToken.set_indent(cParams.iIndent + 1)
             else:
-                oToken.set_indent(iIndent)
+                oToken.set_indent(cParams.iIndent)
             continue
 
         if isinstance(oToken, token.architecture_body.architecture_keyword):
-            bLibraryFound = False
-            bArchitectureFound = True
+            cParams.bLibraryFound = False
+            cParams.bArchitectureFound = True
             continue
 
         if isinstance(oToken, token.architecture_body.semicolon):
-            bArchitectureFound = False
+            cParams.bArchitectureFound = False
             continue
 
         if isinstance(oToken, token.entity_declaration.entity_keyword):
-            bLibraryFound = False
+            cParams.bLibraryFound = False
             continue
 
         if isinstance(oToken, token.package_body.package_keyword):
-            bLibraryFound = False
+            cParams.bLibraryFound = False
             continue
 
         if isinstance(oToken, token.package_declaration.package_keyword):
-            bLibraryFound = False
+            cParams.bLibraryFound = False
             continue
 
         ### Comments
         if isinstance(oToken, parser.comment):
-            set_indent_of_comment(bLibraryFound, iToken, lTokens, iIndent, lTokenKeys, dIndents)
+            set_indent_of_comment(cParams, iToken, lTokens)
             continue
 
         ### Concurrent signal assignment
         if isinstance(oToken, token.concurrent_signal_assignment_statement.label_name):
-            dVars['insideConcurrentSignalAssignment'] = True
+            cParams.insideConcurrentSignalAssignment = True
             continue
 
         if isinstance(oToken, token.concurrent_signal_assignment_statement.postponed_keyword):
-            dVars['insideConcurrentSignalAssignment'] = True
+            cParams.insideConcurrentSignalAssignment = True
             continue
 
         if isinstance(oToken, token.concurrent_simple_signal_assignment.target):
-            dVars['insideConcurrentSignalAssignment'] = True
+            cParams.insideConcurrentSignalAssignment = True
             continue
 
         if isinstance(oToken, token.concurrent_conditional_signal_assignment.target):
-            dVars['insideConcurrentSignalAssignment'] = True
+            cParams.insideConcurrentSignalAssignment = True
             continue
 
         if isinstance(oToken, token.concurrent_selected_signal_assignment.with_keyword):
-            dVars['insideConcurrentSignalAssignment'] = True
+            cParams.insideConcurrentSignalAssignment = True
             continue
 
         if isinstance(oToken, token.concurrent_simple_signal_assignment.semicolon):
-            dVars['insideConcurrentSignalAssignment'] = False
+            cParams.insideConcurrentSignalAssignment = False
             continue
 
         if isinstance(oToken, token.concurrent_conditional_signal_assignment.semicolon):
-            dVars['insideConcurrentSignalAssignment'] = False
+            cParams.insideConcurrentSignalAssignment = False
             continue
 
         if isinstance(oToken, token.concurrent_selected_signal_assignment.semicolon):
-            dVars['insideConcurrentSignalAssignment'] = False
+            cParams.insideConcurrentSignalAssignment = False
             continue
 
         oToken.set_indent(iTokenIndent)
@@ -154,28 +151,39 @@ def is_use_clause_use_keyword_next(iIndex, lTokens):
     return False
 
 
-def set_indent_of_comment(bLibraryFound, iToken, lTokens, iIndent, lTokenKeys, dIndents):
+def set_indent_of_comment(cParams, iToken, lTokens):
     oToken = lTokens[iToken]
-    if bLibraryFound:
+    if cParams.bLibraryFound:
         if is_use_clause_use_keyword_next(iToken + 1, lTokens):
-            oToken.set_indent(iIndent + 1)
+            oToken.set_indent(cParams.iIndent + 1)
         else:
-            oToken.set_indent(iIndent)
+            oToken.set_indent(cParams.iIndent)
     elif oToken.is_block_comment:
         if oToken.block_comment_indent == 0:
             oToken.set_indent(0)
         else:
-            oToken.set_indent(iIndent)
+            oToken.set_indent(cParams.iIndent)
     else:
-        iTemp = get_indent_value_of_next_token(iToken, lTokens, iIndent, lTokenKeys, dIndents)
+        iTemp = get_indent_value_of_next_token(iToken, lTokens, cParams)
         oToken.set_indent(iTemp)
 
 
-def get_indent_value_of_next_token(iToken, lTokens, iIndent, lTokenKeys, dIndents):
+def get_indent_value_of_next_token(iToken, lTokens, cParams):
     iIndex = utils.find_next_non_whitespace_token(iToken + 1, lTokens)
     sUniqueId = lTokens[iIndex].get_unique_id(sJoin=':')
-    if sUniqueId in lTokenKeys:
-        token_key = dIndents[sUniqueId]['token']
-        iTokenIndent = update_indent_var(iIndent, token_key)
+    if sUniqueId in cParams.lTokenKeys:
+        token_key = cParams.dIndents[sUniqueId]['token']
+        iTokenIndent = update_indent_var(cParams.iIndent, token_key)
         return iTokenIndent
-    return iIndent
+    return cParams.iIndent
+
+
+class parameters():
+
+    def __init__(self):
+        self.lTokenKeys = None
+        self.dIndents = None
+        self.bLibraryFound = False
+        self.bArchitectureFound = False
+        self.insideConcurrentSignalAssignment = False
+        self.iIndent = 0
