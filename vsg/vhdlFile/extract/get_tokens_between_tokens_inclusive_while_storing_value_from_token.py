e47114e8a935fe4eaf2e2c13e5e27515006d689d
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-04-27 20:12:35 -0500

    Updates for issue #761
    Fixing issue with rule block_007 not honoring nested blocks with respect to labels.
    
      1)  Updated tests to expose the issue
      2)  Updated extractor to honor nested constructs

--- vsg/vhdlFile/extract/get_tokens_between_tokens_inclusive_while_storing_value_from_token.py
+++ vsg/vhdlFile/extract/get_tokens_between_tokens_inclusive_while_storing_value_from_token.py
@@ -9,30 +9,42 @@ def get_tokens_between_tokens_inclusive_while_storing_value_from_token(left_toke
     lReturn = []
 
     lStart, lEnd = oTokenMap.get_token_pair_indexes(left_token, right_token)
-
+#    print(lStart)
     lValueIndexes = oTokenMap.get_token_indexes(value_token)
-
+#    print(lValueIndexes)
     iPreviousEnd = 0
+    lValues = []
+    lPopedValues = []
     for iStart, iEnd in zip(lStart, lEnd):
-        sValue = None
+#        sValue = None
+        push_value_index(lValueIndexes, lValues, lPopedValues, iStart) 
+#        print('='*80)
+#        print(f'iStart = {iStart}')
+#        print(f'lValues = {lValues}')
+#        print(f'lPopedValues = {lPopedValues}')
 
-        iValueIndex = bisect.bisect_left(lValueIndexes, iEnd) - 1
-        if iValueIndex >= 0:
-            iValue = lValueIndexes[iValueIndex]
-
-            oValueToken = lAllTokens[iValue]
-            if iValue < iPreviousEnd:
-                sValue = None
-            else:
-                sValue = oValueToken.get_value()
-        else:
+        try:
+            oValueToken = lAllTokens[lValues[-1]]
+            sValue = oValueToken.get_value()
+        except IndexError:
             sValue = None
-
+#        print(f'sValue = {sValue}')
+ 
         iLine = oTokenMap.get_line_number_of_index(iStart)
 
         oTokens = tokens.New(iStart, iLine, lAllTokens[iStart:iEnd + 1])
         oTokens.set_token_value(sValue)
         lReturn.append(oTokens)
         iPreviousEnd = iEnd
+        try:
+            lPopedValues.append(lValues.pop(-1))
+        except IndexError:
+            pass
 
     return lReturn
+
+
+def push_value_index(lValueIndexes, lValues, lPopedValues, iLeftIndex):
+    for iIndex, iValue in enumerate(lValueIndexes):
+        if iLeftIndex > iValue and iValue not in lPopedValues and iValue not in lValues:
+            lValues.append(iValue)
