4d449e820c1cd9168a3967fd86189ac2402d93e7
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-04-28 08:21:11 -0500

    Issue 761 (#765)
    * Updates for issue #761
    
    Fixing issue with rule block_007 not honoring nested blocks with respect to labels.
    
      1)  Updated tests to expose the issue
      2)  Updated extractor to honor nested constructs
    
    * Refactoring to improve readability.
    
    * Removing trailing whitespace.
    
    * Removing unused variable.

--- vsg/tests/block/rule_007_test_input.fixed_remove.vhd
+++ vsg/tests/block/rule_007_test_input.fixed_remove.vhd
@@ -5,8 +5,22 @@ begin
 
   BLOCK_LABEL : block is begin end block;
 
+  BLOCK_LABEL_1 : block is begin
+
+    BLOCK_LABEL_2 : block is begin
+    end block;
+
+  end block;
+
   -- Violations below
 
   BLOCK_LABEL : block is begin end block;
+
+  BLOCK_LABEL_1 : block is begin
+
+    BLOCK_LABEL_2 : block is begin
+    end block;
+
+  end block;
 
 end architecture RTL;
