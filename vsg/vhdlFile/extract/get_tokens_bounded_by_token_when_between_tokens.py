e4226681bc2b70bf10dea3956ddb149beec4cb44
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-04-30 08:27:41 -0500

    Updates for issue #762 (#766)
    * Updates for issue #762
    
    Fixing issue with nested generates and rule generate_403.
    
      1)  Updated tests to expose the issue
      2)  Updated token_map.get_token_pair_indexes to honor nesting
    
    * Removing debug code.
    
    * Refactoring method get_token_pair_indexes to improve readability.
    
    * Refactoring to reduce complexity.
    
    * Fixing nested pair logic to properly extract pairs within pairs.
    
    * Refactoring to reduce complexity.
    
    * Formatting updates.
    
    * Refactoring to improve readability.

--- vsg/vhdlFile/extract/get_tokens_bounded_by_token_when_between_tokens.py
+++ vsg/vhdlFile/extract/get_tokens_bounded_by_token_when_between_tokens.py
@@ -6,18 +6,51 @@ from vsg.vhdlFile.extract import tokens
 
 def get_tokens_bounded_by_token_when_between_tokens(oLeft, oRight, oStart, oEnd, lAllTokens, oTokenMap, include_trailing_whitespace=False):
 
-    lLeft, lRight = oTokenMap.get_token_pair_indexes(oLeft, oRight)
-    lStart, lEnd = oTokenMap.get_token_pair_indexes(oStart, oEnd)
+    lMyPairs = get_token_pairs([oLeft, oRight], [oStart, oEnd], oTokenMap)
+
     lReturn = []
+
+    for lPair in lMyPairs:
+        iLeft = lPair[0]
+        iRight = lPair[1]
+        iLine = oTokenMap.get_line_number_of_index(iLeft)
+        if include_trailing_whitespace:
+            if oTokenMap.is_token_at_index(iRight + 1, parser.whitespace):
+                lReturn.append(tokens.New(iLeft, iLine, lAllTokens[iLeft:iRight + 2]))
+            else:
+                lReturn.append(tokens.New(iLeft, iLine, lAllTokens[iLeft:iRight + 1]))
+        else:
+            lReturn.append(tokens.New(iLeft, iLine, lAllTokens[iLeft:iRight + 1]))
+
+    return lReturn
+
+
+def get_token_pairs(lInnerPair, lOuterPair, oTokenMap):
+    lLeft, lRight = oTokenMap.get_token_pair_indexes(lInnerPair[0], lInnerPair[1])
+    lStart, lEnd = oTokenMap.get_token_pair_indexes(lOuterPair[0], lOuterPair[1])
+
+    lPairs = keep_pairs_that_are_within_pairs(lLeft, lRight, lStart, lEnd)
+
+    lReturn = remove_duplicate_pairs(lPairs)
+    return lReturn
+
+
+def keep_pairs_that_are_within_pairs(lLeft, lRight, lStart, lEnd):
+    lPairs = []
     for iStart, iEnd in zip(lStart, lEnd):
-        for iLeft, iRight in zip(lLeft, lRight):
-            if iStart < iLeft and iRight < iEnd:
-                iLine = oTokenMap.get_line_number_of_index(iLeft)
-                if include_trailing_whitespace:
-                    if oTokenMap.is_token_at_index(iRight + 1, parser.whitespace):
-                        lReturn.append(tokens.New(iLeft, iLine, lAllTokens[iLeft:iRight + 2]))
-                    else:
-                        lReturn.append(tokens.New(iLeft, iLine, lAllTokens[iLeft:iRight + 1]))
-                else:
-                    lReturn.append(tokens.New(iLeft, iLine, lAllTokens[iLeft:iRight + 1]))
+        find_inner_pairs_inside_outer_pairs([iStart, iEnd], lLeft, lRight, lPairs)
+    return lPairs
+
+
+def find_inner_pairs_inside_outer_pairs(lOuterPair, lLeft, lRight, lPairs):
+    for iLeft, iRight in zip(lLeft, lRight):
+        if lOuterPair[0] < iLeft and iRight < lOuterPair[1]:
+            lPairs.append([iLeft, iRight])
+
+
+def remove_duplicate_pairs(lPairs):
+    lReturn = []
+    for lPair in lPairs:
+        if lPair not in lReturn:
+            lReturn.append(lPair)
     return lReturn
