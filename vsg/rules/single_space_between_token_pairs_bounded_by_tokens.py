bad07458b84d669424a5769372e649a364861d5b
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-05-14 13:49:36 -0500

    Making single_space_between_token_pairs_bounded_by_tokens extend whitespace_between_tokens.

--- vsg/rules/single_space_between_token_pairs_bounded_by_tokens.py
+++ vsg/rules/single_space_between_token_pairs_bounded_by_tokens.py
@@ -1,15 +1,13 @@
 
 
 from vsg import parser
-from vsg import violation
 
-from vsg.rule_group import whitespace
-from vsg.rules import utils as rules_utils
+from vsg.rules.whitespace_between_tokens import Rule
 
 from vsg.vhdlFile import utils
 
 
-class single_space_between_token_pairs_bounded_by_tokens(whitespace.Rule):
+class single_space_between_token_pairs_bounded_by_tokens(Rule):
     '''
     Checks for a single space between two tokens.
 
@@ -27,7 +25,7 @@ class single_space_between_token_pairs_bounded_by_tokens(whitespace.Rule):
     '''
 
     def __init__(self, name, identifier, lTokens, oStart, oEnd):
-        whitespace.Rule.__init__(self, name=name, identifier=identifier)
+        Rule.__init__(self, name=name, identifier=identifier)
         self.lTokens = lTokens
         self.oStart = oStart
         self.oEnd = oEnd
@@ -40,19 +38,3 @@ class single_space_between_token_pairs_bounded_by_tokens(whitespace.Rule):
             lToi_a = oFile.get_sequence_of_tokens_matching_bounded_by_tokens(lTokenPair, self.oStart, self.oEnd)
             lToi = utils.combine_two_token_class_lists(lToi, lToi_a)
         return lToi
-
-    def _analyze(self, lToi):
-        for oToi in lToi:
-            lTokens = oToi.get_tokens()
-            if len(lTokens) == 2:
-                self.add_violation(violation.New(oToi.get_line_number(), oToi, self.solution))
-            elif len(lTokens[1].get_value()) != 1:
-                self.add_violation(violation.New(oToi.get_line_number(), oToi, self.solution))
-
-    def _fix_violation(self, oViolation):
-        lTokens = oViolation.get_tokens()
-        if isinstance(lTokens[1], parser.whitespace):
-            lTokens[1].set_value(' ')
-        else:
-            rules_utils.insert_whitespace(lTokens, 1)
-        oViolation.set_tokens(lTokens)
