48d13184416ee653ffe5a2589bb28a961f7e7252
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-05-29 17:40:30 -0500

    Refactoring to improve readability.

--- vsg/vhdlFile/hierarchy.py
+++ vsg/vhdlFile/hierarchy.py
@@ -5,33 +5,62 @@ from vsg import token
 def set_token_hierarchy_value(lTokens):
     lIfHierarchy = []
     for oToken in lTokens:
-        if isinstance(oToken, token.if_statement.if_keyword):
-            if len(lIfHierarchy) == 0:
-                lIfHierarchy.append(0)
-            oToken.set_hierarchy(lIfHierarchy[-1])
-            lIfHierarchy[-1] += 1
-        if isinstance(oToken, token.if_statement.elsif_keyword):
-            oToken.set_hierarchy(lIfHierarchy[-1] - 1)
-        if isinstance(oToken, token.if_statement.else_keyword):
-            oToken.set_hierarchy(lIfHierarchy[-1] - 1)
-        if isinstance(oToken, token.if_statement.semicolon):
-            lIfHierarchy[-1] -= 1
-            oToken.set_hierarchy(lIfHierarchy[-1])
+        check_if_statement(oToken, lIfHierarchy)
+        check_case_statement(oToken, lIfHierarchy)
+        check_loop_statement(oToken, lIfHierarchy)
+
+
+def check_case_statement(oToken, lIfHierarchy):
+    check_for_hierarchy_nesting(oToken, lIfHierarchy, token.case_statement.case_keyword, token.case_statement.semicolon)
+
+
+def check_loop_statement(oToken, lIfHierarchy):
+    check_for_hierarchy_nesting(oToken, lIfHierarchy, token.loop_statement.loop_keyword, token.loop_statement.semicolon)
+
+
+def check_for_hierarchy_nesting(oToken, lIfHierarchy, oStartToken, oEndToken):
+    if tokens_match(oToken, oStartToken):
+        lIfHierarchy.append(0)
+    if tokens_match(oToken, oEndToken):
+        if len(lIfHierarchy) > 0:
             if lIfHierarchy[-1] == 0:
                 lIfHierarchy.pop()
-        if isinstance(oToken, token.case_statement.case_keyword):
+
+
+def check_if_statement(oToken, lIfHierarchy):
+    set_if_keyword_hierarchy(oToken, lIfHierarchy)
+    set_elsif_keyword_hierarchy(oToken, lIfHierarchy)
+    set_else_keyword_hierarchy(oToken, lIfHierarchy)
+    set_if_semicolon_hierarchy(oToken, lIfHierarchy)
+
+
+def set_if_keyword_hierarchy(oToken, lIfHierarchy):
+    if tokens_match(oToken, token.if_statement.if_keyword):
+        if len(lIfHierarchy) == 0:
             lIfHierarchy.append(0)
-        if isinstance(oToken, token.case_statement.semicolon):
-            try:
-                if lIfHierarchy[-1] == 0:
-                    lIfHierarchy.pop()
-            except IndexError:
-                pass
-        if isinstance(oToken, token.loop_statement.loop_keyword):
-            lIfHierarchy.append(0)
-        if isinstance(oToken, token.loop_statement.semicolon):
-            try:
-                if lIfHierarchy[-1] == 0:
-                    lIfHierarchy.pop()
-            except IndexError:
-                pass
+        oToken.set_hierarchy(lIfHierarchy[-1])
+        lIfHierarchy[-1] += 1
+
+
+def set_elsif_keyword_hierarchy(oToken, lIfHierarchy):
+    if tokens_match(oToken, token.if_statement.elsif_keyword):
+        oToken.set_hierarchy(lIfHierarchy[-1] - 1)
+
+
+def set_else_keyword_hierarchy(oToken, lIfHierarchy):
+    if tokens_match(oToken, token.if_statement.else_keyword):
+        oToken.set_hierarchy(lIfHierarchy[-1] - 1)
+
+
+def set_if_semicolon_hierarchy(oToken, lIfHierarchy):
+    if tokens_match(oToken, token.if_statement.semicolon):
+        lIfHierarchy[-1] -= 1
+        oToken.set_hierarchy(lIfHierarchy[-1])
+        if lIfHierarchy[-1] == 0:
+            lIfHierarchy.pop()
+
+
+def tokens_match(oToken1, oToken2):
+    if isinstance(oToken1, oToken2):
+        return True
+    return False
