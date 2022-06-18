2a5735a7629c4d3937a96e4e7711b3a98d898035
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-06-17 21:07:03 -0500

    Changing the following to use subtype_indication.classify:
      constrained_array_definition

--- vsg/vhdlFile/classify/constrained_array_definition.py
+++ vsg/vhdlFile/classify/constrained_array_definition.py
@@ -30,6 +30,6 @@ def classify(iToken, lObjects):
 
     iCurrent = utils.assign_next_token_required('of', token.of_keyword, iCurrent, lObjects)
 
-    iCurrent = subtype_indication.classify_until([';'], iCurrent, lObjects)
+    iCurrent = subtype_indication.classify(iCurrent, lObjects)
 
     return iCurrent
