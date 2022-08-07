ba411e66105d8bdc4e5a54f95f95b2c3b60419a0
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-08-07 10:10:54 -0500

    Adding smart_tabs test for rule assert_statement_001

--- vsg/tests/assert_statement/test_rule_001.py
+++ vsg/tests/assert_statement/test_rule_001.py
@@ -14,12 +14,16 @@ dIndentMap = utils.read_indent_file()
 
 dIndentMap = utils.read_indent_file()
 
-lExpected = []
-lExpected.append('')
-utils.read_file(os.path.join(sTestDir, 'rule_001_test_input.fixed.vhd'), lExpected)
+lExpected_spaces = []
+lExpected_spaces.append('')
+utils.read_file(os.path.join(sTestDir, 'rule_001_test_input.fixed_spaces.vhd'), lExpected_spaces)
+
+lExpected_smart_tabs = []
+lExpected_smart_tabs.append('')
+utils.read_file(os.path.join(sTestDir, 'rule_001_test_input.fixed_smart_tabs.vhd'), lExpected_smart_tabs)
 
 
-class test_assert_rule(unittest.TestCase):
+class test_rule(unittest.TestCase):
 
     def setUp(self):
         self.oFile = vhdlFile.vhdlFile(lFile)
@@ -27,8 +31,9 @@ class test_assert_rule(unittest.TestCase):
         self.oFile.set_indent_map(dIndentMap)
         self.oFile.set_indent_map(dIndentMap)
 
-    def test_rule_001(self):
+    def test_rule_001_spaces(self):
         oRule = assert_statement.rule_001()
+        oRule.indentStyle = 'spaces'
         self.assertTrue(oRule)
         self.assertEqual(oRule.name, 'assert')
         self.assertEqual(oRule.identifier, '001')
@@ -39,14 +44,38 @@ class test_assert_rule(unittest.TestCase):
         oRule.analyze(self.oFile)
         self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
 
-    def test_fix_rule_001(self):
+    def test_fix_rule_001_spaces(self):
         oRule = assert_statement.rule_001()
+        oRule.indentStyle = 'spaces'
 
         oRule.fix(self.oFile)
 
         lActual = self.oFile.get_lines()
 
-        self.assertEqual(lExpected, lActual)
+        self.assertEqual(lExpected_spaces, lActual)
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
+
+    def test_rule_001_smart_tabs(self):
+        oRule = assert_statement.rule_001()
+        oRule.indentStyle = 'smart_tabs'
+        self.assertTrue(oRule)
+
+        lExpected = [7, 9, 10, 12, 13, 15, 16, 17, 19, 21, 22, 24, 25, 27, 28, 29, 32, 34, 35, 37, 38, 40, 41, 42, 44, 46, 47, 49, 50, 52, 53, 54]
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
+
+    def test_fix_rule_001_smart_tabs(self):
+        oRule = assert_statement.rule_001()
+        oRule.indentStyle = 'smart_tabs'
+
+        oRule.fix(self.oFile)
+
+        lActual = self.oFile.get_lines()
+
+        self.assertEqual(lExpected_smart_tabs, lActual)
 
         oRule.analyze(self.oFile)
         self.assertEqual(oRule.violations, [])
