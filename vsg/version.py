a2f23bd638fcb2c1391c20f51d65b6c757dc33c1
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-09-14 08:09:30 -0500

    Preparing for release 3.12.1

--- vsg/version.py
+++ vsg/version.py
@@ -2,7 +2,7 @@
 import subprocess
 import os
 
-sVersion = '3.12.0'
+sVersion = '3.12.1'
 
 
 def print_version(oCommandLineArguments):
@@ -20,7 +20,7 @@ def print_version(oCommandLineArguments):
 
 def get_version_info():
 
-    sVersion = '3.12.0'
+    sVersion = '3.12.1'
 
     if reporting_from_zip_file():
         return sVersion + '+zip.file', 'Unknown.  Installed via zip file.'
