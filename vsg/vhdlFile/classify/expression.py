e65db86d1d3bb019c0e1136835a3c3d9c3eb9585
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-10-01 07:02:57 -0500

    Removing old classify code.

--- vsg/vhdlFile/classify/expression.py
+++ vsg/vhdlFile/classify/expression.py
@@ -51,29 +51,3 @@ def classify_until(lUntils, iToken, lObjects, oType=parser.todo):
         else:
             utils.assign_special_tokens(lObjects, iCurrent, oType)
     return iCurrent
-
-
-def classify_until_old(lUntils, iToken, lObjects):
-    iEnd = len(lObjects) - 1
-    iCurrent = iToken
-    iOpenParenthesis = 0
-    iCloseParenthesis = 0
-    for iIndex in range(iToken, len(lObjects)):
-        print(lObjects[iIndex].get_value())
-        if not utils.is_item(lObjects, iIndex):
-            continue
-        if utils.token_is_open_parenthesis(iIndex, lObjects):
-           iOpenParenthesis += 1
-        if utils.token_is_close_parenthesis(iIndex, lObjects):
-           iCloseParenthesis += 1
-
-        if iOpenParenthesis < iCloseParenthesis:
-            break
-        elif lObjects[iIndex].get_value().lower() in lUntils:
-            if iOpenParenthesis == iCloseParenthesis:
-                break
-        else:
-            utils.assign_token(lObjects, iIndex, parser.todo)
-        if iCurrent == iEnd:
-            return iToken
-    return iCurrent
