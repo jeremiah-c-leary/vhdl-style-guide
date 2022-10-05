24dce789409b48ce048b6b22a962d7b6c9b891b4
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-10-05 06:14:01 -0500

    Updates for issue #833 (#834)
    Pragma comments were being update by rule comment_100 when they should not have been
    
      1)  Added tests
      2)  Updated classification of pragmas to the pragma itself

--- vsg/vhdlFile/classify/pragma.py
+++ vsg/vhdlFile/classify/pragma.py
@@ -10,10 +10,10 @@ def classify(lTokens, lObjects, lOpenPragmas, lClosePragmas, dVars):
 
     '''
     for iToken, sToken in enumerate(lTokens):
-        if sToken in lClosePragmas:
-            dVars['pragma'] = False
-        if dVars['pragma']:
-            if isinstance(lObjects[iToken], parser.item):
-                lObjects[iToken] = pragma.ignore(sToken)
         if sToken in lOpenPragmas:
             dVars['pragma'] = True
+        if dVars['pragma']:
+            if not isinstance(lObjects[iToken], parser.whitespace):
+                lObjects[iToken] = pragma.ignore(sToken)
+        if sToken in lClosePragmas:
+            dVars['pragma'] = False
