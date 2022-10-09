c5d8c5c7106abda89fdd1cad996fa0dba0e27ad6
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-10-08 19:26:47 -0500

    Adding disabled rule alias_declaration_103.

---
+++ vsg/rules/alias_declaration/rule_103.py
@@ -0,0 +1,31 @@
+
+from vsg.rules.whitespace_between_tokens import Rule
+
+from vsg.token import alias_declaration as token
+
+
+class rule_103(Rule):
+    '''
+    |phase_2| |disabled| |error| |whitespace|
+
+    This rule checks for a single space before the designator.
+
+    |configuring_whitespace_rules_link|
+
+    **Violation**
+
+    .. code-block:: vhdl
+
+       alias          alias_designator is name;
+
+    **Fix**
+
+    .. code-block:: vhdl
+
+       alias alias_designator is name;
+    '''
+    def __init__(self):
+        Rule.__init__(self, 'alias_declaration', '103')
+        self.disable = True
+        self.left_token = token.alias_keyword
+        self.right_token = token.alias_designator
