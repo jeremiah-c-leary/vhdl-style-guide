dac9e0d5afd0e25407ccfbe863c8c5b8410ed87f
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-10-28 06:56:16 -0500

    Adding rules:
      selected_expressions_101
      selected_waveforms_101

--- vsg/rules/selected_waveforms/__init__.py
+++ vsg/rules/selected_waveforms/__init__.py
@@ -1,4 +1,5 @@
 
 from .rule_100 import rule_100
+from .rule_101 import rule_101
 
 from .rule_500 import rule_500
---
+++ vsg/rules/selected_waveforms/rule_101.py
@@ -0,0 +1,36 @@
+
+from vsg.rules.whitespace_after_token import Rule
+
+from vsg.token import selected_waveforms as token
+
+lTokens = []
+lTokens.append(token.when_keyword)
+
+
+class rule_101(Rule):
+    '''
+    This rule checks for a single space after the **when** keyword.
+
+    |configuring_whitespace_rules_link|
+
+    **Violation**
+
+    .. code-block:: vhdl
+
+       with mux_sel select
+         addr <= "0000" when      0,
+                 "0001" when    1,
+                 "1111" when  others;
+
+    **Fix**
+
+    .. code-block:: vhdl
+
+       with mux_sel select
+         addr <= "0000" when 0,
+                 "0001" when 1,
+                 "1111" when others;
+    '''
+
+    def __init__(self):
+        Rule.__init__(self, 'selected_waveforms', '101', lTokens)
