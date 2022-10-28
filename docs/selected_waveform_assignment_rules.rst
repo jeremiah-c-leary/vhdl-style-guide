74d8eccc496d5607c06ffac55fa7d6bd6dab4f8c
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-10-28 06:23:59 -0500

    Adding rules:
      concurrent_selected_signal_assignment_104
      selected_force_assignment_104
      selected_variable_assignment_104
      selected_waveform_assignment_104

--- docs/selected_waveform_assignment_rules.rst
+++ docs/selected_waveform_assignment_rules.rst
@@ -109,6 +109,33 @@ This rule checks for a single space before the **<=**.
              "0001" when 1,
              "1111" when others;
 
+selected_waveform_assignment_104
+################################
+
+|phase_2| |error| |whitespace|
+
+This rule checks for a single space after the **<=**.
+
+|configuring_whitespace_rules_link|
+
+**Violation**
+
+.. code-block:: vhdl
+
+   with mux_sel select
+     addr <="0000" when 0,
+             "0001" when 1,
+             "1111" when others;
+
+**Fix**
+
+.. code-block:: vhdl
+
+   with mux_sel select
+     addr <= "0000" when 0,
+             "0001" when 1,
+             "1111" when others;
+
 selected_waveform_assignment_300
 ################################
 
---
+++ vsg/rules/selected_force_assignment/rule_104.py
@@ -0,0 +1,36 @@
+
+from vsg.rules.whitespace_after_token import Rule
+
+from vsg.token import selected_force_assignment as token
+
+lTokens = []
+lTokens.append(token.assignment)
+
+
+class rule_104(Rule):
+    '''
+    This rule checks for a single space after the **<=**.
+
+    |configuring_whitespace_rules_link|
+
+    **Violation**
+
+    .. code-block:: vhdl
+
+       with mux_sel select
+         addr <=force "0000" when 0,
+                       "0001" when 1,
+                       "1111" when others;
+
+    **Fix**
+
+    .. code-block:: vhdl
+
+       with mux_sel select
+         addr <= force "0000" when 0,
+                       "0001" when 1,
+                       "1111" when others;
+    '''
+
+    def __init__(self):
+        Rule.__init__(self, 'selected_force_assignment', '104', lTokens)
