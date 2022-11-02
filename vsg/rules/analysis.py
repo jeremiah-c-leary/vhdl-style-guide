e45d1f85fd7638aef1e3d3e1e2044028d1fc28d3
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-11-01 19:32:17 -0500

    Refactoring analysis functions to improve readability.

--- vsg/rules/analysis.py
+++ vsg/rules/analysis.py
@@ -8,33 +8,46 @@ from vsg import violation
 def check_for_carriage_return_after_token(self, oToi):
     if self.value == 'ignore':
         return []
+    elif self.value == 'no':
+        return check_no(self, oToi)
+    elif self.value == 'yes':
+        return check_yes(self, oToi)
+
+
+def check_no(self, oToi):    
+    return check(self, oToi, 'remove_new_line')
+
+
+def check_yes(self, oToi):    
+    return check(self, oToi, 'add_new_line')
+
+
+def check(self, oToi, action):
     lTokens = oToi.get_tokens()
     iLine = oToi.get_line_number()
     lReturn = []
 
+    value = set_check_value(action)
+
     for iToken, oToken in enumerate(lTokens):
         if rules_utils.token_exists_in_token_type_list(oToken, self.analysis_options):
-            if utils.is_token_at_end_of_line(iToken, lTokens) and self.value == 'no':
-                sSolution = 'jcl-fix this'
-                iViolation_line_number = iLine + rules_utils.number_of_carriage_returns(lTokens[0:iToken])
-                iEndIndex = utils.find_next_non_whitespace_token(iToken + 1, lTokens) - 1
-                oMyToi = oToi.extract_tokens(iToken + 1, iEndIndex)
-                oViolation = violation.New(iViolation_line_number, oMyToi, 'jcl-fix this')
-                dAction = {}
-                dAction['action'] = 'remove_new_line'
-                oViolation.set_action(dAction)
-                lReturn.append(oViolation)
-            elif not utils.is_token_at_end_of_line(iToken, lTokens) and self.value == 'yes':
+            if utils.is_token_at_end_of_line(iToken, lTokens) == value:
                 iViolation_line_number = iLine + rules_utils.number_of_carriage_returns(lTokens[0:iToken])
                 sSolution = 'jcl-fix this'
                 iEndIndex = utils.find_next_non_whitespace_token(iToken + 1, lTokens) - 1
                 oMyToi = oToi.extract_tokens(iToken + 1, iEndIndex)
-                oViolation = violation.New(iViolation_line_number, oMyToi, 'jcl-fix this')
+                oViolation = violation.New(iViolation_line_number, oMyToi, sSolution)
                 dAction = {}
-                dAction['action'] = 'add_new_line'
+                dAction['action'] = action
                 oViolation.set_action(dAction)
                 lReturn.append(oViolation)
     return lReturn
+
+
+def set_check_value(action):
+    if action == 'remove_new_line':
+        return True
+    return False
 
 
 def check_for_carriage_return_before_token(self, oToi):
