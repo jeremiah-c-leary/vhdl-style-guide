a96cb699fffae72e749629626ff77ff77039caee
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-11-25 15:23:20 -0600

    Rule procedure_call_401 was adding addition whitespace when not needed.

--- vsg/rules/procedure_call/rule_401.py
+++ vsg/rules/procedure_call/rule_401.py
@@ -42,3 +42,5 @@ class rule_401(Rule):
     def __init__(self):
         Rule.__init__(self, 'procedure_call', '401', lAlign, oBegin, oEnd)
         self.solution = 'Align =>.'
+        self.subphase = 2
+        self.bIncludeTillBeginningOfLine = True
