a355d2f123a373a533ae7391be3fcd02c7ba8c74
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2023-04-13 07:47:21 -0500

    Issue#923:  Adding indenting of protected type declarations. (#935)

--- vsg/tests/procedure/rule_001_test_input.vhd
+++ vsg/tests/procedure/rule_001_test_input.vhd
@@ -25,6 +25,19 @@ procedure proc_name (
   begin
   end procedure proc_name;
 
+  -- There must be an indent after the proctected type
+  type t_protected_fifo is protected
+
+    procedure reset_fifo (
+      void : t_void
+    );
+
+        procedure reset_fifo (
+      void : t_void
+    );
+
+  end protected t_protected_fifo;
+
 begin
 
 end architecture RTL;
