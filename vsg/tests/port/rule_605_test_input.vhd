023a018bf4a56270b33d395922f626ae207c41d6
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2023-05-03 20:32:11 -0500

    Issue#951:  Fixing suffix checks on blank lines.

--- vsg/tests/port/rule_605_test_input.vhd
+++ vsg/tests/port/rule_605_test_input.vhd
@@ -2,6 +2,13 @@
 entity FIFO is
   port (
     WR_EN_I  : in  std_logic;
+
+
+    WR_EN_I  : in  std_logic;
+
+    WR_EN_I  : in  std_logic;
+    WR_EN_I  : in  std_logic;
+
     DATA_O   : out std_logic_vector(31 downto 0);
     RD_EN_IO : inout  std_logic;
     DATA_O   : out std_logic_vector(31 downto 0)
@@ -12,8 +19,17 @@ end entity FIFO;
 entity FIFO is
   port (
     WR_EN : in  std_logic;
+
+
+    WR_EN : in  std_logic;
+
+    WR_EN : in  std_logic;
+    WR_EN : in  std_logic;
+
     DATA  : out std_logic_vector(31 downto 0);
+
     RD_EN : inout  std_logic;
+
     DATA  : out std_logic_vector(31 downto 0)
   );
 end entity FIFO;
