d577b2448d5d8e73d334168d665ba3f0910d495a
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2023-05-09 21:31:29 -0500

    Issue#947:  Rule generate_015 could produce invalid code.

---
+++ vsg/rules/generate/rule_021.py
@@ -0,0 +1,34 @@
+
+
+from vsg.rules import move_token_right_to_next_non_whitespace_token as Rule
+
+from vsg import token
+
+lTokens = []
+lTokens.append(token.case_generate_statement.label_colon)
+lTokens.append(token.for_generate_statement.label_colon)
+lTokens.append(token.if_generate_statement.label_colon)
+
+
+class rule_021(Rule):
+    '''
+    This rule checks a label colon is on the same line as the **case**, **if**, and **for** keywords.
+
+    **Violation**
+
+    .. code-block:: vhdl
+
+       label :
+       case
+
+    **Fix**
+
+    .. code-block:: vhdl
+
+       label
+       : case
+    '''
+
+    def __init__(self):
+        Rule.__init__(self, 'generate', '021', lTokens)
+        self.subphase = 2
