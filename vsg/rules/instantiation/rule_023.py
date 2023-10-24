acb7de10d45bc71f933d3dabadb5f4cfbe4729a4
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2023-10-24 16:34:13 -0500

    Issue#942:  Moved rule instantiation_023 to port_map_010. (#958)

--- vsg/rules/instantiation/rule_023.py
+++ vsg/rules/instantiation/rule_023.py
@@ -1,35 +1,11 @@
 
-from vsg.rules import remove_comments_from_end_of_lines_bounded_by_tokens
-
-from vsg import token
-
-oStart = token.component_instantiation_statement.instantiation_label
-
-oEnd = token.component_instantiation_statement.semicolon
+from vsg import deprecated_rule
 
 
-class rule_023(remove_comments_from_end_of_lines_bounded_by_tokens):
+class rule_023(deprecated_rule.Rule):
     '''
-    This rule checks for comments at the end of the port and generic assignments in instantiations.
-    These comments represent additional maintainence.
-    They will be out of sync with the entity at some point.
-    Refer to the entity for port types, port directions and purpose.
-
-    **Violation**
-
-    .. code-block:: vhdl
-
-       WR_EN => w_wr_en;   -- out : std_logic
-       RD_EN => w_rd_en;   -- Reads data when asserted
-
-    **Fix**
-
-    .. code-block:: vhdl
-
-       WR_EN => w_wr_en;
-       RD_EN => w_rd_en;
+    This rule has been renamed to `port_map_010 <port_map_rules.html#port-map-010>`_.
     '''
-
     def __init__(self):
-        remove_comments_from_end_of_lines_bounded_by_tokens.__init__(self, 'instantiation', '023', oStart, oEnd)
-        self.solution = 'Remove comment.'
+        deprecated_rule.Rule.__init__(self, 'instantiation', '023')
+        self.message.append('Rule ' + self.unique_id + ' has been renamed to port_map_010.')
--- vsg/tests/port_map/test_rule_010.py
+++ vsg/tests/port_map/test_rule_010.py
@@ -2,38 +2,38 @@
 import os
 import unittest
 
-from vsg.rules import instantiation
+from vsg.rules import port_map
 from vsg import vhdlFile
 from vsg.tests import utils
 
 sTestDir = os.path.dirname(__file__)
 
-lFile, eError =vhdlFile.utils.read_vhdlfile(os.path.join(sTestDir,'rule_023_test_input.vhd'))
+lFile, eError =vhdlFile.utils.read_vhdlfile(os.path.join(sTestDir,'rule_010_test_input.vhd'))
 
 lExpected = []
 lExpected.append('')
-utils.read_file(os.path.join(sTestDir, 'rule_023_test_input.fixed.vhd'), lExpected)
+utils.read_file(os.path.join(sTestDir, 'rule_010_test_input.fixed.vhd'), lExpected)
 
 
-class test_instantiation_rule(unittest.TestCase):
+class test_port_map_rule(unittest.TestCase):
 
     def setUp(self):
         self.oFile = vhdlFile.vhdlFile(lFile)
         self.assertIsNone(eError)
 
-    def test_rule_023(self):
-        oRule = instantiation.rule_023()
+    def test_rule_010(self):
+        oRule = port_map.rule_010()
         self.assertTrue(oRule)
-        self.assertEqual(oRule.name, 'instantiation')
-        self.assertEqual(oRule.identifier, '023')
+        self.assertEqual(oRule.name, 'port_map')
+        self.assertEqual(oRule.identifier, '010')
 
         lExpected = [9, 11, 13, 18, 20, 22]
 
         oRule.analyze(self.oFile)
         self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
 
-    def test_fix_rule_023(self):
-        oRule = instantiation.rule_023()
+    def test_fix_rule_010(self):
+        oRule = port_map.rule_010()
 
         oRule.fix(self.oFile)
 
