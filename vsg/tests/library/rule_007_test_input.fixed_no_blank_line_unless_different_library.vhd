32d168c16817327f11ed068ec10c6fcab0601fab
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2023-11-11 12:15:13 -0600

    Issue#909: Changed conflict between library_007 and entity_203
      * no_blank_line_unless_different_library does not flag if previous line has a design_unit semicolon.

--- vsg/tests/library/rule_007_test_input.fixed_no_blank_line_unless_different_library.vhd
+++ vsg/tests/library/rule_007_test_input.fixed_no_blank_line_unless_different_library.vhd
@@ -17,3 +17,75 @@ library ieee;
 use work.registers_pkg.all;
 use work.utility_pkg.all;
 
+-- Entity declaration
+
+entity fifo is end;
+
+use work.utility_pkg.all;
+
+entity fifo is end;
+use work.utility_pkg.all;
+
+-- context
+
+context selected_name;
+
+use work.utility_pkg.all;
+
+context selected_name;
+use work.utility_pkg.all;
+
+-- package declaration
+
+package fifo_pkg is end package;
+
+use work.utility_pkg.all;
+
+package fifo_pkg is end package;
+use work.utility_pkg.all;
+
+-- configuration
+
+configuration fifo_cfg of fifo is end configuration;
+
+use work.utility_pkg.all;
+
+configuration fifo_cfg of fifo is end configuration;
+use work.utility_pkg.all;
+
+-- package
+
+package fifo_pkg is new fifo_pkg;
+
+use work.utility_pkg.all;
+
+package fifo_pkg is new fifo_pkg;
+use work.utility_pkg.all;
+
+-- context declaration
+
+context my_context is end;
+
+use work.utility_pkg.all;
+
+context my_context is end;
+use work.utility_pkg.all;
+
+-- architecture
+
+architecture rtl of fifo is begin end;
+
+use work.utility_pkg.all;
+
+architecture rtl of fifo is begin end;
+use work.utility_pkg.all;
+
+-- package body
+
+package body fifo_pkg is end package body;
+
+use work.utility_pkg.all;
+
+package body fifo_pkg is end package body;
+use work.utility_pkg.all;
+
