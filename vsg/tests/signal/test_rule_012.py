eee1af7baaa1bb0de188885aea1daadb345f0c6c
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2023-11-19 12:53:27 -0600

    Issue#967:  Adding options to rule signal_012
       * Added blank_line_ends_group
       * Added comment_line_ends_group

--- vsg/tests/signal/test_rule_012.py
+++ vsg/tests/signal/test_rule_012.py
@@ -17,28 +17,32 @@ class test_signal_rule(unittest.TestCase):
         self.oFile = vhdlFile.vhdlFile(lFile)
         self.assertIsNone(eError)
 
-    def test_rule_012_yes(self):
+    def test_rule_012_yes_no_no(self):
         oRule = signal.rule_012()
         oRule.compact_alignment = 'yes'
+        oRule.blank_line_ends_group = 'no'
+        oRule.comment_line_ends_group = 'no'
         self.assertTrue(oRule)
         self.assertEqual(oRule.name, 'signal')
         self.assertEqual(oRule.identifier, '012')
         self.assertEqual(oRule.groups, ['alignment'])
 
-        lExpected = [4, 5, 10]
+        lExpected = [4, 5, 9, 10, 12, 13, 17, 20]
 
         oRule.analyze(self.oFile)
         self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
 
-    def test_fix_rule_012_yes(self):
+    def test_fix_rule_012_yes_no_no(self):
         oRule = signal.rule_012()
         oRule.compact_alignment = 'yes'
+        oRule.blank_line_ends_group = 'no'
+        oRule.comment_line_ends_group = 'no'
 
         oRule.fix(self.oFile)
 
         lExpected = []
         lExpected.append('')
-        utils.read_file(os.path.join(sTestDir, 'rule_012_test_input.fixed_yes.vhd'), lExpected)
+        utils.read_file(os.path.join(sTestDir, 'rule_012_test_input.fixed_yes_no_no.vhd'), lExpected)
 
         lActual = self.oFile.get_lines()
 
@@ -47,24 +51,28 @@ class test_signal_rule(unittest.TestCase):
         oRule.analyze(self.oFile)
         self.assertEqual(oRule.violations, [])
 
-    def test_rule_012_no(self):
+    def test_rule_012_no_no_no(self):
         oRule = signal.rule_012()
         oRule.compact_alignment = 'no'
+        oRule.blank_line_ends_group = 'no'
+        oRule.comment_line_ends_group = 'no'
 
-        lExpected = [4, 5, 10]
+        lExpected = [4, 5, 9, 10, 13, 17, 19, 20]
 
         oRule.analyze(self.oFile)
         self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
 
-    def test_fix_rule_012_no(self):
+    def test_fix_rule_012_no_no_no(self):
         oRule = signal.rule_012()
         oRule.compact_alignment = 'no'
+        oRule.blank_line_ends_group = 'no'
+        oRule.comment_line_ends_group = 'no'
 
         oRule.fix(self.oFile)
 
         lExpected = []
         lExpected.append('')
-        utils.read_file(os.path.join(sTestDir, 'rule_012_test_input.fixed_no.vhd'), lExpected)
+        utils.read_file(os.path.join(sTestDir, 'rule_012_test_input.fixed_no_no_no.vhd'), lExpected)
 
         lActual = self.oFile.get_lines()
 
@@ -72,3 +80,188 @@ class test_signal_rule(unittest.TestCase):
 
         oRule.analyze(self.oFile)
         self.assertEqual(oRule.violations, [])
+
+    def test_rule_012_yes_yes_no(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'yes'
+        oRule.blank_line_ends_group = 'yes'
+        oRule.comment_line_ends_group = 'no'
+
+        lExpected = [4]
+        lExpected.extend([9, 10, 12, 13])
+        lExpected.extend([20])
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
+
+    def test_fix_rule_012_yes_no_no(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'yes'
+        oRule.blank_line_ends_group = 'yes'
+        oRule.comment_line_ends_group = 'no'
+
+        oRule.fix(self.oFile)
+
+        lExpected = []
+        lExpected.append('')
+        utils.read_file(os.path.join(sTestDir, 'rule_012_test_input.fixed_yes_yes_no.vhd'), lExpected)
+
+        lActual = self.oFile.get_lines()
+
+        self.assertEqual(lExpected, lActual)
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
+
+    def test_rule_012_yes_no_yes(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'yes'
+        oRule.blank_line_ends_group = 'no'
+        oRule.comment_line_ends_group = 'yes'
+
+        lExpected = [4, 5, 10]
+        lExpected.extend([12, 13])
+        lExpected.extend([17, 20])
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
+
+    def test_fix_rule_012_yes_no_yes(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'yes'
+        oRule.blank_line_ends_group = 'no'
+        oRule.comment_line_ends_group = 'yes'
+
+        oRule.fix(self.oFile)
+
+        lExpected = []
+        lExpected.append('')
+        utils.read_file(os.path.join(sTestDir, 'rule_012_test_input.fixed_yes_no_yes.vhd'), lExpected)
+
+        lActual = self.oFile.get_lines()
+
+        self.assertEqual(lExpected, lActual)
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
+
+    def test_rule_012_yes_yes_yes(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'yes'
+        oRule.blank_line_ends_group = 'yes'
+        oRule.comment_line_ends_group = 'yes'
+
+        lExpected = [4, 10, 12, 13, 20]
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
+
+    def test_fix_rule_012_yes_yes_yes(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'yes'
+        oRule.blank_line_ends_group = 'yes'
+        oRule.comment_line_ends_group = 'yes'
+
+        oRule.fix(self.oFile)
+
+        lExpected = []
+        lExpected.append('')
+        utils.read_file(os.path.join(sTestDir, 'rule_012_test_input.fixed_yes_yes_yes.vhd'), lExpected)
+
+        lActual = self.oFile.get_lines()
+
+        self.assertEqual(lExpected, lActual)
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
+
+    def test_rule_012_no_yes_no(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'no'
+        oRule.blank_line_ends_group = 'yes'
+        oRule.comment_line_ends_group = 'no'
+
+        lExpected = [5, 9, 10, 13, 20]
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
+
+    def test_fix_rule_012_no_yes_no(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'no'
+        oRule.blank_line_ends_group = 'yes'
+        oRule.comment_line_ends_group = 'no'
+
+        oRule.fix(self.oFile)
+
+        lExpected = []
+        lExpected.append('')
+        utils.read_file(os.path.join(sTestDir, 'rule_012_test_input.fixed_no_yes_no.vhd'), lExpected)
+
+        lActual = self.oFile.get_lines()
+
+        self.assertEqual(lExpected, lActual)
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
+
+    def test_rule_012_no_no_yes(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'no'
+        oRule.blank_line_ends_group = 'no'
+        oRule.comment_line_ends_group = 'yes'
+
+        lExpected = [4, 5, 9, 13, 17, 20]
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
+
+    def test_fix_rule_012_no_no_yes(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'no'
+        oRule.blank_line_ends_group = 'no'
+        oRule.comment_line_ends_group = 'yes'
+
+        oRule.fix(self.oFile)
+
+        lExpected = []
+        lExpected.append('')
+        utils.read_file(os.path.join(sTestDir, 'rule_012_test_input.fixed_no_no_yes.vhd'), lExpected)
+
+        lActual = self.oFile.get_lines()
+
+        self.assertEqual(lExpected, lActual)
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
+
+    def test_rule_012_no_yes_yes(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'no'
+        oRule.blank_line_ends_group = 'yes'
+        oRule.comment_line_ends_group = 'yes'
+
+        lExpected = [5, 9, 13, 20]
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
+
+    def test_fix_rule_012_no_yes_yes(self):
+        oRule = signal.rule_012()
+        oRule.compact_alignment = 'no'
+        oRule.blank_line_ends_group = 'yes'
+        oRule.comment_line_ends_group = 'yes'
+
+        oRule.fix(self.oFile)
+
+        lExpected = []
+        lExpected.append('')
+        utils.read_file(os.path.join(sTestDir, 'rule_012_test_input.fixed_no_yes_yes.vhd'), lExpected)
+
+        lActual = self.oFile.get_lines()
+
+        self.assertEqual(lExpected, lActual)
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
+
