91d46d3db67eab89d3e82ad0fa48893f4374afe3
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2023-12-02 08:25:41 -0600

    Adding pragma configuration tests.

---
+++ vsg/tests/vsg/pragmas/pragmas.vhd
@@ -0,0 +1,15 @@
+
+     -- synthesis translate_off
+
+architecture rtl of fifo is
+
+--vhdl_comp_off
+--vhdl_comp_on
+  -- Some comment
+     -- xilinx something
+-- synthesis translate_off
+  signal write : std_logic;
+
+begin
+
+end architecture rtl;
---
+++ vsg/tests/vsg/test_pragma.py
@@ -0,0 +1,74 @@
+import filecmp
+import pathlib
+import unittest
+from unittest import mock
+import os
+import subprocess
+import shutil
+import sys
+import shutil
+import yaml
+
+import contextlib
+from io import StringIO
+
+from tempfile import TemporaryFile
+
+from vsg.tests import utils
+from vsg import version
+from vsg import __main__
+
+
+class testMain(unittest.TestCase):
+
+    @mock.patch('sys.stdout')
+    def test_default_pragmas(self, mock_stdout):
+
+        sCall = ''
+        sCall += 'ERROR: vsg/tests/vsg/pragmas/pragmas.vhd(2)pragma_300 -- Indent level 0'
+        sCall += '\n'
+        sCall += 'ERROR: vsg/tests/vsg/pragmas/pragmas.vhd(9)pragma_300 -- Use 2 spaces for indent'
+        sCall += '\n'
+        sCall += 'ERROR: vsg/tests/vsg/pragmas/pragmas.vhd(10)pragma_300 -- Use 2 spaces for indent'
+
+        lExpected = []
+        lExpected.append(mock.call(sCall))
+        lExpected.append(mock.call('\n'))
+
+        sys.argv = ['vsg']
+        sys.argv.extend(['-f', 'vsg/tests/vsg/pragmas/pragmas.vhd'])
+        sys.argv.extend(['--output_format', 'syntastic'])
+        sys.argv.extend(['-p 1'])
+
+        try:
+            __main__.main()
+        except SystemExit:
+            pass
+
+        mock_stdout.write.assert_has_calls(lExpected)
+
+    @mock.patch('sys.stdout')
+    def test_configuring_pragmas(self, mock_stdout):
+
+        sCall = ''
+        sCall += 'ERROR: vsg/tests/vsg/pragmas/pragmas.vhd(2)pragma_300 -- Indent level 0'
+        sCall += '\n'
+        sCall += 'ERROR: vsg/tests/vsg/pragmas/pragmas.vhd(9)comment_010 -- Use 2 spaces for indent'
+        sCall += '\n'
+        sCall += 'ERROR: vsg/tests/vsg/pragmas/pragmas.vhd(10)pragma_300 -- Use 2 spaces for indent'
+
+        lExpected = []
+        lExpected.append(mock.call(sCall))
+        lExpected.append(mock.call('\n'))
+
+        sys.argv = ['vsg']
+        sys.argv.extend(['--configuration', 'vsg/tests/vsg/pragmas/config_pragma.json'])
+        sys.argv.extend(['--output_format', 'syntastic'])
+        sys.argv.extend(['-p 1'])
+
+        try:
+            __main__.main()
+        except SystemExit:
+            pass
+
+        mock_stdout.write.assert_has_calls(lExpected)
