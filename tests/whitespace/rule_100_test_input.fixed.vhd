992012934dd68c062a242231dc1ee897a63440af
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2024-06-02 08:46:20 -0500

    Issue#1171:  Adding rule whitespace_100 to enforce single spacing around relational operators.

---
+++ tests/whitespace/rule_100_test_input.fixed.vhd
@@ -0,0 +1,125 @@
+
+architecture RTL of FIFO is
+
+begin
+
+  -- Passing
+  process begin
+    if a = b then
+    elsif a /= b then
+    elsif a < b then
+    elsif a <= b then
+    elsif a > b then
+    elsif a >= b then
+    elsif a ?= b then
+    elsif a ?/= b then
+    elsif a ?< b then
+    elsif a ?<= b then
+    elsif a ?> b then
+    elsif a ?>= b then
+    end if;
+  end process;
+
+  -- missing spaces after operator
+  process begin
+    if a = b then
+    elsif a /= b then
+    elsif a < b then
+    elsif a <= b then
+    elsif a > b then
+    elsif a >= b then
+    elsif a ?= b then
+    elsif a ?/= b then
+    elsif a ?< b then
+    elsif a ?<= b then
+    elsif a ?> b then
+    elsif a ?>= b then
+    end if;
+  end process;
+
+  -- missing spaces before operator
+  process begin
+    if a = b then
+    elsif a /= b then
+    elsif a < b then
+    elsif a <= b then
+    elsif a > b then
+    elsif a >= b then
+    elsif a ?= b then
+    elsif a ?/= b then
+    elsif a ?< b then
+    elsif a ?<= b then
+    elsif a ?> b then
+    elsif a ?>= b then
+    end if;
+  end process;
+
+  -- missing spaces before and after operator
+  process begin
+    if a = b then
+    elsif a /= b then
+    elsif a < b then
+    elsif a <= b then
+    elsif a > b then
+    elsif a >= b then
+    elsif a ?= b then
+    elsif a ?/= b then
+    elsif a ?< b then
+    elsif a ?<= b then
+    elsif a ?> b then
+    elsif a ?>= b then
+    end if;
+  end process;
+
+  -- extra spaces before operator
+  process begin
+    if a = b then
+    elsif a /= b then
+    elsif a < b then
+    elsif a <= b then
+    elsif a > b then
+    elsif a >= b then
+    elsif a ?= b then
+    elsif a ?/= b then
+    elsif a ?< b then
+    elsif a ?<= b then
+    elsif a ?> b then
+    elsif a ?>= b then
+    end if;
+  end process;
+
+  -- extra spaces after operator
+  process begin
+    if a = b then
+    elsif a /= b then
+    elsif a < b then
+    elsif a <= b then
+    elsif a > b then
+    elsif a >= b then
+    elsif a ?= b then
+    elsif a ?/= b then
+    elsif a ?< b then
+    elsif a ?<= b then
+    elsif a ?> b then
+    elsif a ?>= b then
+    end if;
+  end process;
+
+  -- extra spaces before and after operator
+  process begin
+    if a = b then
+    elsif a /= b then
+    elsif a < b then
+    elsif a <= b then
+    elsif a > b then
+    elsif a >= b then
+    elsif a ?= b then
+    elsif a ?/= b then
+    elsif a ?< b then
+    elsif a ?<= b then
+    elsif a ?> b then
+    elsif a ?>= b then
+    end if;
+  end process;
+
+end architecture RTL;
--- vsg/rules/whitespace/__init__.py
+++ vsg/rules/whitespace/__init__.py
@@ -12,4 +12,5 @@ from .rule_010 import rule_010
 from .rule_011 import rule_011
 from .rule_012 import rule_012
 from .rule_013 import rule_013
+from .rule_100 import rule_100
 from .rule_200 import rule_200
