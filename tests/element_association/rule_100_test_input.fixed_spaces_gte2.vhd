c6c26e33bc7bd8973d91da545e4589c2d4ef1892
Edgar Vedvik <edgar.vedvik@gmail.com>
2024-06-20 04:32:02 +0200

    PR#1183: Fix bug in whitespace between tokens and add test for it (#1183)
    Co-authored-by: Edgar Vedvik <edgar@radionor.no>

---
+++ tests/element_association/rule_100_test_input.fixed_spaces_gte2.vhd
@@ -0,0 +1,29 @@
+
+architecture rtl of fifo is
+
+begin
+
+  a <= (others  => (others  => '0'));
+
+  process begin
+
+    a <= (others  => (others  => '0'));
+
+  end process;
+
+end architecture;
+
+
+architecture rtl of fifo is
+
+begin
+
+  a <= (others  => (others          => '0'));
+
+  process begin
+
+    a <= (others        => (others  => '0'));
+
+  end process;
+
+end architecture;
