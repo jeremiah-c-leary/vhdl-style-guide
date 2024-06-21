3d48e2f593f8a337b54b44bf2c8ef65a7b8b5f04
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2024-06-21 07:18:37 -0500

    Adding parsing of external names to productions.

--- tests/vhdlFile/external_name/classification_test_input.vhd
+++ tests/vhdlFile/external_name/classification_test_input.vhd
@@ -9,4 +9,37 @@ architecture RTL of FIFO is
 
 begin
 
+  probe_signal <= << constant some.hierarchy : std_logic >> or
+                  << signal some.hieararchy : std_logic >> or
+                  << variable some.hierarchy : std_logic >>;
+
+  process (<< constant some.hierarchy : std_logic >>,
+           << signal some.hieararchy : std_logic >>,
+           << variable some.hierarchy : std_logic >>) is
+  begin
+
+    probe_signal <= << constant some.hierarchy : std_logic >> or
+                    << signal some.hieararchy : std_logic >> or
+                    << variable some.hierarchy : std_logic >>;
+
+    if << constant some.hierarchy : std_logic >> or
+                    << signal some.hieararchy : std_logic >> or
+                    << variable some.hierarchy : std_logic >> then
+
+    elsif << constant some.hierarchy : std_logic >> or
+                    << signal some.hieararchy : std_logic >> or
+                    << variable some.hierarchy : std_logic >> then
+    else
+
+        sig1 := func(<< signal some.hierarchy : std_logic >>, << signal some.hierarchy : std_logic >>, << variable some.hierarchy : std_logic >>);
+
+        var1 := func(<< signal some.hierarchy : std_logic >>, << signal some.hierarchy : std_logic >>, << variable some.hierarchy : std_logic >>);
+
+    end if;
+
+    case << signal some.hierarchy : std_logic >> & << signal some.hierarchy : std_logic >> & << variable some.hierarchy : std_logic >> is
+    end case;
+
+  end process;
+
 end architecture RTL;
