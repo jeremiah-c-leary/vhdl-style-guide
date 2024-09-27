64c1ea365ae0a0a131a407ba8fd690f79ecebbd2
JHertz5 <jukka.hertzog@live.co.uk>
2024-09-27 21:31:43 +0100

    Issue#1228: Added rules subtype_005 and subtype_006.

--- vsg/rules/subtype/__init__.py
+++ vsg/rules/subtype/__init__.py
@@ -4,6 +4,8 @@ from .rule_001 import rule_001
 from .rule_002 import rule_002
 from .rule_003 import rule_003
 from .rule_004 import rule_004
+from .rule_005 import rule_005
+from .rule_006 import rule_006
 from .rule_100 import rule_100
 from .rule_500 import rule_500
 from .rule_501 import rule_501
---
+++ vsg/rules/subtype/rule_006.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from vsg import token
+from vsg.rules.whitespace_between_token_pairs import Rule
+
+lTokens = []
+lTokens.append([token.subtype_declaration.is_keyword, token.type_mark.name])
+lTokens.append([token.subtype_declaration.is_keyword, token.ieee.std_logic_1164.types.std_logic_vector])
+lTokens.append([token.subtype_declaration.is_keyword, token.ieee.std_logic_1164.types.std_ulogic_vector])
+lTokens.append([token.subtype_declaration.is_keyword, token.ieee.std_logic_1164.types.std_ulogic])
+lTokens.append([token.subtype_declaration.is_keyword, token.ieee.std_logic_1164.types.std_logic])
+lTokens.append([token.subtype_declaration.is_keyword, token.ieee.std_logic_1164.types.integer])
+lTokens.append([token.subtype_declaration.is_keyword, token.ieee.std_logic_1164.types.signed])
+lTokens.append([token.subtype_declaration.is_keyword, token.ieee.std_logic_1164.types.unsigned])
+lTokens.append([token.subtype_declaration.is_keyword, token.ieee.std_logic_1164.types.natural])
+
+
+class rule_006(Rule):
+    """
+    This rule checks for a single space after the **is** keyword.
+
+    |configuring_whitespace_rules_link|
+
+    **Violation**
+
+    .. code-block:: vhdl
+
+       subtype counter is     unsigned(4 downto 0);
+
+    **Fix**
+
+    .. code-block:: vhdl
+
+       subtype counter is unsigned(4 downto 0);
+    """
+
+    def __init__(self):
+        super().__init__(lTokens)
