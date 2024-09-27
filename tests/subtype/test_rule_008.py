981bd0f7d385c9feb9512536a154c922c5e953f1
JHertz5 <jukka.hertzog@live.co.uk>
2024-09-27 22:28:12 +0100

    Issue#1228: Added tests for rules subtype_007 and subtype_008.

---
+++ tests/subtype/test_rule_008.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import os
+import unittest
+
+from tests import utils
+from vsg import vhdlFile
+from vsg.rules import subtype
+
+sTestDir = os.path.dirname(__file__)
+
+lFile, eError = vhdlFile.utils.read_vhdlfile(os.path.join(sTestDir, "rule_008_test_input.vhd"))
+
+lExpected = []
+lExpected.append("")
+utils.read_file(os.path.join(sTestDir, "rule_008_test_input.fixed.vhd"), lExpected)
+
+
+class test_type_definition_rule(unittest.TestCase):
+    def setUp(self):
+        self.oFile = vhdlFile.vhdlFile(lFile)
+        self.assertIsNone(eError)
+
+    def test_rule_008(self):
+        oRule = subtype.rule_008()
+        self.assertTrue(oRule)
+        self.assertEqual(oRule.name, "subtype")
+        self.assertEqual(oRule.identifier, "008")
+
+        lExpected = [10]
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
+
+    def test_fix_rule_008(self):
+        oRule = subtype.rule_008()
+
+        oRule.fix(self.oFile)
+
+        lActual = self.oFile.get_lines()
+
+        self.assertEqual(lExpected, lActual)
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
