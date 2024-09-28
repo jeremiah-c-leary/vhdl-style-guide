45c3bb7e2e60a6d3bf6c3dd3adc1117c507b67fd
JHertz5 <jukka.hertzog@live.co.uk>
2024-09-28 10:58:14 +0100

    Issue#1228: Added tests for rule subtype_200.

---
+++ tests/subtype/rule_200_test_input.vhd
@@ -0,0 +1,14 @@
+
+architecture RTL of FIFO is
+
+  subtype counter_t is unsigned(4 downto 0);
+  subtype counter is unsigned(4 downto 0);
+  constant width : integer := 32;
+
+  subtype counter is unsigned(4 downto 0);
+  subtype counter_t is unsigned(4 downto 0);
+  constant width : integer := 32;
+
+begin
+
+end architecture RTL;
