0875ade829e5fabfcc2def2b2624c202bf3647f7
JHertz5 <jukka.hertzog@live.co.uk>
2024-10-18 18:21:00 +0100

    Issue#1253: Added tests for rule function_510.

---
+++ tests/function/rule_510_test_input.fixed_lower.vhd
@@ -0,0 +1,31 @@
+package rule_510_test_input is
+
+  pure function func1 return integer;
+  impure function func1 return integer;
+
+  pure FUNCTION FUNC1 RETURN INTEGER;
+  impure FUNCTION FUNC1 RETURN INTEGER;
+
+end package;
+
+package body rule_510_test_input is
+
+  pure function func1 return integer is begin end function func1;
+  impure function func1 return integer is begin end function func1;
+
+  pure FUNCTION FUNC1 RETURN INTEGER IS BEGIN END FUNCTION FUNC1;
+  impure FUNCTION FUNC1 RETURN INTEGER IS BEGIN END FUNCTION FUNC1;
+
+end package body;
+
+architecture RTL of FIFO is
+
+  pure function func1 return integer is begin end function func1;
+  impure function func1 return integer is begin end function func1;
+
+  pure FUNCTION FUNC1 RETURN INTEGER IS BEGIN END FUNCTION FUNC1;
+  impure FUNCTION FUNC1 RETURN INTEGER IS BEGIN END FUNCTION FUNC1;
+
+begin
+
+end architecture RTL;
