32ff55eb6a772f0a6fd74479a151eb211f975aed
JHertz5 <jukka.hertzog@live.co.uk>
2024-10-19 17:02:15 +0100

    Issue#1271: Added rules for null statements (#1272)
    * Issue#1271: Added rules null_statement_300 and null_statement_500.
    
    * Issue#1271: Added tests for rules null_statement_300 and null_statement_500.
    
    * Issue#1271: Added documentation for rules null_statement_300 and null_statement_500.

---
+++ tests/null_statement/rule_500_test_input.fixed_upper.vhd
@@ -0,0 +1,28 @@
+
+architecture RTL of FIFO is
+
+  function func1 return integer is begin
+
+    loop
+
+      NULL;
+      null_label : NULL;
+
+    end loop;
+
+  end function func1;
+
+  function func1 return integer is begin
+
+    loop
+
+      NULL;
+      null_label : NULL;
+
+    end loop;
+
+  end function func1;
+
+begin
+
+end architecture RTL;
