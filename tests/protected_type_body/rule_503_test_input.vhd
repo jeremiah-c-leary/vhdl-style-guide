f313504306805b482f3c2ea90c9ad4a37cabafa1
JHertz5 <jukka.hertzog@live.co.uk>
2024-10-22 22:55:18 +0100

    Issue#1285: Added tests for protected_type_body rules

---
+++ tests/protected_type_body/rule_503_test_input.vhd
@@ -0,0 +1,22 @@
+
+package body test is
+
+  type flag_pt is protected body
+  end protected body;
+
+  type flag_pt is protected body
+  end PROTECTED body;
+
+end package body test;
+
+architecture rtl of test is
+
+  type flag_pt is protected body
+  end protected body;
+
+  type flag_pt is protected body
+  end PROTECTED body;
+
+begin
+
+end architecture rtl;
