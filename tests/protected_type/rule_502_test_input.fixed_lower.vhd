61470c0a673914d4b69c9c2b13cbf6261ab8387b
JHertz5 <jukka.hertzog@live.co.uk>
2024-10-22 22:42:11 +0100

    Issue#1285: Added tests for protected_type rules

---
+++ tests/protected_type/rule_502_test_input.fixed_lower.vhd
@@ -0,0 +1,21 @@
+package test is
+
+  type flag_pt is protected
+  end protected;
+
+  type flag_pt is protected
+  end protected;
+
+end package test;
+
+architecture rtl of test is
+
+  type flag_pt is protected
+  end protected;
+
+  type flag_pt is protected
+  end protected;
+
+begin
+
+end architecture rtl;
---
+++ tests/protected_type/rule_502_test_input.vhd
@@ -0,0 +1,21 @@
+package test is
+
+  type flag_pt is protected
+  end protected;
+
+  type flag_pt is protected
+  end PROTECTED;
+
+end package test;
+
+architecture rtl of test is
+
+  type flag_pt is protected
+  end protected;
+
+  type flag_pt is protected
+  end PROTECTED;
+
+begin
+
+end architecture rtl;
