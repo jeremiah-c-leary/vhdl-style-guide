450076c06dff1a37a73b21afbf99111596fb8dc2
JHertz5 <jukka.hertzog@live.co.uk>
2024-10-24 21:51:02 +0100

    Issue#1287: Added tests for alignment rules for protected type body

---
+++ tests/protected_type_body/rule_401_test_input.vhd
@@ -0,0 +1,30 @@
+
+package body RTL is
+  type t_my_type is protected body
+    -- These should pass
+    variable v_var1   : std_logic;
+    constant c_cons1  : std_logic;
+    file     f_fil1   : load_file_type open read_mode is load_file_name;
+    type     t_typ1   is (idle, write, read);
+    subtype  s_sub1   is integer range 0 to 9;
+    alias    a_alias1 is name;
+    alias    a_alias2 : subtype_indictor is name;
+
+    -- These should fail
+    variable v_var1 : std_logic;
+    constant c_cons1 : std_logic;
+    file     f_fil1 : load_file_type open read_mode is load_file_name;
+    type     t_typ1 is (idle, write, read);
+    subtype  s_sub1 is integer range 0 to 9;
+    alias    a_alias1      is name;
+    alias    a_alias2             : subtype_indictor is name;
+
+    -- Checking exclusions
+    constant c_abc : integer;
+    function my_func return integer is
+      constant c_abc : integer;
+    begin
+    end function;
+  end protected body;
+
+end package body RTL;
---
+++ tests/protected_type_body/test_rule_400.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import os
+import unittest
+
+from tests import utils
+from vsg import vhdlFile
+from vsg.rules import protected_type_body
+
+sTestDir = os.path.dirname(__file__)
+
+lFile, eError = vhdlFile.utils.read_vhdlfile(os.path.join(sTestDir, "rule_400_test_input.vhd"))
+
+lExpected = []
+lExpected.append("")
+utils.read_file(os.path.join(sTestDir, "rule_400_test_input.fixed.vhd"), lExpected)
+
+
+class test_protected_type_body_rule(unittest.TestCase):
+    def setUp(self):
+        self.oFile = vhdlFile.vhdlFile(lFile)
+        self.assertIsNone(eError)
+
+    def test_rule_400(self):
+        oRule = protected_type_body.rule_400()
+        self.assertTrue(oRule)
+        self.assertEqual(oRule.name, "protected_type_body")
+        self.assertEqual(oRule.identifier, "400")
+
+        lExpected = []
+        lExpected.extend([7, 8, 9, 10, 21, 22, 23, 25, 26, 30, 31, 44, 90, 91, 92])
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
+
+    def test_fix_rule_400(self):
+        oRule = protected_type_body.rule_400()
+
+        oRule.fix(self.oFile)
+
+        lActual = self.oFile.get_lines()
+
+        self.assertEqual(lExpected, lActual)
+
+        oRule.analyze(self.oFile)
+        self.assertEqual(oRule.violations, [])
