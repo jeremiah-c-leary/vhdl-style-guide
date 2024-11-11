acdbbd81cb6723cc18eee2d77f3a6d962bc303c5
Jukka Hertzog <jukka.hertzog@live.co.uk>
2024-11-11 02:00:22 +0000

    Issue#1285: Added case rules for protected type declarations and bodies (#1293)
    * Issue#1285: Added case rules for protected type declarations.
    
    * Issue#1285: Added case rules for protected type body declarations.
    
    * Issue#1285: Correction in protected_type_502
    
    * Issue#1285: Added protected_type and protected_type_body to top-level rules object
    
    * Issue#1285: Added tests for protected_type rules
    
    * Issue#1285: Whitespace changes in test cases.
    
    * Issue#1285: Updated protected_type tests.
    
    * Issue#1285: Added tests for protected_type_body rules
    
    * Issue#1285: Updated documentation for protected_type and protected_type_body rules.

---
+++ tests/protected_type_body/rule_502_test_input.vhd
@@ -0,0 +1,22 @@
+
+package body test is
+
+  type flag_pt is protected body
+  end protected body;
+
+  type flag_pt is protected body
+  END protected body;
+
+end package body test;
+
+architecture rtl of test is
+
+  type flag_pt is protected body
+  end protected body;
+
+  type flag_pt is protected body
+  END protected body;
+
+begin
+
+end architecture rtl;
