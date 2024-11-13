0322c3cd7033ef4a8e06601cc3ee90d578e3ec3d
JHertz5 <jukka.hertzog@live.co.uk>
2024-11-13 22:25:45 +0000

    Issue#1324: Added rule package_instantiation_201.

--- vsg/rules/package_instantiation/__init__.py
+++ vsg/rules/package_instantiation/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from .rule_200 import rule_200
+from .rule_201 import rule_201
 from .rule_300 import rule_300
 from .rule_500 import rule_500
 from .rule_501 import rule_501
