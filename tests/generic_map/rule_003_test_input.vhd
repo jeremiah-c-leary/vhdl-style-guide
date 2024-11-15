cca23e5a9b58d5d92ebadcfd21de199aa67895de
JHertz5 <jukka.hertzog@live.co.uk>
2024-11-15 13:42:04 +0000

    Issue#1319: Updated test for rule generic_map_003 to test generic maps other than those in component instantiations.

--- tests/generic_map/rule_003_test_input.vhd
+++ tests/generic_map/rule_003_test_input.vhd
@@ -1,6 +1,11 @@
 
 architecture ARCH of ENTITY1 is
 
+  function my_func is new my_func
+    generic map (
+      g_test => c_test
+    );
+
 begin
 
   U_INST1 : INST1
@@ -15,6 +20,32 @@ begin
       PORT_3 => w_port_3
     );
 
+  my_block : block is
+    generic (
+      g_test : BOOLEAN
+    );
+    generic map (
+      g_test => C_TEST
+    );
+  begin
+  end block;
+
+end architecture ARCH;
+
+package my_pkg_g is new my_pkg
+  generic map (
+    g_test => c_test
+  );
+
+architecture ARCH of ENTITY1 is
+
+  function my_func is new my_func
+    generic map(
+      g_test => c_test
+    );
+
+begin
+
   U_INST1 : INST1
     generic map(
       G_GEN_1 => 3,
@@ -27,7 +58,34 @@ begin
       PORT_3 => w_port_3
     );
 
+  my_block : block is
+    generic (
+      g_test : BOOLEAN
+    );
+    generic map(
+      g_test => C_TEST
+    );
+  begin
+  end block;
+
+end architecture ARCH;
+
+package my_pkg_g is new my_pkg
+  generic map(
+    g_test => c_test
+  );
+
   -- Violations below
+
+architecture ARCH of ENTITY1 is
+
+  function my_func is new my_func
+    generic map
+    (
+      g_test => c_test
+    );
+
+begin
 
   U_INST1 : INST1
     generic map
@@ -36,10 +94,29 @@ begin
       G_GEN_2 => 4,
       G_GEN_3 => 5
     )
-    port map (
+    port map
+    (
       PORT_1 => w_port_1,
       PORT_2 => w_port_2,
       PORT_3 => w_port_3
     );
 
+  my_block : block is
+    generic (
+      g_test : BOOLEAN
+    );
+    generic map
+    (
+      g_test => C_TEST
+    );
+  begin
+  end block;
+
 end architecture ARCH;
+
+package my_pkg_g is new my_pkg
+  generic map
+  (
+    g_test => c_test
+  );
+
