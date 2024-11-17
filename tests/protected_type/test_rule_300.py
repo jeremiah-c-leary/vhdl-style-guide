4ab01fe454f84c99e21e6ebcd4408fda1e8375d1
Jukka Hertzog <jukka.hertzog@live.co.uk>
2024-11-17 01:24:43 +0000

    Issue#1288: Added indent rules for protected type declarations and bodies (#1294)
    * Issue#1285: Added case rules for protected type declarations.
    
    * Issue#1285: Added case rules for protected type body declarations.
    
    * Issue#1285: Correction in protected_type_502
    
    * Issue#1285: Added protected_type and protected_type_body to top-level rules object
    
    * Issue#1285: Added tests for protected_type rules
    
    * Issue#1285: Whitespace changes in test cases.
    
    * Issue#1285: Updated protected_type tests.
    
    * Issue#1285: Added tests for protected_type_body rules
    
    * Issue#1285: Updated documentation for protected_type and protected_type_body rules.
    
    * Issue#1288: Some corrections in next statement test names
    
    * Issue#1288: Added rule protected_type_300.
    
    * Issue#1288: Added tests for rule protected_type_300.
    
    * Issue#1288: Added rule protected_type_body_300.
    
    * Issue#1288: Added tests for rule protected_type_body_300.
    
    * Issue#1288: Updated documentation.
    
    * Issue#1288: Corrected test cases

--- tests/protected_type/test_rule_300.py
+++ tests/protected_type/test_rule_300.py
@@ -5,7 +5,7 @@ import unittest
 
 from tests import utils
 from vsg import vhdlFile
-from vsg.rules import next_statement
+from vsg.rules import protected_type
 
 sTestDir = os.path.dirname(__file__)
 
@@ -18,25 +18,25 @@ lExpected.append("")
 utils.read_file(os.path.join(sTestDir, "rule_300_test_input.fixed.vhd"), lExpected)
 
 
-class test_next_statement_rule(unittest.TestCase):
+class test_protected_type_rule(unittest.TestCase):
     def setUp(self):
         self.oFile = vhdlFile.vhdlFile(lFile)
         self.assertIsNone(eError)
         self.oFile.set_indent_map(dIndentMap)
 
     def test_rule_300(self):
-        oRule = next_statement.rule_300()
+        oRule = protected_type.rule_300()
         self.assertTrue(oRule)
-        self.assertEqual(oRule.name, "next_statement")
+        self.assertEqual(oRule.name, "protected_type")
         self.assertEqual(oRule.identifier, "300")
 
-        lExpected = [23, 24, 25, 26, 27, 28]
+        lExpected = [8, 11, 21, 24]
 
         oRule.analyze(self.oFile)
         self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
 
     def test_fix_rule_300(self):
-        oRule = next_statement.rule_300()
+        oRule = protected_type.rule_300()
 
         oRule.fix(self.oFile)
 
