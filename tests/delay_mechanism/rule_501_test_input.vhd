a2396c1982426dad677e5e958568837c09b77a65
JHertz5 <jukka.hertzog@live.co.uk>
2024-12-14 23:26:51 +0000

    Issue#1341: Corrected tests for rules delay_mechanism_501, delay_mechanism_502.

--- tests/delay_mechanism/rule_501_test_input.vhd
+++ tests/delay_mechanism/rule_501_test_input.vhd
@@ -5,6 +5,6 @@ begin
 
   test1 <= reject 2 ns inertial 0 after 10 ns;
 
-  test1 <= REJECT 2 ns inertial 0 after 10 ns;
+  test1 <= reject 2 ns INERTIAL 0 after 10 ns;
 
 end architecture;
--- tests/delay_mechanism/rule_502_test_input.fixed_upper.vhd
+++ tests/delay_mechanism/rule_502_test_input.fixed_upper.vhd
@@ -3,8 +3,8 @@ architecture rtl of fifo is
 
 begin
 
-  test1 <= reject 2 ns INERTIAL 0 after 10 ns;
+  test1 <= REJECT 2 ns inertial 0 after 10 ns;
 
-  test1 <= reject 2 ns INERTIAL 0 after 10 ns;
+  test1 <= REJECT 2 ns inertial 0 after 10 ns;
 
 end architecture;
