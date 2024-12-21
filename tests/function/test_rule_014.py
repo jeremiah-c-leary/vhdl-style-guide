178571e7cde3b37b5be49a6e88a665811ffff9cf
JHertz5 <jukka.hertzog@live.co.uk>
2024-12-21 18:10:01 +0000

    Issue#1364: Updated tests for rules function_014 and procedure_505.

--- tests/function/test_rule_014.py
+++ tests/function/test_rule_014.py
@@ -1,73 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import os
 import unittest
 
-from tests import utils
-from vsg import vhdlFile
 from vsg.rules import function
 
-sTestDir = os.path.dirname(__file__)
 
-lFile, eError = vhdlFile.utils.read_vhdlfile(os.path.join(sTestDir, "rule_014_test_input.vhd"))
-
-lExpected_lower = []
-lExpected_lower.append("")
-utils.read_file(os.path.join(sTestDir, "rule_014_test_input.fixed_lower.vhd"), lExpected_lower)
-
-lExpected_upper = []
-lExpected_upper.append("")
-utils.read_file(os.path.join(sTestDir, "rule_014_test_input.fixed_upper.vhd"), lExpected_upper)
-
-
-class test_function_rule(unittest.TestCase):
-    def setUp(self):
-        self.oFile = vhdlFile.vhdlFile(lFile)
-        self.assertIsNone(eError)
-
-    def test_rule_014_lower(self):
+class test_rule(unittest.TestCase):
+    def test_rule_014(self):
         oRule = function.rule_014()
         self.assertTrue(oRule)
         self.assertEqual(oRule.name, "function")
         self.assertEqual(oRule.identifier, "014")
-
-        lExpected = [15, 17]
-
-        oRule.analyze(self.oFile)
-        self.assertEqual(utils.extract_violation_lines_from_violation_object(oRule.violations), lExpected)
-
-    def test_rule_014_upper(self):
-        oRule = function.rule_014()
-        oRule.case = "upper"
-        self.assertTrue(oRule)
-        self.assertEqual(oRule.name, "function")
-        self.assertEqual(oRule.identifier, "014")
-
-        lExpected = [13, 17]
-        oRule.analyze(self.oFile)
-        self.assertEqual(utils.extract_violation_lines_from_violation_object(oRule.violations), lExpected)
-
-    def test_fix_rule_014_lower(self):
-        oRule = function.rule_014()
-
-        oRule.fix(self.oFile)
-
-        lActual = self.oFile.get_lines()
-
-        self.assertEqual(lExpected_lower, lActual)
-
-        oRule.analyze(self.oFile)
-        self.assertEqual(oRule.violations, [])
-
-    def test_fix_rule_014_upper(self):
-        oRule = function.rule_014()
-        oRule.case = "upper"
-
-        oRule.fix(self.oFile)
-
-        lActual = self.oFile.get_lines()
-
-        self.assertEqual(lExpected_upper, lActual)
-
-        oRule.analyze(self.oFile)
-        self.assertEqual(oRule.violations, [])
+        self.assertTrue(oRule.deprecated)
