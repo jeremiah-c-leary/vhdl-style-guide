c66211af0e8a3126b13ae6a9df8254a140c42034
JHertz5 <jukka.hertzog@live.co.uk>
2024-12-21 11:21:47 +0000

    Issue#1361: Added tests for rule array_constraint_500.

---
+++ tests/array_constraint/rule_500_test_input.vhd
@@ -0,0 +1,31 @@
+
+architecture rtl of fifo is
+
+  subtype t_my_array is t_array(open)(t_range);
+
+begin
+
+  cmp : entity work.my_component
+    port map (
+      clk   => open,
+      reset => reset
+    );
+
+end architecture rtl;
+
+--====== UPPERCASE before
+
+
+architecture rtl of fifo is
+
+  subtype t_my_array is t_array(OPEN)(t_range);
+
+begin
+
+  cmp : entity work.my_component
+    port map (
+      clk   => OPEN,
+      reset => reset
+    );
+
+end architecture rtl;
