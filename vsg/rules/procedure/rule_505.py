c1732451d873db306751f2099e3989f81fa5782f
JHertz5 <jukka.hertzog@live.co.uk>
2024-12-21 17:16:57 +0000

    Issue#1364: Updated procedure_505 to only work at the end of procedure declarations.

--- vsg/rules/procedure/rule_505.py
+++ vsg/rules/procedure/rule_505.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from vsg import token
-from vsg.rules import token_case
+from vsg.rules import token_case_in_range_bounded_by_tokens
 
 lTokens = []
 lTokens.append(token.subprogram_kind.procedure_keyword)
 
+oStartToken = token.subprogram_body.end_keyword
+oEndToken = token.subprogram_body.semicolon
 
-class rule_505(token_case):
+class rule_505(token_case_in_range_bounded_by_tokens):
     """
     This rule checks the **procedure** keyword in the **end procedure** has proper case.
 
