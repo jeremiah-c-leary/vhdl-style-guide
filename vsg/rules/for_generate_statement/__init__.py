0a92f649fb9add4f9f28379a8509677f93232a2c
JHertz5 <jukka.hertzog@live.co.uk>
2024-12-29 17:49:50 +0000

    Issue#1376: Added case rule for parameter identifiers in for-generates.

--- vsg/rules/for_generate_statement/__init__.py
+++ vsg/rules/for_generate_statement/__init__.py
@@ -3,3 +3,4 @@
 from .rule_500 import rule_500
 from .rule_501 import rule_501
 from .rule_502 import rule_502
+from .rule_503 import rule_503
---
+++ vsg/rules/for_generate_statement/rule_503.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from vsg import token
+from vsg.rules import token_case_in_range_bounded_by_tokens
+
+lTokens = []
+lTokens.append(token.parameter_specification.identifier)
+
+oStartToken = token.for_generate_statement.for_keyword
+oEndToken = token.for_generate_statement.generate_keyword
+
+
+class rule_503(token_case_in_range_bounded_by_tokens):
+    """
+    This rule checks the parameter identifier has proper case.
+
+    |configuring_uppercase_and_lowercase_rules_link|
+
+    **Violation**
+
+    .. code-block:: vhdl
+
+       gen_label : for LV_THING in t_thing generate
+
+    **Fix**
+
+    .. code-block:: vhdl
+
+       gen_label : for lv_thing in t_thing generate
+    """
+
+    def __init__(self):
+        super().__init__(lTokens, oStartToken, oEndToken)
+        self.groups.append("case::keyword")
