78380c9067d090acdc70a46faf075aca9a2ee79a
JHertz5 <jukka.hertzog@live.co.uk>
2024-12-30 18:55:58 +0000

    Issue#1376: Added tests for parameter_specification rule for identifier case.

---
+++ tests/parameter_specification/rule_500_test_input.vhd
@@ -0,0 +1,28 @@
+
+architecture rtl of fifo is
+
+begin
+
+  gen_test : for gv_param in t_range generate
+  end generate gen_test;
+
+  my_proc : process is
+  begin
+
+    for lv_param in t_range loop
+    end loop;
+
+  end process my_proc;
+
+  gen_test : for GV_PARAM in t_range generate
+  end generate gen_test;
+
+  my_proc : process is
+  begin
+
+    for LV_PARAM in t_range loop
+    end loop;
+
+  end process my_proc;
+
+end;
