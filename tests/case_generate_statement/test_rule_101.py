e8373470522162af14afe4c19505bf779e0ba79b
JHertz5 <jukka.hertzog@live.co.uk>
2025-01-16 20:32:22 +0000

    Issue#1389: Corrected test file reference.

--- tests/case_generate_statement/test_rule_101.py
+++ tests/case_generate_statement/test_rule_101.py
@@ -9,11 +9,11 @@ from vsg.rules import case_generate_statement
 
 sTestDir = os.path.dirname(__file__)
 
-lFile, eError = vhdlFile.utils.read_vhdlfile(os.path.join(sTestDir, "rule_003_test_input.vhd"))
+lFile, eError = vhdlFile.utils.read_vhdlfile(os.path.join(sTestDir, "rule_101_test_input.vhd"))
 
 lExpected = []
 lExpected.append("")
-utils.read_file(os.path.join(sTestDir, "rule_003_test_input.fixed.vhd"), lExpected)
+utils.read_file(os.path.join(sTestDir, "rule_101_test_input.fixed.vhd"), lExpected)
 
 
 class test_rule(unittest.TestCase):
@@ -21,8 +21,8 @@ class test_rule(unittest.TestCase):
         self.oFile = vhdlFile.vhdlFile(lFile)
         self.assertIsNone(eError)
 
-    def test_rule_003(self):
-        oRule = case_generate_statement.rule_003()
+    def test_rule_101(self):
+        oRule = case_generate_statement.rule_101()
         self.assertTrue(oRule)
         self.assertEqual(oRule.name, "case_generate_statement")
         self.assertEqual(oRule.identifier, "101")
@@ -32,8 +32,8 @@ class test_rule(unittest.TestCase):
         oRule.analyze(self.oFile)
         self.assertEqual(lExpected, utils.extract_violation_lines_from_violation_object(oRule.violations))
 
-    def test_fix_rule_003(self):
-        oRule = case_generate_statement.rule_003()
+    def test_fix_rule_101(self):
+        oRule = case_generate_statement.rule_101()
 
         oRule.fix(self.oFile)
 
