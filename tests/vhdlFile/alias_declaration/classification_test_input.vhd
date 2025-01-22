5f461d437845f2e1c12f3dd8456c792f62a19112
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2025-01-22 06:43:20 -0600

    Issue#1399:  external names would end processing of the name production.

--- tests/vhdlFile/alias_declaration/classification_test_input.vhd
+++ tests/vhdlFile/alias_declaration/classification_test_input.vhd
@@ -1,10 +1,6 @@
 
 architecture RTL of FIFO is
 
-
-
-
-
   alias ident : std_logic_vector(3 downto 0) is write_enable [name1, name2 return integer];
 
   alias ident : std_logic_vector(3 downto 0) is write_enable [name1, name2];
@@ -21,6 +17,8 @@ architecture RTL of FIFO is
   alias ident is write_enable [return integer];
 
   alias ident is write_enable;
+
+  alias alias_sop : std_logic is << signal .tb_top.submodule.i_rx_data : t_mac_interface >> .sop;
 
 begin
 
