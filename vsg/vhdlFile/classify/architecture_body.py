d93121852ada6f1e0e040e4e577da28bd421b078
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2025-02-06 21:42:14 -0600

    Refactoring architecture_body.

--- vsg/vhdlFile/classify/architecture_body.py
+++ vsg/vhdlFile/classify/architecture_body.py
@@ -23,27 +23,18 @@ def detect(oDataStructure):
 
 
 def classify(oDataStructure):
-    classify_opening_declaration(oDataStructure)
-
-    architecture_declarative_part.detect(oDataStructure)
-
-    oDataStructure.replace_next_token_required("begin", token.begin_keyword)
-
-    #architecture_statement_part.classify_until(["end"])
-    architecture_statement_part.detect(oDataStructure)
-
-    classify_closing_declaration(oDataStructure)
-
-
-def classify_opening_declaration(oDataStructure):
     oDataStructure.replace_next_token_with(token.architecture_keyword)
     oDataStructure.replace_next_token_with(token.identifier)
     oDataStructure.replace_next_token_required("of", token.of_keyword)
     oDataStructure.replace_next_token_with(token.entity_name)
     oDataStructure.replace_next_token_required("is", token.is_keyword)
 
+    architecture_declarative_part.detect(oDataStructure)
 
-def classify_closing_declaration(oDataStructure):
+    oDataStructure.replace_next_token_required("begin", token.begin_keyword)
+
+    architecture_statement_part.detect(oDataStructure)
+
     oDataStructure.replace_next_token_required("end", token.end_keyword)
     oDataStructure.replace_next_token_with_if("architecture", token.end_architecture_keyword)
     oDataStructure.replace_next_token_with_if_not(";", token.architecture_simple_name)
