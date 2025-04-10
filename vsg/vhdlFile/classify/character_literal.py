3059cea0288028f42f9b7139776ff0ac840ecc4d
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2025-04-09 21:30:13 -0500

    Removing unused align_seek_index.

--- vsg/vhdlFile/classify/character_literal.py
+++ vsg/vhdlFile/classify/character_literal.py
@@ -9,7 +9,6 @@ def detect(oDataStructure):
     character_literal ::=
         ' graphic_character '
     """
-    oDataStructure.align_seek_index()
     oDataStructure.advance_to_next_seek_token()
     sValue = oDataStructure.get_seek_token_lower_value()
     if len(sValue) == 3 and sValue.startswith("'") and sValue.endswith("'"):
