4600a01c6120a9b42e9b75242c9f235ba19bb1e2
JHertz5 <jukka.hertzog@live.co.uk>
2025-06-02 15:29:16 +0100

    Revert "Issue#1459: Test change to trigger CI run."
    This reverts commit d983807ceadb9c6b942ea60f327dce28fc96b485.

--- vsg/rules/instantiation/rule_033.py
+++ vsg/rules/instantiation/rule_033.py
@@ -16,7 +16,7 @@ class rule_033(insert_token_left_of_token_if_it_does_not_exist_between_tokens):
     """
     This rule checks for the **component** keyword for a component instantiation.
 
-    |configuring_optional_items_link
+    |configuring_optional_items_link|
 
     **Violation**
 
