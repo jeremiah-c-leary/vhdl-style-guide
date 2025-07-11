798759c281b431f90720168f93793627237dd702
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2025-07-10 21:46:27 -0500

    Fixing style issues.

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ import vsg.version
 # ones.
 extensions = ["sphinx.ext.coverage", "sphinx.ext.autodoc", "sphinx.ext.autosectionlabel", "sphinx_rtd_theme"]
 
-suppress_warnings = ['autosectionlabel.*']
+suppress_warnings = ["autosectionlabel.*"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -105,7 +105,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
