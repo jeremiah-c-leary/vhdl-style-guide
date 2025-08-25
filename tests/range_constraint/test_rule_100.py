533c5658ed4bc29102940770d51b1e5fa9f0bbe0
JHertz5 <jukka.hertzog@live.co.uk>
2025-08-25 08:14:44 +0100

    Issue#1491: Updated test class names.

--- tests/range_constraint/test_rule_100.py
+++ tests/range_constraint/test_rule_100.py
@@ -16,7 +16,7 @@ lExpected.append("")
 utils.read_file(os.path.join(sTestDir, "rule_100_test_input.fixed.vhd"), lExpected)
 
 
-class test_range_constraint_rule(unittest.TestCase):
+class test_rule(unittest.TestCase):
     def setUp(self):
         self.oFile = vhdlFile.vhdlFile(lFile)
         self.assertIsNone(eError)
