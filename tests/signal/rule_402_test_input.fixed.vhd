b6bcd8cea35048620d796b660a3d4cf45373fcc7
JHertz5 <jukka.hertzog@live.co.uk>
2025-09-14 11:33:39 +0100

    Issue#1503: Added tests for signal_402.

---
+++ tests/signal/rule_402_test_input.fixed.vhd
@@ -0,0 +1,20 @@
+
+architecture rtl of fifo is
+
+  signal wr_data : my_record :=
+  (
+    (name => "Hold in reset",
+      clk_in => "01",
+      rst_in => "11",
+      cnt_en_in => "00",
+      cnt_out => "00"),
+    (name => "Not enabled",
+      clk_in => "01",
+      rst_in => "00",
+      cnt_en_in => "00",
+      cnt_out => "00")
+  );
+
+begin
+
+end architecture rtl;
