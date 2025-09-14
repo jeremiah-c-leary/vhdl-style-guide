7610bfa8afcc68ac9b76ced4f8e23e93c94810f9
JHertz5 <jukka.hertzog@live.co.uk>
2025-09-14 17:06:30 +0100

    Issue#1510: Added new rules to handle multiline variable initialisations.

---
+++ vsg/rules/variable/rule_402.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+
+from vsg.token import variable_declaration as token
+from vsg.rules import multiline_array_alignment as Rule
+
+lTokenPairs = []
+lTokenPairs.append([token.assignment_operator, token.semicolon])
+
+
+class rule_402(Rule):
+    """
+    This rule checks the alignment of multiline variables that contain arrays.
+
+    |configuring_multiline_indent_rules_link|
+
+    .. NOTE:: The structure of multiline array variables is handled by the rule `variable_403 <variable_rules.html#variable-403>`_.
+
+    **Violation**
+
+    .. code-block:: vhdl
+
+       variable rom : romq_type :=
+       (
+                0,
+            65535,
+            32768
+         );
+
+    **Fix**
+
+    .. code-block:: vhdl
+
+       variable rom : romq_type :=
+       (
+         0,
+         65535,
+         32768
+       );
+    """
+
+    def __init__(self):
+        super().__init__(lTokenPairs)
+        self.assignment_operator = token.assignment_operator
