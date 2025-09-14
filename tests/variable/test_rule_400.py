1256425cabe90fc7162ba755971760327212c6f6
JHertz5 <jukka.hertzog@live.co.uk>
2025-09-14 17:41:22 +0100

    Issue#1510: Corrections.

--- tests/variable/test_rule_400.py
+++ tests/variable/test_rule_400.py
@@ -48,7 +48,6 @@ class test_rule(unittest.TestCase):
 
         lExpected = []
         lExpected.extend(range(5, 20))
-        # lExpected.extend(range(22, 26))
         lExpected.extend(range(28, 32))
         lExpected.extend(range(34, 36))
 
@@ -140,7 +139,6 @@ class test_rule(unittest.TestCase):
 
         lExpected = []
         lExpected.extend(range(4, 20))
-        # lExpected.extend(range(22, 26))
         lExpected.extend(range(28, 32))
         lExpected.extend(range(34, 36))
 
@@ -186,7 +184,6 @@ class test_rule(unittest.TestCase):
 
         lExpected = []
         lExpected.extend(range(5, 20))
-        # lExpected.extend(range(22, 26))
         lExpected.extend(range(28, 32))
         lExpected.extend(range(34, 36))
 
