a3c97a8c805d3d5c74d4380f05ea3208b7a3d474
JHertz5 <jukka.hertzog@live.co.uk>
2025-09-14 17:47:37 +0100

    Issue#1510: Removed commented-out code.

--- tests/variable/test_rule_400.py
+++ tests/variable/test_rule_400.py
@@ -93,7 +93,6 @@ class test_rule(unittest.TestCase):
 
         lExpected = []
         lExpected.extend(range(4, 20))
-        # lExpected.extend(range(22, 26))
         lExpected.extend(range(28, 32))
         lExpected.extend(range(34, 36))
 
