380226beb180e7a5fc1da28b8473fe6d90f538f9
JHertz5 <jukka.hertzog@live.co.uk>
2025-09-14 17:33:45 +0100

    Issue#1510: Added tests for new rules to handle multiline variable initialisations.

---
+++ tests/variable/rule_403_test_input.fixed.vhd
@@ -0,0 +1,54 @@
+
+architecture RTL of FIFO is
+
+begin
+
+  my_proc : process
+
+    -- These are passing
+    variable ret : my_type :=
+ (
+      data  => (others => '-'),
+      valid => '0',
+      sop   => '0',
+      eop   => '0',
+      empty => (others => '0'),
+      error => (others => '0')
+    );
+
+    -- These are failing
+
+    variable ret : my_type :=
+ (
+ data  => (others => '-'),
+ valid => '0',
+ sop   => '0',
+ eop   => '0',
+ empty => (others => '0'),
+ error => (others => '0')
+ );
+
+
+  -- This is not an array and should not be "fixed"
+
+  variable d : my_type :=
+       (d2 xor to_stdulogic(gen2)) &
+       (d1 xor to_stdulogic(gen1));
+
+  variable new_phase : my_type := (not sig1) or
+               sig2 or
+               sig3;
+
+  -- Test functions/constant in array
+
+  variable data_concurrent : my_type :=
+  (
+    c_enum_list(ENUM_LITERAL_1) => 1,
+    others       => 'X'
+  );
+
+  begin
+
+  end process;
+
+end architecture RTL;
