f5250c180d9bf4212e3f562457db3ec8191e4a9d
Luca Zulberti <Luca.Zulberti@ingeniars.com>
2026-03-24 12:52:02 +0100

    Add support for optional multi-style block comments

--- vsg/block_rule.py
+++ vsg/block_rule.py
@@ -1,11 +1,42 @@
 # -*- coding: utf-8 -*-
 
+import math
 import string
 
-from vsg import parser
+from vsg import exceptions, parser
 from vsg.rule_group import structure
 from vsg.rules import utils as rules_utils
 from vsg.vhdlFile import utils
+
+HEADER_FIELDS = [
+    "header_left",
+    "header_left_repeat",
+    "header_string",
+    "header_right_repeat",
+    "header_alignment",
+    "max_header_column",
+]
+
+BODY_FIELDS = [
+    "comment_left",
+]
+
+FOOTER_FIELDS = [
+    "footer_left",
+    "footer_left_repeat",
+    "footer_string",
+    "footer_right_repeat",
+    "footer_alignment",
+    "max_footer_column",
+]
+
+ALL_STYLE_FIELDS = HEADER_FIELDS + BODY_FIELDS + FOOTER_FIELDS
+
+RULE_FIELD_MAP = {
+    "block_comment_001": HEADER_FIELDS,
+    "block_comment_002": BODY_FIELDS,
+    "block_comment_003": FOOTER_FIELDS,
+}
 
 
 class Rule(structure.Rule):
@@ -19,6 +50,38 @@ class Rule(structure.Rule):
         self.allow_indenting = "yes"
         self.configuration.append("allow_indenting")
         self.configuration_documentation_link = "configuring_block_comments_link"
+
+        # Shared block comment style attributes.
+        # Concrete rules expose only their own configuration keys, but the base
+        # class owns the full style set so it can validate the family counts
+        # and select one consistent style for the entire block.
+        self.header_left = None
+        self.header_left_repeat = "-"
+        self.header_string = None
+        self.header_right_repeat = None
+        self.header_alignment = "center"
+        self.max_header_column = 120
+
+        self.comment_left = None
+
+        self.footer_left = None
+        self.footer_left_repeat = "-"
+        self.footer_string = None
+        self.footer_right_repeat = None
+        self.footer_alignment = "center"
+        self.max_footer_column = 120
+
+        self._block_comment_style_count = 1
+
+    def configure(self, oConfig):
+        lReturn = super().configure(oConfig)
+        self._synchronize_block_comment_style_attributes(oConfig)
+        self._block_comment_style_count = self._validate_and_get_style_count()
+        return lReturn
+
+    def analyze(self, oFile):
+        self.allow_indenting = utils.convert_yes_no_option_to_boolean(self.allow_indenting)
+        super().analyze(oFile)
 
     def _get_tokens_of_interest(self, oFile):
         lToi = oFile.get_consecutive_lines_starting_with_token(parser.comment, self.min_height)
@@ -40,7 +103,7 @@ class Rule(structure.Rule):
         """
         Applies fixes for any rule violations.
         """
-        self.analyze(oFile)
+        super().fix(oFile, dFixOnly)
 
     def set_token_indent(self, oToken):
         oToken.is_block_comment = True
@@ -55,37 +118,263 @@ class Rule(structure.Rule):
             iWhitespace = 0
         return iWhitespace
 
-    def build_footer(self, oToken):
+    def get_style_count(self):
+        return self._block_comment_style_count
+
+    def get_style(self, iIndex):
+        if iIndex < 0 or iIndex >= self.get_style_count():
+            raise IndexError("Style index out of range: {0}".format(iIndex))
+
+        dStyle = {}
+        for sField in ALL_STYLE_FIELDS:
+            dStyle[sField] = get_style_value_at_index(getattr(self, sField), iIndex)
+        return dStyle
+
+    def get_styles(self):
+        return [self.get_style(iIndex) for iIndex in range(self.get_style_count())]
+
+    def build_header(self, oToken, dStyle=None):
+        if dStyle is None:
+            dStyle = self.get_style(0)
+
+        iWhitespace = self.calculate_leading_whitespace(oToken)
+
+        sHeader = "--"
+
+        sHeaderLeft = dStyle["header_left"]
+        sHeaderLeftRepeat = dStyle["header_left_repeat"] or ""
+        sHeaderString = dStyle["header_string"]
+        sHeaderRightRepeat = dStyle["header_right_repeat"] or ""
+        sHeaderAlignment = dStyle["header_alignment"]
+        iMaxHeaderColumn = dStyle["max_header_column"]
+
+        if sHeaderLeft is not None:
+            sHeader += sHeaderLeft
+            iHeaderLeft = len(sHeaderLeft)
+        else:
+            iHeaderLeft = 0
+
+        if sHeaderString is None:
+            sHeader += sHeaderLeftRepeat * (iMaxHeaderColumn - iWhitespace - len(sHeader))
+        elif sHeaderAlignment == "center":
+            iLeft = math.floor((iMaxHeaderColumn - iWhitespace - len(sHeaderString)) / 2) - iHeaderLeft - 2
+            sLeft = sHeaderLeftRepeat * iLeft
+            iRight = iMaxHeaderColumn - iWhitespace - 2 - iHeaderLeft - len(sHeaderString) - iLeft
+            sRight = sHeaderRightRepeat * iRight
+            sHeader += sLeft + sHeaderString + sRight
+        elif sHeaderAlignment == "left":
+            sHeader += sHeaderLeftRepeat
+            sHeader += sHeaderString
+            iLength = iMaxHeaderColumn - iWhitespace - len(sHeader)
+            sHeader += sHeaderRightRepeat * iLength
+        elif sHeaderAlignment == "right":
+            iLength = iMaxHeaderColumn - iWhitespace - len(sHeader) - len(sHeaderString) - 1
+            sHeader += sHeaderLeftRepeat * iLength
+            sHeader += sHeaderString
+            sHeader += sHeaderRightRepeat
+
+        return sHeader
+
+    def build_footer(self, oToken, dStyle=None):
+        if dStyle is None:
+            dStyle = self.get_style(0)
+
         iWhitespace = self.calculate_leading_whitespace(oToken)
         sFooter = "--"
-        if self.footer_left is not None:
-            sFooter += self.footer_left
-            iFooter_left = len(self.footer_left)
-        else:
-            iFooter_left = 0
 
-        if self.footer_string is None:
-            sFooter += self.footer_left_repeat * (self.max_footer_column - iWhitespace - len(sFooter))
-        elif self.footer_alignment == "center":
-            iLength = int((self.max_footer_column - iWhitespace - len(self.footer_string)) / 2) - iFooter_left - 2
-            sFooter += self.footer_left_repeat * (iLength)
-            sFooter += self.footer_string
-            sFooter += self.footer_right_repeat * (self.max_footer_column - len(sFooter))
-        elif self.footer_alignment == "left":
-            sFooter += self.footer_left_repeat
-            sFooter += self.footer_string
-            sFooter += self.footer_right_repeat * (self.max_footer_column - len(sFooter))
-        elif self.footer_alignment == "right":
-            iLength = self.max_footer_column - iWhitespace - len(sFooter) - len(self.footer_string) - 1
-            sFooter += self.footer_left_repeat * (iLength)
-            sFooter += self.footer_string
-            sFooter += self.footer_right_repeat
+        sFooterLeft = dStyle["footer_left"]
+        sFooterLeftRepeat = dStyle["footer_left_repeat"] or ""
+        sFooterString = dStyle["footer_string"]
+        sFooterRightRepeat = dStyle["footer_right_repeat"] or ""
+        sFooterAlignment = dStyle["footer_alignment"]
+        iMaxFooterColumn = dStyle["max_footer_column"]
+
+        if sFooterLeft is not None:
+            sFooter += sFooterLeft
+            iFooterLeft = len(sFooterLeft)
+        else:
+            iFooterLeft = 0
+
+        if sFooterString is None:
+            sFooter += sFooterLeftRepeat * (iMaxFooterColumn - iWhitespace - len(sFooter))
+        elif sFooterAlignment == "center":
+            iLength = int((iMaxFooterColumn - iWhitespace - len(sFooterString)) / 2) - iFooterLeft - 2
+            sFooter += sFooterLeftRepeat * iLength
+            sFooter += sFooterString
+            sFooter += sFooterRightRepeat * (iMaxFooterColumn - iWhitespace - len(sFooter))
+        elif sFooterAlignment == "left":
+            sFooter += sFooterLeftRepeat
+            sFooter += sFooterString
+            sFooter += sFooterRightRepeat * (iMaxFooterColumn - iWhitespace - len(sFooter))
+        elif sFooterAlignment == "right":
+            iLength = iMaxFooterColumn - iWhitespace - len(sFooter) - len(sFooterString) - 1
+            sFooter += sFooterLeftRepeat * iLength
+            sFooter += sFooterString
+            sFooter += sFooterRightRepeat
+
         return sFooter
 
-    def build_comment(self, oToken):
-        sHeader = "--"
-        sHeader += self.comment_left
-        return sHeader
+    def build_comment(self, oToken=None, dStyle=None):
+        if dStyle is None:
+            dStyle = self.get_style(0)
+
+        sComment = "--"
+        if dStyle["comment_left"] is not None:
+            sComment += dStyle["comment_left"]
+        return sComment
+
+    def matching_style_indexes(self, oToi):
+        lComments = get_comment_tokens(oToi)
+        if len(lComments) < 2:
+            return []
+
+        lMatches = []
+
+        for iStyleIndex in range(self.get_style_count()):
+            dStyle = self.get_style(iStyleIndex)
+
+            if not self.header_matches(lComments[0], dStyle):
+                continue
+
+            bMiddleMatches = True
+            for oToken in lComments[1:-1]:
+                if not self.middle_matches(oToken, dStyle):
+                    bMiddleMatches = False
+                    break
+
+            if not bMiddleMatches:
+                continue
+
+            if not self.footer_matches(lComments[-1], dStyle):
+                continue
+
+            lMatches.append(iStyleIndex)
+
+        return lMatches
+
+    def select_style_index(self, oToi):
+        lMatches = self.matching_style_indexes(oToi)
+        if lMatches:
+            return lMatches[0]
+
+        iBestIndex = 0
+        iBestScore = -1
+
+        for iStyleIndex in range(self.get_style_count()):
+            dStyle = self.get_style(iStyleIndex)
+            iScore = self.style_score(oToi, dStyle)
+            if iScore > iBestScore:
+                iBestScore = iScore
+                iBestIndex = iStyleIndex
+
+        return iBestIndex
+
+    def style_score(self, oToi, dStyle):
+        lComments = get_comment_tokens(oToi)
+        if len(lComments) < 2:
+            return -1
+
+        iScore = 0
+
+        if self.header_matches(lComments[0], dStyle):
+            iScore += 1
+
+        for oToken in lComments[1:-1]:
+            if self.middle_matches(oToken, dStyle):
+                iScore += 1
+
+        if self.footer_matches(lComments[-1], dStyle):
+            iScore += 1
+
+        return iScore
+
+    def header_matches(self, oToken, dStyle):
+        sComment = oToken.get_value()
+
+        try:
+            if not is_header(sComment):
+                return False
+        except IndexError:
+            return False
+
+        return sComment == self.build_header(oToken, dStyle)
+
+    def middle_matches(self, oToken, dStyle):
+        if dStyle["comment_left"] is None:
+            return True
+        return oToken.get_value().startswith(self.build_comment(oToken, dStyle))
+
+    def footer_matches(self, oToken, dStyle):
+        sComment = oToken.get_value()
+        if not is_footer(sComment):
+            return False
+        return sComment == self.build_footer(oToken, dStyle)
+
+    def _synchronize_block_comment_style_attributes(self, oConfig):
+        try:
+            dRuleConfig = oConfig.dConfig["rule"]
+        except (AttributeError, KeyError, TypeError):
+            return
+
+        for sRuleName, lFields in RULE_FIELD_MAP.items():
+            try:
+                dSource = dRuleConfig[sRuleName]
+            except KeyError:
+                continue
+
+            for sField in lFields:
+                if sField in dSource:
+                    setattr(self, sField, dSource[sField])
+
+    def _validate_and_get_style_count(self):
+        dCounts = {
+            "header": self._get_family_style_count(HEADER_FIELDS, "header"),
+            "body": self._get_family_style_count(BODY_FIELDS, "body"),
+            "footer": self._get_family_style_count(FOOTER_FIELDS, "footer"),
+        }
+
+        lUniqueCounts = sorted(set(dCounts.values()))
+        if len(lUniqueCounts) != 1:
+            raise exceptions.ConfigurationError(
+                "Block comment style count mismatch: header={0}, body={1}, footer={2}. "
+                "All three block_comment rules must define the same number of styles.".format(
+                    dCounts["header"], dCounts["body"], dCounts["footer"]
+                )
+            )
+
+        return lUniqueCounts[0]
+
+    def _get_family_style_count(self, lFields, sFamilyName):
+        iStyleCount = 1
+
+        for sField in lFields:
+            iLength = get_style_value_length(getattr(self, sField))
+            if iLength != 1:
+                if iStyleCount == 1:
+                    iStyleCount = iLength
+                elif iLength != iStyleCount:
+                    raise exceptions.ConfigurationError(
+                        "Block comment {0} configuration mismatch: field '{1}' has {2} entries, "
+                        "expected 1 or {3}.".format(sFamilyName, sField, iLength, iStyleCount)
+                    )
+
+        return iStyleCount
+
+
+def get_style_value_length(oValue):
+    if isinstance(oValue, (list, tuple)):
+        return len(oValue)
+    return 1
+
+
+def get_style_value_at_index(oValue, iIndex):
+    if isinstance(oValue, (list, tuple)):
+        return oValue[iIndex]
+    return oValue
+
+
+def get_comment_tokens(oToi):
+    return [oToken for oToken in oToi.get_tokens() if isinstance(oToken, parser.comment)]
 
 
 def is_header(sComment):
--- vsg/rules/block_comment/rule_001.py
+++ vsg/rules/block_comment/rule_001.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import math
-
 from vsg import block_rule, parser, violation
-from vsg.vhdlFile import utils
 
 
 class rule_001(block_rule.Rule):
@@ -33,70 +30,100 @@ class rule_001(block_rule.Rule):
 
     def __init__(self):
         super().__init__()
-        self.header_left = None
-        self.header_left_repeat = "-"
-        self.header_string = None
-        self.header_right_repeat = None
-        self.header_alignment = "center"
-        self.max_header_column = 120
-        self.configuration.extend(["header_left", "header_left_repeat", "header_string", "header_right_repeat", "header_alignment", "max_header_column"])
+        self.fixable = True
+        self.configuration.extend(
+            [
+                "header_left",
+                "header_left_repeat",
+                "header_string",
+                "header_right_repeat",
+                "header_alignment",
+                "max_header_column",
+            ]
+        )
 
-    def analyze(self, oFile):
-        self.allow_indenting = utils.convert_yes_no_option_to_boolean(self.allow_indenting)
+    def analyze_comments(self, oToi):
+        oToken = oToi.get_first_token_matching(parser.comment)
+        if oToken is None:
+            return
 
-        self._print_debug_message("Analyzing rule: " + self.unique_id)
-        lToi = self._get_tokens_of_interest(oFile)
+        sComment = oToken.get_value()
 
-        for oToi in lToi:
-            lTokens = oToi.get_tokens()
+        try:
+            if not block_rule.is_header(sComment):
+                return
+        except IndexError:
+            return
 
-            if not self.allow_indenting:
-                iWhitespace = 0
-            elif isinstance(lTokens[0], parser.comment):
-                iWhitespace = self.indent_size * lTokens[0].get_indent()
-            else:
-                iWhitespace = self.indent_size * lTokens[1].get_indent()
+        self.set_token_indent(oToken)
 
-            sHeader = "--"
+        iStyleIndex = select_autofix_header_style_index(self, oToi)
+        dStyle = self.get_style(iStyleIndex)
+        sHeader = self.build_header(oToken, dStyle)
 
-            if self.header_left is not None:
-                sHeader += self.header_left
-                iHeader_left = len(self.header_left)
-            else:
-                iHeader_left = 0
+        if sComment != sHeader:
+            sSolution = "Change block comment header to : " + sHeader
+            oViolation = violation.New(oToi.get_line_number(), oToi, sSolution)
 
-            if self.header_string is None:
-                sHeader += self.header_left_repeat * (self.max_header_column - iWhitespace - len(sHeader))
-            elif self.header_alignment == "center":
-                iLeft = math.floor((self.max_header_column - iWhitespace - len(self.header_string)) / 2) - iHeader_left - 2
-                sLeft = self.header_left_repeat * iLeft
-                iRight = self.max_header_column - iWhitespace - 2 - iHeader_left - len(self.header_string) - iLeft
-                sRight = self.header_right_repeat * iRight
-                sHeader += sLeft + self.header_string + sRight
-            elif self.header_alignment == "left":
-                sHeader += self.header_left_repeat
-                sHeader += self.header_string
-                iLength = self.max_header_column - iWhitespace - len(sHeader)
-                sHeader += self.header_right_repeat * iLength
-            elif self.header_alignment == "right":
-                iLength = self.max_header_column - iWhitespace - len(sHeader) - len(self.header_string) - 1
-                sHeader += self.header_left_repeat * iLength
-                sHeader += self.header_string
-                sHeader += self.header_right_repeat
+            if style_can_autofix_separator_header(oToken, dStyle):
+                oViolation.set_action({"expected": sHeader})
 
-            for oToken in lTokens:
-                if isinstance(oToken, parser.comment):
-                    sComment = oToken.get_value()
-                    try:
-                        if block_rule.is_header(sComment):
-                            self.set_token_indent(oToken)
-                            if sComment != sHeader:
-                                sSolution = "Change block comment header to : " + sHeader
-                                oViolation = violation.New(oToi.get_line_number(), oToi, sSolution)
-                                self.add_violation(oViolation)
-                        break
-                    except IndexError:
-                        break
+            self.add_violation(oViolation)
+
+    def _fix_violation(self, oViolation):
+        dAction = oViolation.get_action()
+        if dAction is None or "expected" not in dAction:
+            return
+
+        lTokens = oViolation.get_tokens()
+
+        for oToken in lTokens:
+            if isinstance(oToken, parser.comment):
+                oToken.set_value(dAction["expected"])
+                break
+
+        oViolation.set_tokens(lTokens)
+
+
+def select_autofix_header_style_index(self, oToi):
+    oHeaderToken = oToi.get_first_token_matching(parser.comment)
+    if oHeaderToken is None:
+        return self.select_style_index(oToi)
+
+    for iStyleIndex in range(self.get_style_count()):
+        dStyle = self.get_style(iStyleIndex)
+        if style_can_autofix_separator_header(oHeaderToken, dStyle):
+            return iStyleIndex
+
+    return self.select_style_index(oToi)
+
+
+def style_can_autofix_separator_header(oToken, dStyle):
+    sComment = oToken.get_value()
+
+    if dStyle["header_string"] not in (None, ""):
+        return False
+
+    sPrefix = "--"
+    if dStyle["header_left"] is not None:
+        sPrefix += dStyle["header_left"]
+
+    sRepeat = dStyle["header_left_repeat"] or ""
+    if sRepeat == "":
+        return False
+
+    if not sComment.startswith(sPrefix):
+        return False
+
+    sTail = sComment[len(sPrefix) :]
+    if sTail == "":
+        return True
+
+    for sChar in sTail:
+        if sChar != sRepeat:
+            return False
+
+    return True
 
 
 #         1         2         3         4         5         6         7         8
